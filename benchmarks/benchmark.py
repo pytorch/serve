b82784011e18c5ed35075eaa446120350c3dd6c8
Vamshidhar Dantu <vamshidhar.d.k@gmail.com>
2019-11-08 12:12:14 -0800

    Removed copyrights

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# Licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License is located at
-#     http://www.apache.org/licenses/LICENSE-2.0
-# or in the "license" file accompanying this file. This file is distributed
-# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-# express or implied. See the License for the specific language governing
-# permissions and limitations under the License.
+
 
 """
 Execute the MMS Benchmark.  For instructions, run with the --help flag
