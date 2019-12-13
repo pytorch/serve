9b9b9e345542c747236798d8582182597698b63d
harshbafna <harsh_bafna@persistent.co.in>
2019-12-13 20:43:11 +0530

    fixed issue

--- model-archiver/model_archiver/manifest_components/model.py
+++ model-archiver/model_archiver/manifest_components/model.py
@@ -13,11 +13,11 @@ class Model(object):
     def __init__(self, model_name, serialized_file, model_file=None, handler=None, description=None, model_version=None, extensions=None):
         self.model_name = model_name
         self.serialized_file = serialized_file.split("/")[-1]
-        self.model_file = model_file.split("/")[-1]
+        self.model_file = model_file
         self.description = description
         self.model_version = model_version
         self.extensions = extensions
-        self.handler = handler.split("/")[-1]
+        self.handler = handler
         self.model_dict = self.__to_dict__()
 
     def __to_dict__(self):
@@ -28,9 +28,9 @@ class Model(object):
         model_dict['serializedFile'] = self.serialized_file
 
         if self.model_file is not None:
-            model_dict['modelFile'] = self.model_file
+            model_dict['modelFile'] = self.model_file.split("/")[-1]
 
-        model_dict['handler'] = self.handler if self.handler else ""
+        model_dict['handler'] = self.handler.split("/")[-1] if self.handler else ""
 
         if self.description is not None:
             model_dict['description'] = self.description
