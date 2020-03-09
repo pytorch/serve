a512b07612913578bea386b90547be09d054248a
harshbafna <harsh_bafna@persistent.co.in>
2020-03-09 19:17:10 +0530

    updated scripts

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IMAGE_NAME="torchserve:1.0"
 
-echo "start torchserve:1.0 docker image"
+echo "Starting torchserve:1.0 docker image"
 
 docker run -d --rm -it -p 8080:8080 -p 8081:8081 torchserve:1.0 > /dev/null 2>&1
 container_id=$(docker ps --filter="ancestor=$IMAGE_NAME" -q | xargs)
@@ -11,12 +11,11 @@ sleep 30
 echo "Successfully started torchserve in docker"
 
 echo "Registering resnet-18 model"
-response=$(curl --retry 5 -X POST "http://localhost:8081/models?url=https://torchserve.s3.amazonaws.com/mar_files/resnet-18.mar")
+response=$(curl --write-out %{http_code} --silent --output /dev/null --retry 5 -X POST "http://localhost:8081/models?url=https://torchserve.s3.amazonaws.com/mar_files/resnet-18.mar")
 
-if [ ! $response = { "status": "Model \"resnet-18\" registered" } ]
+if [ ! "$response" == 200 ]
 then
     echo "failed to register model with torchserve"
-    exit 1
 else
     echo "successfully registered resnet-18 model with torchserve"
 fi
