49d00121ae05a01f03a1bfce00160dd21b1bb361
Hamid <hamid.nazeri2010@gmail.com>
2020-04-24 22:15:28 -0700

    modified to include mapping of predicitons and decoding received text

--- examples/text_classification/bert/bert_handler.py
+++ examples/text_classification/bert/bert_handler.py
@@ -5,6 +5,9 @@ import os
 import torch
 from torch.autograd import Variable
 from transformers import BertTokenizer, BertForSequenceClassification
+import json
+
+
 
 
 logger = logging.getLogger(__name__)
@@ -22,6 +25,13 @@ class Bertseqclassifier(object):
         self.mapping = None
         self.device = None
         self.initialized = False
+        self.name_index = {
+        "1":"World",
+         "2":"Sports",
+         "3":"Business",
+         "4":"Sci/Tec"
+        }
+
 
     def initialize(self, ctx):
         """We simply load the pre-trained model from transfromers here is bert-base model"""
@@ -30,7 +40,7 @@ class Bertseqclassifier(object):
         self.device = torch.device("cuda:" + str(properties.get("gpu_id")) if torch.cuda.is_available() else "cpu")
         model_dir = properties.get("model_dir")
 
-        # Read model serialize/pt file and if not found load it from pe-trained model 
+        # Read model serialize/pt file and if not found load it from pe-trained model
         model_pt_path = os.path.join(model_dir, "traced_bert.pt")
         if not os.path.isfile(model_pt_path):
             self.model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels = 4)
@@ -44,7 +54,14 @@ class Bertseqclassifier(object):
         # self.model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels = 4)
         self.model.to(self.device)
         self.model.eval()
+        # Read the mapping file, index to object name
+        mapping_file_path = os.path.join(model_dir, "index_to_name.json")
 
+        if os.path.isfile(mapping_file_path):
+            with open(mapping_file_path) as f:
+                self.mapping = json.load(f)
+        else:
+            logger.warning('Missing the index_to_name.json file. Inference output will not include class name.')
         # logger.debug('Model file {0} loaded successfully'.format(model_pt_path))
         self.initialized = True
 
@@ -53,9 +70,14 @@ class Bertseqclassifier(object):
          Process the text to the input format for Bert model.
         """
         text = data[0].get("data")
+        if text is None:
+            text = data[0].get("body")
+
+        # Convert to a "unicode" object
+        text_obj = text.decode('UTF-8')
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-        input_ids = torch.tensor(self.tokenizer.encode(text, add_special_tokens=True)).unsqueeze(0)  # Batch size 1
+        input_ids = torch.tensor(self.tokenizer.encode(text_obj, add_special_tokens=True)).unsqueeze(0)  # Batch size 1
 
 
         return input_ids
@@ -67,12 +89,16 @@ class Bertseqclassifier(object):
 
         self.model.eval()
         outputs = self.model(input_ids)
+        output = outputs[0].argmax(1).item() + 1
+        if self.mapping:
+            output = self.mapping[str(output)]
 
-        _, y_hat = outputs[0].max(1)
-        predicted_idx = str(y_hat.item())
-        return [predicted_idx]
+        # _, y_hat = outputs[0].max(1)
+        # predicted_idx = str(y_hat.item())
+        return [output]
 
     def postprocess(self, inference_output):
+
         return inference_output
 
 
