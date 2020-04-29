e5fbc07d9a9e1d78909dd432de4595323efbb2f8
Hamid <hamid.nazeri2010@gmail.com>
2020-04-29 09:37:47 -0700

    modified comments

--- examples/Transformers/Download_Transformer_models.py
+++ examples/Transformers/Download_Transformer_models.py
@@ -2,19 +2,19 @@ import transformers
 from pathlib import Path
 import os
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
-""" This function save the checkpoint, config file and also tokenizer config and vocab
+""" This function, save the checkpoint, config file along with tokenizer config and vocab files
     of a transformer model of your choice.
 """
-print('Transformers version',transformers.__version__) # Current version: 2.3.0
+print('Transformers version',transformers.__version__)
 
 def transformers_model_dowloader(pretrained_model_name = 'bert-base-uncased'):
     print("Download model and tokenizer", pretrained_model_name)
     transformer_model = AutoModelForSequenceClassification.from_pretrained(pretrained_model_name)
     transformer_tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
 
-    # NOTE : for demonstration purposes, we scape fine-tune processing here.
-    # A Fine_tunining process based on your needs can be added here.
-    # An example of Colab note notebook for Fine_tunining process has been mentioned in the README.
+    # NOTE : for demonstration purposes, we do not go through the fine-tune processing here.
+    # A Fine_tunining process based on your needs can be added.
+    # An example of Colab notebook for Fine_tunining process has been provided in the README.
 
     NEW_DIR = "./Transformer_model"
     try:
