5bb1597dd213bb88e6199e1545a44135885473c2
Hamid <hamid.nazeri2010@gmail.com>
2020-05-06 21:22:49 -0700

    Separated the constructor and initliaze func

--- examples/Huggingface_Transformers/Transformer_handler_generalized.py
+++ examples/Huggingface_Transformers/Transformer_handler_generalized.py
@@ -15,9 +15,13 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
     """
     Transformers handler class for sequence, token classification and question answering.
     """
-    def __init__(self,ctx):
+    def __init__(self):
         super(TransformersSeqClassifierHandler, self).__init__()
+        self.initialized = False
+
+    def initialize(self, ctx):
         self.manifest = ctx.manifest
+
         properties = ctx.system_properties
         model_dir = properties.get("model_dir")
         self.device = torch.device("cuda:" + str(properties.get("gpu_id")) if torch.cuda.is_available() else "cpu")
@@ -62,6 +66,7 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
             else:
                 logger.warning('Missing the index_to_name.json file.')
 
+        self.initialized = True
 
     def preprocess(self, data):
         """ Basic text preprocessing, based on the user's chocie of application mode.
@@ -132,9 +137,13 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         return inference_output
 
 
+_service = TransformersSeqClassifierHandler()
+
+
 def handle(data, context):
     try:
-        _service = TransformersSeqClassifierHandler(context)
+        if not _service.initialized:
+            _service.initialize(context)
 
         if data is None:
             return None
