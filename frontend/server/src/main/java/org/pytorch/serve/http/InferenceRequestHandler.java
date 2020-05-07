7ac26fbf312d37076674e5066ec5dca5f13569b6
admins <madhusudan_jarhad@persistent.com>
2020-05-07 19:58:16 +0530

    Corrected error message

--- frontend/server/src/main/java/org/pytorch/serve/http/InferenceRequestHandler.java
+++ frontend/server/src/main/java/org/pytorch/serve/http/InferenceRequestHandler.java
@@ -179,7 +179,11 @@ public class InferenceRequestHandler extends HttpRequestHandlerChain {
         Job job = new Job(ctx, modelName, modelVersion, WorkerCommands.PREDICT, input);
         if (!ModelManager.getInstance().addJob(job)) {
             if (modelVersion == null) {
-                modelVersion = Model.DEFAULT_VERSION;
+                throw new ServiceUnavailableException(
+                        "Model \""
+                                + modelName
+                                + "\" has no worker to serve inference request. "
+                                + "Please use scale workers API to add workers.");
             }
             throw new ServiceUnavailableException(
                     "Model \""
