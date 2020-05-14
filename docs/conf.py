a487bd12cde17d26bf06d8cf2699125d5f95ba91
Jessica Lin <jplin@fb.com>
2020-05-13 17:43:56 -0700

    Update source suffix to use latest recommonmark syntax
    Updating to the syntax found here: https://www.sphinx-doc.org/en/master/usage/markdown.html
    
    Co-authored-by: Aaron Markham <markhama@amazon.com>

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,11 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 # source_suffix = '.rst'
 
 
