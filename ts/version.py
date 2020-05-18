311f8aa427c35be435337f8e798555e87f2fbd73
Shivam Shriwas <shivamshriwas21@gmail.com>
2020-05-18 16:29:25 +0530

    python formatting

--- ts/version.py
+++ ts/version.py
@@ -1,9 +1,8 @@
-
 """
 This is the current version of TorchServe
 """
 import os
 from pathlib import Path
 
-version_path=os.path.join(Path(__file__).resolve().parents[1],'version.txt')
+version_path = os.path.join(Path(__file__).resolve().parents[1], 'version.txt')
 __version__ = open(version_path, 'r').read().strip()
