59943ab1b91fd3adb0834404c67fc25862ced286
harshbafna <harsh_bafna@persistent.co.in>
2020-05-25 19:55:17 +0530

    templatized conda meta.yaml for version

--- binaries/conda/build_packages.sh
+++ binaries/conda/build_packages.sh
@@ -4,12 +4,18 @@ set -eou pipefail
 
 PYTHON_VERSIONS="3.6 3.7 3.8"
 PKGS="torchserve torch-model-archiver"
+TORCHSERVE_VERSION=`cat ../../ts/version.txt`
+TORCH_MODEL_ARCHIVER_VERSION=`cat ../../model-archiver/model_archiver/version.txt`
 
 for pkg in ${PKGS}; do
+    PKG_VERSION=$(echo $pkg | tr 'a-z' 'A-Z' | tr '-' '_')_VERSION
+    cat $pkg/meta.yaml.tmpl | sed -e "s/{version}/$PKG_VERSION/" >> $pkg/meta.yaml
     for python_version in ${PYTHON_VERSIONS}; do
         (
             set -x
             conda build --output-folder output/ --python="${python_version}" "${pkg}"
+            set +x
         )
     done
+    rm $pkg/meta.yaml
 done
