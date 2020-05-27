3a1b23f978db53950f5aae803428919005bf95e5
Hamid Nazeri <Hamid.nazeri2010@gmail.com>
2020-05-27 10:03:12 -0700

    Add support for torchscript

--- examples/Huggingface_Transformers/Download_Transformer_models.py
+++ examples/Huggingface_Transformers/Download_Transformer_models.py
@@ -10,7 +10,7 @@ from transformers import (AutoModelForSequenceClassification, AutoTokenizer, Aut
 """
 print('Transformers version',transformers.__version__)
 
-def transformers_model_dowloader(mode,pretrained_model_name,num_labels,do_lower_case):
+def transformers_model_dowloader(mode,pretrained_model_name,num_labels,do_lower_case,max_length,save_mode):
     print("Download model and tokenizer", pretrained_model_name)
     #loading pre-trained model and tokenizer
     if mode== "sequence_classification":
@@ -31,54 +31,6 @@ def transformers_model_dowloader(mode,pretrained_model_name,num_labels,do_lower_
     # An example of Colab notebook for Fine_tunining process has been provided in the README.
 
 
-    """ For demonstration purposes, we show an example of using question answering
-        data preprocessing and inference. Using the pre-trained models will not yeild
-        good results, we need to use fine_tuned models. For example, instead of "bert-base-uncased",
-        if 'bert-large-uncased-whole-word-masking-finetuned-squad' be passed to
-        the AutoModelForSequenceClassification.from_pretrained(pretrained_model_name)
-        a better result can be achieved. Models such as RoBERTa, xlm, xlnet,etc. can be
-        passed as the pre_trained models.
-    """
-
-    text = r"""
-    🤗 Transformers (formerly known as pytorch-transformers and pytorch-pretrained-bert) provides general-purpose
-    architectures (BERT, GPT-2, RoBERTa, XLM, DistilBert, XLNet…) for Natural Language Understanding (NLU) and Natural
-    Language Generation (NLG) with over 32+ pretrained models in 100+ languages and deep interoperability between
-    TensorFlow 2.0 and PyTorch.
-    """
-
-    questions = [
-        "How many pretrained models are available in Transformers?",
-        "What does Transformers provide?",
-        "Transformers provides interoperability between which frameworks?",
-    ]
-
-    device = torch.device("cuda:" + str(properties.get("gpu_id")) if torch.cuda.is_available() else "cpu")
-    model.to(device)
-
-
-    for question in questions:
-        inputs = tokenizer.encode_plus(question, text, add_special_tokens=True, return_tensors="pt")
-        for key in inputs.keys():
-            inputs[key]= inputs[key].to(device)
-
-        input_ids = inputs["input_ids"].tolist()[0]
-
-        # text_tokens = tokenizer.convert_ids_to_tokens(input_ids)
-
-        answer_start_scores, answer_end_scores = model(**inputs)
-
-        answer_start = torch.argmax(
-            answer_start_scores
-        )  # Get the most likely beginning of answer with the argmax of the score
-        answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
-
-        answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-
-        print(f"Question: {question}")
-        print(f"Answer: {answer}\n")
-
-
     NEW_DIR = "./Transformer_model"
     try:
         os.mkdir(NEW_DIR)
@@ -87,9 +39,16 @@ def transformers_model_dowloader(mode,pretrained_model_name,num_labels,do_lower_
     else:
         print ("Successfully created directory %s " % NEW_DIR)
 
-    print("Save model and tokenizer", pretrained_model_name, 'in directory', NEW_DIR)
-    model.save_pretrained(NEW_DIR)
-    tokenizer.save_pretrained(NEW_DIR)
+    print("Save model and tokenizer or Torchscript model based on the setting from setup_config", pretrained_model_name, 'in directory', NEW_DIR)
+    if save_mode == "pretrained":
+        model.save_pretrained(NEW_DIR)
+        tokenizer.save_pretrained(NEW_DIR)
+    elif save_mode == "torchscript":
+        dummy_input = "This is a dummy input for torch trace"
+        inputs = tokenizer.encode_plus(dummy_input,max_length = int(max_length),pad_to_max_length = True, add_special_tokens = True, return_tensors = 'pt')
+        input_ids = inputs["input_ids"]
+        traced_model = torch.jit.trace(model, [input_ids])
+        torch.jit.save(traced_model,os.path.join(NEW_DIR, "traced_model.pt"))
     return
 if __name__== "__main__":
     dirname = os.path.dirname(__file__)
@@ -100,4 +59,6 @@ if __name__== "__main__":
     model_name = options["model_name"]
     num_labels = int(options["num_labels"])
     do_lower_case = options["do_lower_case"]
-    transformers_model_dowloader(mode,model_name, num_labels,do_lower_case)
+    max_length = options["max_length"]
+    save_mode = options["save_mode"]
+    transformers_model_dowloader(mode,model_name, num_labels,do_lower_case, max_length, save_mode)
