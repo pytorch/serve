faf1d8c66ff793dbe756c27bac9d6ad820ad62ad
Tzu-Wei Huang <huang.dexter@gmail.com>
2020-05-31 23:36:57 +0800

    minor fix

--- examples/iris/iris_handler.py
+++ examples/iris/iris_handler.py
@@ -4,6 +4,10 @@ import os
 import torch
 import torch.nn.functional as F
 
+# Command to build and run the server:
+# torch-model-archiver --model-name iris --version 1.0 --model-file examples/iris/iris.py --serialized-file examples/iris/iris.pt --export-path model_store  --handler examples/iris/iris_handler.py --force
+# torchserve --start --ncs --foreground --model-store model_store --models iris.mar
+
 logger = logging.getLogger(__name__)
 
 class Net(torch.nn.Module):
