3447dcb8a48822b52fa5e896fbbfb2bad56b06ab
harshbafna <harsh_bafna@persistent.co.in>
2020-06-01 20:28:14 +0530

    added traps for cleanup

--- binaries/pip/build_wheels.sh
+++ binaries/pip/build_wheels.sh
@@ -6,6 +6,7 @@ source ../../scripts/install_utils
 
 cleanup()
 {
+  echo "cleaning up residuals"
   rm -rf dist
   rm -rf build
   rm -rf *.egg-info
@@ -13,7 +14,10 @@ cleanup()
 
 create_torchserve_wheel()
 {
+
   cd ../../
+
+  trap 'cleanup;exit 1' SIGINT SIGTERM EXIT
 
   python setup.py bdist_wheel --release --universal
 
@@ -22,19 +26,29 @@ create_torchserve_wheel()
   cleanup
 
   cd -
+
+  trap - SIGINT SIGTERM EXIT
 }
 
 create_model_archiver_wheel()
 {
   cd ../../model-archiver
 
+  trap 'cleanup;exit 1' SIGINT SIGTERM EXIT
+
   python setup.py bdist_wheel --release --universal
 
   cp dist/*.whl ../binaries/pip/output/
 
+  echo 'sleeping for test'
+
+  sleep 60
+
   cleanup
 
   cd -
+
+  trap - SIGINT SIGTERM EXIT
 }
 
 rm -rf output
