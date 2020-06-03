f94b3d4cfdf31ef042eeb1d5f532ba29f113d851
Ubuntu <ubuntu@ip-172-31-41-127.us-east-2.compute.internal>
2020-06-03 01:22:57 +0000

    Implemented fully loaded benchmark script by fixing various bugs,corner and edge cases. But due to permission issue on the docker server in logs directory, had to put a hack to get the prediction results back from server to local. Needs the right fix, which might also require a change in Dockerfile. file to set the right permissions. List of things done: 1) Cleaned up mgmt api stuff 2) Integrated docker to run inside of benchmark script 3) Handled corner cases like do not run ab test suite if the model which is passed is not registered already. 4) Handled corner cases like avoiding registration/unregistration loop by not deleting the existing ts container unless it has exited 5) Removed model unregistration code after running inference test. We SHOULD NOT REGISTER/UNREGISTER any models. Our job is to just infer on registered models. 6) Fixed the broken config.properties file(see prev commit) 7) Put a hack for now to invoke docker successfully inspite of permissions issue with docker server image(logs directory). This needs to be cross verified to put the right fix ideally. 8) As a side-effect of permission issues, manipulated the logic to get prediction results for model latency. Did this by processing model_metrics_log file on docker server to get the right model latency values inside the program. 9) Fixed bug due to non initialization of certain variables like batch_size and batch_delay. 10) Cleaned up unused params like 'bcount' etc. 11) Moved the docker pull command outside of if clause. It is a MANDATORY thing so should not be inside any if clause in the first place. 12) Batch inference logic.

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -53,10 +53,6 @@ do
         shift
         shift
         ;;
-        -b|--cnt)
-	BCOUNT="$2"
-	shift
-	;;
         --default)
         DEFAULT=YES
         shift
@@ -97,7 +93,7 @@ fi
 docker pull "${IMAGE}"
 
 if [[ -z "${CONCURRENCY}" ]]; then
-    CONCURRENCY=100
+    CONCURRENCY=1
 fi
 
 if [[ -z "${REQUESTS}" ]]; then
@@ -118,23 +114,19 @@ if [[ ! -z "${WORKER}" ]]; then
     echo "default_workers_per_model=${WORKER}" >> /tmp/benchmark/conf/config.properties
 fi
 
-if [[ -z "${OP}" ]] || test "${OP}" = "R"; then
-    #echo "load_models=benchmark=${URL}" >> /tmp/benchmark/conf/config.properties
-    echo 'setting content type'
-    if [[ ! -z "${INPUT}" ]] && [[ -f "${BASEDIR}/${INPUT}" ]]; then
-        CONTENT_TYPE="application/jpg"
-        cp -rf ${BASEDIR}/${INPUT} /tmp/benchmark/input
-    else
-        CONTENT_TYPE="application/jpg"
-        curl https://s3.amazonaws.com/model-server/inputs/kitten.jpg -s -S -o /tmp/benchmark/input
-    fi
+echo 'setting content type'
+if [[ ! -z "${INPUT}" ]] && [[ -f "${BASEDIR}/${INPUT}" ]]; then
+    CONTENT_TYPE="application/jpg"
+    cp -rf ${BASEDIR}/${INPUT} /tmp/benchmark/input
+else
+    CONTENT_TYPE="application/jpg"
+    curl https://s3.amazonaws.com/model-server/inputs/kitten.jpg -s -S -o /tmp/benchmark/input
 fi
 
 echo "starting docker..."
 
  #start ts docker
 set +e
-echo "docker_runtime is ${DOCKER_RUNTIME} and enable_gpu is ${ENABLE_GPU} and image is ${IMAGE}"
 if [ ! "$(docker ps -q -f name="ts")" ]; then
     if [ "$(docker ps -aq -f status=exited -f name="ts")" ]; then
         # cleanup
@@ -148,94 +140,52 @@ if [ ! "$(docker ps -q -f name="ts")" ]; then
         --ts-config /home/model-server/config.properties
 fi
 set -e
-#docker run -d --rm -it --name ts -p 8080:8080 -p 8081:8081 pytorch/torchserve:latest > /dev/null 2>&1
-#    -v /tmp/benchmark/conf:/opt/ml/conf \
-#    -v /tmp/benchmark/logs:/home/model-server/logs \
-#    -u root torchserve --start --ncs\
-#    --ts-config /opt/ml/conf/config.properties\
-#> /dev/null 2>&1
-#docker run ${DOCKER_RUNTIME} --name ts --rm -p 8080:8080 -p 8081:8081 $ENABLE_GPU\
-#    -v /tmp/benchmark/conf:/opt/ml/conf \
-#    -v /tmp/benchmark/logs:/home/model-server/logs \
-#    -u root -itd ${IMAGE} torchserve --start --ncs\
-#    --ts-config /opt/ml/conf/config.properties
 
-echo "2.2.2.2"
+echo "Docker initiated"
 TS_VERSION=`docker exec -it ts pip freeze | grep torchserve`
 echo "ts_version is ${TS_VERSION}"
-set -e
-echo "3333"
+
 until curl -s "http://localhost:8080/ping" > /dev/null
 do
   echo "Waiting for docker start..."
   sleep 3
 done
 
-echo "4444"
-echo "Docker started successfully"
-
-sleep 10
 container_id=$(docker ps --filter="ancestor=$IMAGE" -q | xargs)
-echo "Docker initiated with container id ${container_id}"
-
-if [[ -z ${OP} ]] || test "${OP}" = "R"; then
-   echo "model already registered so should not send register request again"
-   sleep 10
-else
-    echo "http://localhost:8081/models?url=${URL}&initial_workers=1&synchronous=true"
-    response=$(curl --write-out %{http_code} --silent --output /dev/null --retry 5 -X POST "http://localhost:8081/models?url=${URL}&initial_workers=1&synchronous=true")
-
-    if [ ! "$response" == 200 ]
-    then
-        echo "failed to register model with torchserve"
-    else
-        echo "successfully registered the model with torchserve"
-    fi
-fi
+echo "Docker started successfully with container id ${container_id}"
+sleep 10
 
 result_file="/tmp/benchmark/result.txt"
-metric_log="/tmp/benchmark/logs/model_metrics.log"
+#metric_log="/tmp/benchmark/logs/model_metrics.log"
+metric_log="/home/model-server/logs/model_metrics.log"
+
+if [[ -z "${BATCH_SIZE}" ]]; then
+    BATCH_SIZE=1
+fi
+
+if [[ -z "${BATCH_DELAY}" ]]; then
+    BATCH_DELAY=100
+fi
+
+response=$(curl --write-out %{http_code} --silent --output /dev/null --retry 5 http://localhost:8081/models/${MODEL})
+
+if [ ! "$response" == 200 ]
+then
+    echo "Cannot run inference as this model is not registered with torchserve!!"
+    exit 1
+else
+    echo "Model is already registered with torchserve.. Proceeding with apache benchmark performance tests now!!"
+fi
 
 echo "Executing ab"
 
-if [[ -z "${OP}" ]]; then
-    echo 'Executing inference performance test'
-    ab -c ${CONCURRENCY} -n ${REQUESTS} -k -p /tmp/benchmark/input -T "${CONTENT_TYPE}" \
-        http://127.0.0.1:8080/predictions/${MODEL} > ${result_file}
-
-    if [[ -z "${BATCH_SIZE}" ]]; then
-	    BATCH_SIZE=1
-    fi
-else
-    echo "Executing operation ${OP}"
-    
-    if [[ -z "${BATCH_SIZE}" ]]; then
-	    BATCH_SIZE=1
-    fi
-
-    if [[ -z "${BATCH_DELAY}" ]]; then
-	    BATCH_DELAY=100
-    fi
-
-    if [[ -z "${WORKERS}" ]]; then
-	    WORKERS=1
-    fi
-
-    if test "${OP}" = "R"; then	
-        RURL="?model_name=${MODEL}&url=${URL}&batch_size=${BATCH_SIZE}&max_batch_delay=${BATCH_DELAY}&initial_workers=${WORKERS}&synchronous=true"
-        curl -X POST "http://localhost:8081/models${RURL}"
-
-	echo 'Executing inference performance test'
-        ab -c ${CONCURRENCY} -n ${REQUESTS} -k -p /tmp/benchmark/input -T "${CONTENT_TYPE}" \
-        http://127.0.0.1:8080/predictions/${MODEL} > ${result_file}
-
-	OP=""
-    fi
-fi
+echo 'Executing inference performance test'
+ab -c ${CONCURRENCY} -n ${REQUESTS} -k -p /tmp/benchmark/input -T "${CONTENT_TYPE}" \
+    http://127.0.0.1:8080/predictions/${MODEL} > ${result_file}
 
 echo "ab Execution completed"
 
-echo "Grabing performance numbers"
+echo "Grabbing performance numbers"
 
 BATCHED_REQUESTS=$((${REQUESTS} / ${BATCH_SIZE}))
 echo "requests is $REQUESTS"
@@ -245,16 +195,12 @@ line50=$((${BATCHED_REQUESTS} / 2))
 line90=$((${BATCHED_REQUESTS} * 9 / 10))
 line99=$((${BATCHED_REQUESTS} * 99 / 100))
 
-if [[ -z "${OP}" ]] || test "${OP}" = "R"; then
-    grep "PredictionTime" ${metric_log} | cut -c55- | cut -d"|" -f1 | sort -g > /tmp/benchmark/predict.txt
-    grep "PreprocessTime" ${metric_log} | cut -c55- | cut -d"|" -f1 | sort -g > /tmp/benchmark/preprocess.txt
-    grep "InferenceTime" ${metric_log} | cut -c54- | cut -d"|" -f1 | sort -g > /tmp/benchmark/inference.txt
-    grep "PostprocessTime" ${metric_log} | cut -c56- | cut -d"|" -f1 | sort -g > /tmp/benchmark/postprocess.txt
-
-    MODEL_P50=`sed -n "${line50}p" /tmp/benchmark/predict.txt`
-    MODEL_P90=`sed -n "${line90}p" /tmp/benchmark/predict.txt`
-    MODEL_P99=`sed -n "${line99}p" /tmp/benchmark/predict.txt`
-fi
+#MODEL_P50=`sed -n "${line50}p" /tmp/benchmark/predict.txt`
+#MODEL_P90=`sed -n "${line90}p" /tmp/benchmark/predict.txt`
+#MODEL_P99=`sed -n "${line99}p" /tmp/benchmark/predict.txt`
+MODEL_P50=`docker exec -it ts grep "PredictionTime" ${metric_log} | cut -c55- | cut -d"|" -f1 | sort -g | sed -n "${line50}p"`
+MODEL_P90=`docker exec -it ts grep "PredictionTime" ${metric_log} | cut -c55- | cut -d"|" -f1 | sort -g | sed -n "${line90}p"`
+MODEL_P99=`docker exec -it ts grep "PredictionTime" ${metric_log} | cut -c55- | cut -d"|" -f1 | sort -g | sed -n "${line99}p"`
 
 TS_ERROR=`grep "Failed requests:" ${result_file} | awk '{ print $NF }'`
 TS_TPS=`grep "Requests per second:" ${result_file} | awk '{ print $4 }'`
@@ -267,16 +213,11 @@ TS_ERROR_RATE=`echo "scale=2;100 * ${TS_ERROR}/${REQUESTS}" | bc | awk '{printf 
 echo "" > /tmp/benchmark/report.txt
 echo "======================================" >> /tmp/benchmark/report.txt
 
-if [[ -z "${OP}" ]] || test "${OP}" = "R"; then
-    curl -s http://localhost:8081/models/${MODEL} >> /tmp/benchmark/report.txt
-    echo "Inference result:" >> /tmp/benchmark/report.txt
-    curl -s -X POST http://127.0.0.1:8080/predictions/${MODEL} -H "Content-Type: ${CONTENT_TYPE}" \
-        -T /tmp/benchmark/input >> /tmp/benchmark/report.txt
-    echo "DB.. Now unregistering model"
-    #curl -X DELETE "http://localhost:8081/models/${MODEL}"
-else
-    echo "Benchmark results - Management API - ${OP}"
-fi
+curl -s http://localhost:8081/models/${MODEL} >> /tmp/benchmark/report.txt
+echo "Inference result:" >> /tmp/benchmark/report.txt
+curl -s -X POST http://127.0.0.1:8080/predictions/${MODEL} -H "Content-Type: ${CONTENT_TYPE}" \
+    -T /tmp/benchmark/input >> /tmp/benchmark/report.txt
+#curl -X DELETE "http://localhost:8081/models/${MODEL}"
 
 echo "" >> /tmp/benchmark/report.txt
 echo "" >> /tmp/benchmark/report.txt
@@ -288,11 +229,9 @@ echo "Model: ${MODEL}" >> /tmp/benchmark/report.txt
 echo "Concurrency: ${CONCURRENCY}" >> /tmp/benchmark/report.txt
 echo "Requests: ${REQUESTS}" >> /tmp/benchmark/report.txt
 
-if [[ -z "${OP}" ]] || test "${OP}" = "R"; then
-    echo "Model latency P50: ${MODEL_P50}" >> /tmp/benchmark/report.txt
-    echo "Model latency P90: ${MODEL_P90}" >> /tmp/benchmark/report.txt
-    echo "Model latency P99: ${MODEL_P99}" >> /tmp/benchmark/report.txt
-fi
+echo "Model latency P50: ${MODEL_P50}" >> /tmp/benchmark/report.txt
+echo "Model latency P90: ${MODEL_P90}" >> /tmp/benchmark/report.txt
+echo "Model latency P99: ${MODEL_P99}" >> /tmp/benchmark/report.txt
 echo "TS throughput: ${TS_TPS}" >> /tmp/benchmark/report.txt
 echo "TS latency P50: ${TS_P50}" >> /tmp/benchmark/report.txt
 echo "TS latency P90: ${TS_P90}" >> /tmp/benchmark/report.txt
