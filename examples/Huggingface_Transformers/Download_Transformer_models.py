1cdc16cd2163ab9c994739981a620ae073181d26
Hamid Nazeri <Hamid.nazeri2010@gmail.com>
2020-06-23 13:03:58 -0700

    Add manual seed and finetune setting

--- examples/Huggingface_Transformers/Download_Transformer_models.py
+++ examples/Huggingface_Transformers/Download_Transformer_models.py
@@ -5,25 +5,26 @@ import json
 import torch
 from transformers import (AutoModelForSequenceClassification, AutoTokenizer, AutoModelForQuestionAnswering,
  AutoModelForTokenClassification, AutoConfig)
+from transformers import set_seed
 """ This function, save the checkpoint, config file along with tokenizer config and vocab files
     of a transformer model of your choice.
 """
 print('Transformers version',transformers.__version__)
-
-def transformers_model_dowloader(mode,pretrained_model_name,num_labels,do_lower_case,max_length,torchscript):
+set_seed(1)
+def transformers_model_dowloader(mode,pretrained_model_name, pretrained_model ,num_labels,do_lower_case,max_length,torchscript):
     print("Download model and tokenizer", pretrained_model_name)
     #loading pre-trained model and tokenizer
     if mode== "sequence_classification":
         config = AutoConfig.from_pretrained(pretrained_model_name,num_labels=num_labels,torchscript=torchscript)
-        model = AutoModelForSequenceClassification.from_pretrained(pretrained_model_name, config=config)
+        model = AutoModelForSequenceClassification.from_pretrained(pretrained_model, config=config)
         tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name,do_lower_case=do_lower_case)
     elif mode== "question_answering":
         config = AutoConfig.from_pretrained(pretrained_model_name,torchscript=torchscript)
-        model = AutoModelForQuestionAnswering.from_pretrained(pretrained_model_name,config=config)
+        model = AutoModelForQuestionAnswering.from_pretrained(pretrained_model,config=config)
         tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name,do_lower_case=do_lower_case)
     elif mode== "token_classification":
-        config = AutoConfig.from_pretrained(pretrained_model_name,num_labels=num_labels,torchscript=torchscript)
-        model = AutoModelForTokenClassification.from_pretrained(pretrained_model_name, config=config)
+        config= AutoConfig.from_pretrained(pretrained_model_name,num_labels=num_labels,torchscript=torchscript)
+        model = AutoModelForTokenClassification.from_pretrained(pretrained_model, config=config)
         tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name,do_lower_case=do_lower_case)
 
         # NOTE : for demonstration purposes, we do not go through the fine-tune processing here.
@@ -43,11 +44,10 @@ def transformers_model_dowloader(mode,pretrained_model_name,num_labels,do_lower_
         model.save_pretrained(NEW_DIR)
         tokenizer.save_pretrained(NEW_DIR)
     elif save_mode == "torchscript":
-        # making dummy input and run it through the model to let the torch.jit.trace()
-        # record the operatiosn and outputs a torchscript module.
         dummy_input = "This is a dummy input for torch jit trace"
         inputs = tokenizer.encode_plus(dummy_input,max_length = int(max_length),pad_to_max_length = True, add_special_tokens = True, return_tensors = 'pt')
         input_ids = inputs["input_ids"]
+        model.eval()
         traced_model = torch.jit.trace(model, [input_ids])
         torch.jit.save(traced_model,os.path.join(NEW_DIR, "traced_model.pt"))
     return
@@ -55,16 +55,22 @@ if __name__== "__main__":
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, 'setup_config.json')
     f = open(filename)
-    options = json.load(f)
-    mode = options["mode"]
-    model_name = options["model_name"]
-    num_labels = int(options["num_labels"])
-    do_lower_case = options["do_lower_case"]
-    max_length = options["max_length"]
-    save_mode = options["save_mode"]
+    settings = json.load(f)
+    mode = settings["mode"]
+    model_name = settings["model_name"]
+    num_labels = int(settings["num_labels"])
+    do_lower_case = settings["do_lower_case"]
+    max_length = settings["max_length"]
+    save_mode = settings["save_mode"]
+    fine_tuned = settings["fine_tuned"]
+    if fine_tuned:
+        pretrained_model = fine_tuned
+    else:
+        pretrained_model = model_name
+
     if save_mode == "torchscript":
         torchscript = True
     else:
         torchscript = False
 
-    transformers_model_dowloader(mode,model_name, num_labels,do_lower_case, max_length, torchscript)
+    transformers_model_dowloader(mode,model_name,pretrained_model, num_labels,do_lower_case, max_length, torchscript)
