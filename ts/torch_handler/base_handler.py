6b1026f35cc5fedbc90f2dd50eb4700c4d6b5477
dhaniram-kshirsagar <26479924+dhaniram-kshirsagar@users.noreply.github.com>
2020-07-06 21:50:27 +0530

    Removed unnecessary changes

--- ts/torch_handler/base_handler.py
+++ ts/torch_handler/base_handler.py
@@ -43,7 +43,7 @@ class BaseHandler(abc.ABC):
 
         try:
             logger.debug('Loading torchscript model')
-            self.model = torch.jit.load(model_pt_path, map_location=self.device)
+            self.model = torch.jit.load(model_pt_path)
         except RuntimeError as e:
             # Read model definition file
             model_file = self.manifest['model']['modelFile']
@@ -58,7 +58,7 @@ class BaseHandler(abc.ABC):
                     model_class_definitions))
 
             model_class = model_class_definitions[0]
-            state_dict = torch.load(model_pt_path, map_location=self.device)
+            state_dict = torch.load(model_pt_path)
             self.model = model_class()
             self.model.load_state_dict(state_dict)
         self.model.to(self.device)
