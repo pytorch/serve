fc28a01391184a1228b7f4e32b9428f76ecd0cda
dhanainme <60679183+dhanainme@users.noreply.github.com>
2020-07-30 11:26:20 -0700

    Update torchserve_sanity.sh

--- torchserve_sanity.sh
+++ torchserve_sanity.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-pip install torch
+pip install --no-cache-dir torch
+
 cuda_status=$(python -c "import torch; print(int(torch.cuda.is_available()))")
 if   [ -x "$(command -v nvidia-smi)" ]  ;
 then
