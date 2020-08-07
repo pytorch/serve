294cadc7f46247d3ff35baaa2462f6fdc0432bdd
MengMeng96 <chunmengzhong@gamil.com>
2020-08-07 16:44:02 +0800

    Make Config File Suitable for Windows

--- frontend/server/src/test/java/org/pytorch/serve/SnapshotTest.java
+++ frontend/server/src/test/java/org/pytorch/serve/SnapshotTest.java
@@ -414,6 +414,7 @@ public class SnapshotTest {
         prop.put("default_workers_per_model", 4);
         prop.put("number_of_gpu", 4);
         prop.put("version", "0.1.1");
+        prop.put("management_address", "https://127.0.0.1:8444");
     }
 
     private String getLastSnapshot() {
