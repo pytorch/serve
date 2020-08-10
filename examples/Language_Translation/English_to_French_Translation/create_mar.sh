a8ff6a791ba01a700eecadb87256e3e3797ce1ea
AshwinChafale <ashwin04chafale@gmail.com>
2020-08-10 15:10:18 +0530

    Updated create_mar.sh and requirements.txt files

--- examples/Language_Translation/English_to_French_Translation/create_mar.sh
+++ examples/Language_Translation/English_to_French_Translation/create_mar.sh
@@ -3,11 +3,10 @@
 rm -rf fairseq
 rm wmt14.en-fr.joined-dict.transformer.tar.bz2
 
-git clone https://github.com/pytorch/fairseq.git
-# cd fairseq/
-# pip install ./
+#download zip file of fairseq
+wget https://github.com/pytorch/serve/archive/master.zip
 
-# cd ..
+#download the En2Fr Translation model checkpoint file
 wget https://dl.fbaipublicfiles.com/fairseq/models/wmt14.en-fr.joined-dict.transformer.tar.bz2
 echo
 echo "extracting wmt14.en-fr.joined-dict.transformer.tar.bz2 file ...."
@@ -18,12 +17,11 @@ echo
 echo
 echo "creating mar file ...."
 mkdir model_store
-torch-model-archiver --model-name TransformerEn2Fr --version 1.0 --serialized-file wmt14.en-fr.joined-dict.transformer/model.pt --export-path model_store --handler model_handler.py
+torch-model-archiver --model-name TransformerEn2Fr --version 1.0 --serialized-file wmt14.en-fr.joined-dict.transformer/model.pt --export-path model_store --handler model_handler.py --extra-files wmt14.en-fr.joined-dict.transformer/dict.en.txt,wmt14.en-fr.joined-dict.transformer/dict.fr.txt,wmt14.en-fr.joined-dict.transformer/bpecodes,master.zip -r requirements.txt
 echo "========> mar file creation completed successfully...."
 echo
 
 echo "removing unwanted files ..."
-# rm -rf fairseq
 rm wmt14.en-fr.joined-dict.transformer.tar.bz2
 echo "========> removing completed successfully ..."
 echo
