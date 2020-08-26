1be8b106e6d832c5d090fddfe8b87c1d273ff006
Dhanasekar Karuppasamy <dhanak@amazon.com>
2020-08-26 04:14:23 +0000

    Fix Checkstyle

--- frontend/modelarchive/src/main/java/org/pytorch/serve/archive/ModelArchive.java
+++ frontend/modelarchive/src/main/java/org/pytorch/serve/archive/ModelArchive.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.nio.file.Paths;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileAlreadyExistsException;
