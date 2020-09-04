e4faadac345b4a0612c9ec11bd831607206720cf
Prashant Sail <psail4444@gmail.com>
2020-09-04 21:22:02 +0530

    corrected space and comments

--- test/pytest/test_example_dcgan.py
+++ test/pytest/test_example_dcgan.py
@@ -8,25 +8,24 @@ from PIL import Image
 from io import BytesIO
 
 CURR_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
-REPO_ROOT_DIR = os.path.normpath(os.path.join(CURR_FILE_PATH,"..",".."))
+REPO_ROOT_DIR = os.path.normpath(os.path.join(CURR_FILE_PATH, "..", ".."))
 MODELSTORE_DIR = os.path.join(REPO_ROOT_DIR, "modelstore")
-DCGAN_EXAMPLE_DIR = os.path.join(REPO_ROOT_DIR,"examples","dcgan_fashiongen")
+DCGAN_EXAMPLE_DIR = os.path.join(REPO_ROOT_DIR, "examples", "dcgan_fashiongen")
 DCGAN_MAR_FILE = os.path.join(DCGAN_EXAMPLE_DIR, "dcgan_fashiongen.mar")
 
 
-
-def setup_module(module):
+def setup_module():
     test_utils.torchserve_cleanup()
     create_example_mar()
 
-    os.makedirs(MODELSTORE_DIR, exist_ok=True) # Create modelstore directory
-    copy(DCGAN_MAR_FILE, MODELSTORE_DIR) # Copy *.mar to modelstore
+    os.makedirs(MODELSTORE_DIR, exist_ok=True)  # Create modelstore directory
+    copy(DCGAN_MAR_FILE, MODELSTORE_DIR)  # Copy *.mar to modelstore
 
     test_utils.start_torchserve(model_store=MODELSTORE_DIR)
     pass
 
 
-def teardown_module(module):
+def teardown_module():
     test_utils.torchserve_cleanup()
 
     # Empty and Remove modelstore directory
@@ -53,7 +52,7 @@ def delete_example_mar():
 
 def test_model_archive_creation():
     # *.mar created in setup phase
-    assert os.path.exists(DCGAN_MAR_FILE) , "Failed to create dcgan mar file"
+    assert os.path.exists(DCGAN_MAR_FILE), "Failed to create dcgan mar file"
 
 
 def test_model_register_unregister():
@@ -61,7 +60,7 @@ def test_model_register_unregister():
     assert reg_resp.status_code == 200, "Model Registration Failed"
 
     unreg_resp = test_utils.unregister_model("dcgan_fashiongen")
-    assert unreg_resp.status_code == 200, "Model Unregistration Failed"  # Un-registration successful
+    assert unreg_resp.status_code == 200, "Model Unregistration Failed"
 
 
 def test_image_generation_without_any_input_constraints():
@@ -70,10 +69,10 @@ def test_image_generation_without_any_input_constraints():
     response = requests.post(url="http://localhost:8080/predictions/dcgan_fashiongen", json=input_json)
     fp = BytesIO(response.content)
     img = Image.open(fp)
-    # Expect a jpeg which contains only 1 image of dimension 64 x 64
+    # Expect a jpeg of dimension 64 x 64, it contains only 1 image by default
     assert response.status_code == 200, "Image generation failed"
     assert img.get_format_mimetype() == "image/jpeg", "Generated image is not a jpeg"
-    assert img.size == (64,64), "Generated image is not of correct dimensions"
+    assert img.size == (64, 64), "Generated image is not of correct dimensions"
     test_utils.unregister_model("dcgan_fashiongen")
 
 
@@ -88,8 +87,8 @@ def test_image_generation_with_input_constraints():
     response = requests.post(url="http://localhost:8080/predictions/dcgan_fashiongen", json=input_json)
     fp = BytesIO(response.content)
     img = Image.open(fp)
-    # Expect a jpeg which contains 64 images with of dimension 530 x 530
+    # Expect a jpeg of dimension 530 x 530, it contains 64 images
     assert response.status_code == 200, "Image generation failed"
     assert img.get_format_mimetype() == "image/jpeg", "Generated image is not a jpeg"
-    assert img.size == (530,530), "Generated image is not of correct dimensions"
+    assert img.size == (530, 530), "Generated image is not of correct dimensions"
     test_utils.unregister_model("dcgan_fashiongen")
