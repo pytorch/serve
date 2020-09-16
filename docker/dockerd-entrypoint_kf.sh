99aedd20d1d6f9d2d0c78bc85b98b06b17cfc194
Abishek Chiffon <abishekchiffon@gmail.com>
2020-09-16 13:39:31 +0530

    Modified kfserving dockerfile and entrypoint

--- docker/dockerd-entrypoint_kf.sh
+++ docker/dockerd-entrypoint_kf.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" = "serve" ]]; then
-    shift 1
-    torchserve --start --ts-config /home/model-server/config.properties
-    python /home/model-server/kfserving_wrapper/__main__.py
-else
-    eval "$@"
-fi
+#starts torchserve 
+torchserve --start --ts-config /home/model-server/config.properties
+#Wrapper class starts kfserver 
+python /home/model-server/kfserving_wrapper/__main__.py
 
 # prevent docker exit
 tail -f /dev/null
