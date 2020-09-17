7ac0126188cfbc24d441ec50ac542fa0dd8098a0
Abishek Chiffon <abishekchiffon@gmail.com>
2020-09-17 11:29:52 +0530

    Modified kfserving dockerfile and entrypoint to accept multiple arguments

--- docker/dockerd-entrypoint_kf.sh
+++ docker/dockerd-entrypoint_kf.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -e
 
-#starts torchserve 
-torchserve --start --ts-config /home/model-server/config.properties
-#Wrapper class starts kfserver 
-python /home/model-server/kfserving_wrapper/__main__.py
+if [[ "$1" = *"model_dir"* ]] || [[ "$1" = *"model_name"* ]] || [[ "$1" = *"http_port"* ]] || [[ "$1" = *"grpc_port"* ]] || [[ "$1" = *"workers"* ]] || [[ "$1" = *"max_buffer_size"* ]]; then
+    #starts torchserve 
+    torchserve --start --ts-config /home/model-server/config.properties
+    #Wrapper class starts kfserver 
+    python /home/model-server/kfserving_wrapper/__main__.py $@
+
+else
+    eval "$@"
+fi
+
+
 
 # prevent docker exit
 tail -f /dev/null
