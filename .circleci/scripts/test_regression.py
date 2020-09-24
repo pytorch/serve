35c9e66b72a9d7fb7f3baab31e039eef2f69bcf1
Prashant Sail <psail4444@gmail.com>
2020-09-25 02:14:34 +0530

    Refactor regression tests

--- .circleci/scripts/test_regression.py
+++ .circleci/scripts/test_regression.py
@@ -2,17 +2,18 @@ import os
 import sys
 import urllib.request
 
-BASE_DIR = os.getcwd()
+REPO_ROOT = os.getcwd()
 
 def generate_densenet_test_model_archive():
+    print("## Started densenet mar creation")
     model_store_dir = os.path.join("/", "workspace", "model_store")
     model_name = "densenet161_v1"
     version = "1.1"
-    model_file = os.path.join(BASE_DIR, "examples", "image_classifier", "densenet_161", "model.py")
+    model_file = os.path.join(REPO_ROOT, "examples", "image_classifier", "densenet_161", "model.py")
     serialized_model_file_name = "densenet161-8d451a50.pth"
     serialized_model_file_url = f"https://download.pytorch.org/models/{serialized_model_file_name}"
     serialized_file_path = os.path.join(model_store_dir, serialized_model_file_name)
-    extra_files = os.path.join(BASE_DIR, "examples", "image_classifier", "index_to_name.json")
+    extra_files = os.path.join(REPO_ROOT, "examples", "image_classifier", "index_to_name.json")
     handler = "image_classifier"
 
     os.makedirs(model_store_dir, exist_ok=True)
@@ -21,26 +22,38 @@ def generate_densenet_test_model_archive():
     urllib.request.urlretrieve(serialized_model_file_url, serialized_model_file_name)
 
     # create mar command
-    sys_exit_code = os.system(f"torch-model-archiver \
-                                    --model-name {model_name} \
-                                    --version {version} \
-                                    --model-file {model_file} \
-                                    --serialized-file {serialized_file_path} \
-                                    --extra-files {extra_files} \
-                                    --handler {handler}")
+    cmd = f"torch-model-archiver \
+                --model-name {model_name} \
+                --version {version} \
+                --model-file {model_file} \
+                --serialized-file {serialized_file_path} \
+                --extra-files {extra_files} \
+                --handler {handler}"
+    print(f"## In directory: {os.getcwd()} | Executing command: {cmd}")
+    sys_exit_code = os.system(cmd)
     os.remove(serialized_file_path)
     return sys_exit_code
 
 
 def run_pytest():
-    os.chdir(os.path.join(BASE_DIR, "test", "pytest"))
-    return os.system(f"python -m pytest -v ./")
+    print("## Started regression pytests")
+    os.chdir(os.path.join(REPO_ROOT, "test", "pytest"))
+    cmd = "python -m pytest -v ./"
+    print(f"## In directory: {os.getcwd()} | Executing command: {cmd}")
+    return os.system(cmd)
+
+def test_regression():
+    print("## Started regression tests")
+    # generate_densenet_test_model_archive $MODEL_STORE
+    model_creation_exit_code = generate_densenet_test_model_archive()
+    py_test_exit_code = run_pytest()
+
+    # If any one of the steps fail, exit with error
+    if model_creation_exit_code != 0:
+        sys.exit("## Densenet mar creation failed !")
+    if py_test_exit_code != 0:
+        sys.exit("## TorchServe Regression Pytests Failed")
 
 
-# generate_densenet_test_model_archive $MODEL_STORE
-MODEL_CREATION_EXIT_CODE = generate_densenet_test_model_archive()
-PY_TEST_EXIT_CODE = run_pytest()
-
-# If any one of the steps fail, exit with error
-if any( EXIT_CODE != 0 for EXIT_CODE in [MODEL_CREATION_EXIT_CODE, PY_TEST_EXIT_CODE]):
-    sys.exit("TorchServe Regression Tests Failed")
+if __name__ == "__main__":
+    test_regression()
