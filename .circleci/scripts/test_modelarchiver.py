1bb42484b5e85940a18b50b47c8a5744d05762a2
Prashant Sail <psail4444@gmail.com>
2020-09-25 00:51:39 +0530

    model archiver refactored

--- .circleci/scripts/test_modelarchiver.py
+++ .circleci/scripts/test_modelarchiver.py
@@ -1,28 +1,44 @@
 import os
 import sys
 
-BASE_DIR = os.getcwd()
 
-os.chdir("model-archiver")
+REPO_ROOT = os.getcwd()
 
-# Lint test
-MA_DIR = os.path.join("model_archiver", ".")
-RC_FILE_PATH = os.path.join(".", "model_archiver", "tests", "pylintrc")
-PY_LINT_CMD = f"pylint -rn --rcfile={RC_FILE_PATH} {MA_DIR}"
-PY_LINT_EXIT_CODE = os.system(PY_LINT_CMD)
+def test_modelarchiver():
+    os.chdir("model-archiver")
 
-# Execute python unit tests
-UT_DIR = os.path.join("model_archiver", "tests", "unit_tests")
-COV_DIR = os.path.join(".")
-RESULTS_DIR_NAME = "result_units"
-PY_UNITS_CMD = f"python -m pytest --cov-report html:{RESULTS_DIR_NAME} --cov={COV_DIR} {UT_DIR}"
-PY_UNITS_EXIT_CODE = os.system(PY_UNITS_CMD)
+    # Lint test
+    print("## Started model archiver linting")
+    ma_dir = os.path.join("model_archiver", ".")
+    rc_file_path = os.path.join(".", "model_archiver", "tests", "pylintrc")
+    py_lint_cmd = f"pylint -rn --rcfile={rc_file_path} {ma_dir}"
+    print(f"## In directory: {os.getcwd()}; Executing command: {py_lint_cmd}")
+    py_lint_exit_code = os.system(py_lint_cmd)
 
-# Execute integration tests
-IT_DIR = os.path.join("model_archiver", "tests", "integ_tests")
-PY_INTEG_CMD = f"python -m pytest {IT_DIR}" # ToDo - Report for Integration tests ?
-PY_INTEG_EXIT_CODE = os.system(PY_INTEG_CMD)
+    # Execute python unit tests
+    print("## Started model archiver pytests - unit tests")
+    ut_dir = os.path.join("model_archiver", "tests", "unit_tests")
+    coverage_dir = os.path.join(".")
+    results_dir_name = "result_units"
+    py_units_cmd = f"python -m pytest --cov-report html:{results_dir_name} --cov={coverage_dir} {ut_dir}"
+    print(f"## In directory: {os.getcwd()}; Executing command: {py_units_cmd}")
+    py_units_exit_code = os.system(py_units_cmd)
 
-# If any one of the steps fail, exit with error
-if any( EXIT_CODE != 0 for EXIT_CODE in [PY_LINT_EXIT_CODE, PY_UNITS_EXIT_CODE, PY_INTEG_EXIT_CODE]):
-    sys.exit("Torch Model Archiver Python Tests Failed")
+    # Execute integration tests
+    print("## Started model archiver pytests - integration tests")
+    it_dir = os.path.join("model_archiver", "tests", "integ_tests")
+    py_integ_cmd = f"python -m pytest {it_dir}" # ToDo - Report for Integration tests ?
+    print(f"## In directory: {os.getcwd()}; Executing command: {py_integ_cmd}")
+    py_integ_exit_code = os.system(py_integ_cmd)
+
+    # If any one of the steps fail, exit with error
+    if py_lint_exit_code != 0:
+        sys.exit("## Model archiver Linting Failed !")
+    if py_units_exit_code != 0:
+        sys.exit("## Model archiver Unit Pytests Failed !")
+    if py_integ_exit_code != 0:
+        sys.exit("## Model archiver Integration Pytests Failed !")
+
+
+if __name__ == "__main__":
+    test_modelarchiver()
