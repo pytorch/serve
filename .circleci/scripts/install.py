7ab72377727af9497ec7d0763c09109048fca3af
Prashant Sail <psail4444@gmail.com>
2020-09-24 22:27:05 +0530

    sys path in install as well

--- .circleci/scripts/install.py
+++ .circleci/scripts/install.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import glob
+
+sys.path.append(os.getcwd()) # To help discover local modules
 from scripts import tsutils
 
 BASE_DIR = os.getcwd()
@@ -9,12 +11,14 @@ if tsutils.is_conda_env():
     print("Using conda to install torchserve and torch-model-archiver")
     channel_dir = os.path.abspath(os.path.join(BASE_DIR, "binaries", "conda", "output"))
     conda_cmd = f"conda install --channel {channel_dir} -y torchserve torch-model-archiver"
+    print(f"Executing command: {conda_cmd}")
     INSTALL_EXIT_CODE = os.system(conda_cmd)
 else:
     print("Using pip to install torchserve and torch-model-archiver")
     TS_WHEEL = glob.glob(os.path.join(BASE_DIR, "dist", "*.whl"))[0]
     MA_WHEEL = glob.glob(os.path.join(BASE_DIR, "model-archiver", "dist", "*.whl"))[0]
     pip_cmd = f"pip install {TS_WHEEL} {MA_WHEEL}"
+    print(f"Executing command: {pip_cmd}")
     INSTALL_EXIT_CODE = os.system(pip_cmd)
 
 if INSTALL_EXIT_CODE != 0 :
