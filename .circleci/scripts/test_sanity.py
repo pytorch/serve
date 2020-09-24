87c5335a2eea6f156a35f4866afe15ae4278fdc4
Prashant Sail <psail4444@gmail.com>
2020-09-25 01:20:28 +0530

    Refactor sanity scripts

--- .circleci/scripts/test_sanity.py
+++ .circleci/scripts/test_sanity.py
@@ -5,69 +5,16 @@ import glob
 
 from scripts import tsutils as ts
 
-resnet18_model = {
-    "name": "resnet-18",
-    "inputs": ["examples/image_classifier/kitten.jpg"],
-    "handler": "image_classifier"
-}
-models_to_validate = [
-    {
-        "name": "fastrcnn",
-        "inputs": ["examples/object_detector/persons.jpg"],
-        "handler": "object_detector"
-    },
-    {
-        "name": "fcn_resnet_101",
-        "inputs": ["docs/images/blank_image.jpg", "examples/image_segmenter/fcn/persons.jpg"],
-        "handler": "image_segmenter"
-    },
-    {
-        "name": "my_text_classifier_v2",
-        "inputs": ["examples/text_classification/sample_text.txt"],
-        "handler": "text_classification"
-    },
-    resnet18_model,
-    {
-        "name": "my_text_classifier_scripted_v2",
-        "inputs": ["examples/text_classification/sample_text.txt"],
-        "handler": "text_classification"
-    },
-    {
-        "name": "alexnet_scripted",
-        "inputs": ["examples/image_classifier/kitten.jpg"],
-        "handler": "image_classifier"
-    },
-    {
-        "name": "fcn_resnet_101_scripted",
-        "inputs": ["examples/image_segmenter/fcn/persons.jpg"],
-        "handler": "image_segmenter"
-    },
-    {
-        "name": "roberta_qa_no_torchscript",
-        "inputs": ["examples/Huggingface_Transformers/QA_artifacts/sample_text.txt"],
-        "handler": "custom"
-    },
-    {
-        "name": "bert_token_classification_no_torchscript",
-        "inputs": ["examples/Huggingface_Transformers/Token_classification_artifacts/sample_text.txt"],
-        "handler":"custom"
-    },
-    {
-        "name": "bert_seqc_without_torchscript",
-        "inputs": ["examples/Huggingface_Transformers/Seq_classification_artifacts/sample_text.txt"],
-        "handler": "custom"
-    }
-]
-ts_log_file = os.path.join("logs", "ts_console.log")
-is_gpu_instance = ts.is_gpu_instance()
-
 
 def run_markdown_link_checker():
+    print("## Started markdown link checker")
     result = True
     for mdfile in glob.glob("**/*.md", recursive=True):
-        status = os.system(f"markdown-link-check {mdfile} --config link_check_config.json")
+        cmd = f"markdown-link-check {mdfile} --config link_check_config.json"
+        print(f"## In directory: {os.getcwd()} | Executing command: {cmd}")
+        status = os.system(cmd)
         if status != 0:
-            print(f"Broken links in {mdfile}")
+            print(f"## Broken links in file: {mdfile}")
             result = False
     return result
 
@@ -75,7 +22,7 @@ def run_markdown_link_checker():
 def validate_model_on_gpu():
     # A quick \ crude way of checking if model is loaded in GPU
     # Assumption is -
-    # 1. GPU on test setup is only utlizied by torchserve
+    # 1. GPUs on test setup are only utlizied by torchserve
     # 2. Models are successfully UNregistered between subsequent calls
     model_loaded = False
     for info in nvgpu.gpu_info():
@@ -85,49 +32,71 @@ def validate_model_on_gpu():
     return model_loaded
 
 
-os.makedirs("model_store", exist_ok=True)
-os.makedirs("logs", exist_ok=True)
+def test_sanity():
+    print("## Started sanity tests")
 
-if is_gpu_instance:
-    import torch
-    if not torch.cuda.is_available():
-      print("Ohh its NOT running on GPU!!")
-      sys.exit(1)
+    resnet18_model = { "name": "resnet-18", "inputs": ["examples/image_classifier/kitten.jpg"], "handler": "image_classifier"}
+    models_to_validate = [
+        {"name": "fastrcnn", "inputs": ["examples/object_detector/persons.jpg"], "handler": "object_detector"},
+        {"name": "fcn_resnet_101", "inputs": ["docs/images/blank_image.jpg", "examples/image_segmenter/fcn/persons.jpg"], "handler": "image_segmenter"},
+        {"name": "my_text_classifier_v2", "inputs": ["examples/text_classification/sample_text.txt"], "handler": "text_classification"},
+        resnet18_model,
+        {"name": "my_text_classifier_scripted_v2", "inputs": ["examples/text_classification/sample_text.txt"], "handler": "text_classification"},
+        {"name": "alexnet_scripted", "inputs": ["examples/image_classifier/kitten.jpg"], "handler": "image_classifier"},
+        {"name": "fcn_resnet_101_scripted", "inputs": ["examples/image_segmenter/fcn/persons.jpg"], "handler": "image_segmenter"},
+        {"name": "roberta_qa_no_torchscript", "inputs": ["examples/Huggingface_Transformers/QA_artifacts/sample_text.txt"], "handler": "custom"},
+        {"name": "bert_token_classification_no_torchscript", "inputs": ["examples/Huggingface_Transformers/Token_classification_artifacts/sample_text.txt"], "handler": "custom"},
+        {"name": "bert_seqc_without_torchscript", "inputs": ["examples/Huggingface_Transformers/Seq_classification_artifacts/sample_text.txt"], "handler": "custom"}
+    ]
+    ts_log_file = os.path.join("logs", "ts_console.log")
+    is_gpu_instance = ts.is_gpu_instance()
 
-ts.start_torchserve(log_file=ts_log_file)
-
-for model in models_to_validate:
-    model_name = model["name"]
-    model_inputs = model["inputs"]
-    model_handler = model["handler"]
-
-    ts.register_model(model_name)
-
-    for input in model_inputs:
-        ts.run_inference(model_name, input)
+    os.makedirs("model_store", exist_ok=True)
+    os.makedirs("logs", exist_ok=True)
 
     if is_gpu_instance:
-        if validate_model_on_gpu():
-            print(f"Model {model_name} successfully loaded on GPU")
-        else:
-            sys.exit(f"Something went wrong, model {model_name} did not load on GPU!!")
+        import torch
+        if not torch.cuda.is_available():
+          sys.exit("## Ohh its NOT running on GPU !")
 
-    #skip unregistering resnet-18 model to test snapshot feature with restart
-    if model != resnet18_model:
-        ts.unregister_model(model_name)
+    ts.start_torchserve(log_file=ts_log_file)
 
-    print(f"{model_handler} default handler is stable.")
+    for model in models_to_validate:
+        model_name = model["name"]
+        model_inputs = model["inputs"]
+        model_handler = model["handler"]
 
-ts.stop_torchserve()
+        ts.register_model(model_name)
 
-# Restarting torchserve
-# This should restart with the generated snapshot and resnet-18 model should be automatically registered
-ts.start_torchserve(log_file=ts_log_file)
+        for input in model_inputs:
+            ts.run_inference(model_name, input)
 
-ts.run_inference(resnet18_model["name"], resnet18_model["inputs"][0])
+        if is_gpu_instance:
+            if validate_model_on_gpu():
+                print(f"## Model {model_name} successfully loaded on GPU")
+            else:
+                sys.exit(f"## Something went wrong, model {model_name} did not load on GPU!!")
 
-ts.stop_torchserve()
+        #skip unregistering resnet-18 model to test snapshot feature with restart
+        if model != resnet18_model:
+            ts.unregister_model(model_name)
 
-links_ok = run_markdown_link_checker()
-if not links_ok:
-    sys.exit("Markdown Link Checker Failed")
+        print(f"## {model_handler} handler is stable.")
+
+    ts.stop_torchserve()
+
+    # Restarting torchserve
+    # This should restart with the generated snapshot and resnet-18 model should be automatically registered
+    ts.start_torchserve(log_file=ts_log_file)
+
+    ts.run_inference(resnet18_model["name"], resnet18_model["inputs"][0])
+
+    ts.stop_torchserve()
+
+    links_ok = run_markdown_link_checker()
+    if not links_ok:
+        sys.exit("## Markdown Link Checker Failed !")
+
+
+if __name__ == "__main__":
+    test_sanity()
