e9f025bcf204053ae9ca799106afaace2c4aef14
Prashant Sail <psail4444@gmail.com>
2020-09-24 14:07:44 +0530

    common conda install torchserve for windows and linux

--- .circleci/scripts/install.py
+++ .circleci/scripts/install.py
@@ -7,7 +7,8 @@ BASE_DIR = os.getcwd()
 IS_CONDA_ENV = True if os.system("conda") == 0 else False
 if IS_CONDA_ENV:
     print("Using conda to install torchserve and torch-model-archiver")
-    conda_cmd = f"conda install -c file://{BASE_DIR}/binaries/conda/output -y torchserve torch-model-archiver"
+    channel_dir = os.path.abspath(os.path.join(BASE_DIR, "binaries", "conda", "output"))
+    conda_cmd = f"conda install --channel {channel_dir} -y torchserve torch-model-archiver"
     INSTALL_EXIT_CODE = os.system(conda_cmd)
 else:
     print("Using pip to install torchserve and torch-model-archiver")
