ef216fec1f2af8614b73204a068769d3944ec536
Prashant Sail <psail4444@gmail.com>
2020-09-25 01:41:23 +0530

    print tweaks

--- .circleci/scripts/install.py
+++ .circleci/scripts/install.py
@@ -13,14 +13,14 @@ def install():
         print("## Using conda to install torchserve and torch-model-archiver")
         channel_dir = os.path.abspath(os.path.join(REPO_ROOT, "binaries", "conda", "output"))
         conda_cmd = f"conda install --channel {channel_dir} -y torchserve torch-model-archiver"
-        print(f"## Executing command: {conda_cmd}")
+        print(f"## In directory: {os.getcwd()} | Executing command: {conda_cmd}")
         install_exit_code = os.system(conda_cmd)
     else:
         print("## Using pip to install torchserve and torch-model-archiver")
         TS_WHEEL = glob.glob(os.path.join(REPO_ROOT, "dist", "*.whl"))[0]
         MA_WHEEL = glob.glob(os.path.join(REPO_ROOT, "model-archiver", "dist", "*.whl"))[0]
         pip_cmd = f"pip install {TS_WHEEL} {MA_WHEEL}"
-        print(f"## Executing command: {pip_cmd}")
+        print(f"## In directory: {os.getcwd()} | Executing command: {pip_cmd}")
         install_exit_code = os.system(pip_cmd)
 
     if install_exit_code != 0:
