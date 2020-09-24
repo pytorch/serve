78e8c12fcd2cee91b0bd0433f6c1b2fcc8cc2fc6
Abishek Chiffon <abishekchiffon@gmail.com>
2020-09-24 11:36:35 +0530

    Modified docker files for kfserving load unload

--- docker/dockerd-entrypoint_kf.sh
+++ docker/dockerd-entrypoint_kf.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" = *"model_dir"* ]] || [[ "$1" = *"model_name"* ]] || [[ "$1" = *"http_port"* ]] || [[ "$1" = *"grpc_port"* ]] || [[ "$1" = *"workers"* ]] || [[ "$1" = *"max_buffer_size"* ]]; then
-    #starts torchserve 
-    torchserve --start --ts-config /home/model-server/config.properties
-    #Wrapper class starts kfserver 
-    python /home/model-server/kfserving_wrapper/__main__.py $@
-
-else
     eval "$@"
-fi
-
-
-
+    python /serve/ts/kfserving_wrapper/__main__.py 
+    
 # prevent docker exit
 tail -f /dev/null
