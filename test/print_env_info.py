15040851fc762b99456d7cb7e6dba81734202fb2
harshbafna <harsh_bafna@persistent.co.in>
2020-10-14 13:47:43 +0530

    incorporated review comments and cleanup

--- test/print_env_info.py
+++ test/print_env_info.py
@@ -6,47 +6,50 @@ import re
 import subprocess
 import sys
 import os
-from collections import namedtuple
 
 try:
     import torch
+
     TORCH_AVAILABLE = True
 except (ImportError, NameError, AttributeError):
     TORCH_AVAILABLE = False
 
 torchserve_env = {
-    "torch" : "**Warning: torch not present ..",
-    "torch_model_archiver" : "**Warning: torch-model-archiver not installed ..",
-    "torchserve" : "**Warning: torchserve not installed ..",
-    "torchtext" : "**Warning: torchtext not present ..",
-    "torchvision" : "**Warning: torchvision not present ..",
-    "torchaudio" : "**Warning: torchaudio not present .."
+    "torch": "**Warning: torch not present ..",
+    "torch_model_archiver": "**Warning: torch-model-archiver not installed ..",
+    "torchserve": "**Warning: torchserve not installed ..",
+    "torchtext": "**Warning: torchtext not present ..",
+    "torchvision": "**Warning: torchvision not present ..",
+    "torchaudio": "**Warning: torchaudio not present .."
 }
 
 python_env = {
-    "python_version" : "N/A",
-    "pip_version" : "",
-    "pip_packages" : []
+    "python_version": "N/A",
+    "pip_version": "",
+    "pip_packages": []
 }
 
 java_env = {
-    "java_version" : []
+    "java_version": []
 }
 
 os_info = {
-    "os" : "",
-    "gcc_version" : "",
-    "clang_version" : "N/A",
-    "cmake_version" : "N/A"
+    "os": "",
+    "gcc_version": "",
+    "clang_version": "N/A",
+    "cmake_version": "N/A"
 }
 
 cuda_env = {
-    "is_cuda_available" : "No",
-    "cuda_runtime_version" : "N/A",
-    "nvidia_gpu_models" : [],
-    "nvidia_driver_version" : "N/A",
-    "cudnn_version" : []
+    "is_cuda_available": "No",
+    "cuda_runtime_version": "N/A",
+    "nvidia_gpu_models": [],
+    "nvidia_driver_version": "N/A",
+    "cudnn_version": []
 }
+
+smi = 'nvidia-smi'
+
 
 def run(command):
     """Returns (return-code, stdout, stderr)"""
@@ -59,14 +62,16 @@ def run(command):
     err = err.decode(enc)
     return rc, output.strip(), err.strip()
 
-def run_and_read_all(run, command):
+
+def run_and_read_all(command):
     """Reads and returns entire output if rc is 0"""
     rc, out, _ = run(command)
     if rc != 0:
         return "N/A"
     return out
 
-def run_and_parse_first_match(run, command, regex):
+
+def run_and_parse_first_match(command, regex):
     """Returns the first regex match if it exists"""
     rc, out, _ = run(command)
     if rc != 0:
@@ -76,25 +81,30 @@ def run_and_parse_first_match(run, command, regex):
         return "N/A"
     return match.group(1)
 
-def get_pip_packages(run, package_name=None):
+
+def get_pip_packages(package_name=None):
     """Returns `pip list` output. """
+
     # systems generally have `pip` as `pip` or `pip3`
     def run_with_pip(pip):
         if package_name == "torch":
             grep_cmd = 'grep "' + package_name + '"'
         else:
             grep_cmd = r'grep "numpy\|pytest\|pylint"'
-        return run_and_read_all(run, pip + ' list --format=freeze | ' + grep_cmd)
+        return run_and_read_all(pip + ' list --format=freeze | ' + grep_cmd)
+
     out = run_with_pip('pip3')
     if out == "N/A":
         out = None
     return 'pip3', out
 
-def get_java_version(run):
+
+def get_java_version():
     rc, out, _ = run("java --version")
     if rc != 0:
         return "**Warning: java not installed..."
     return out
+
 
 def get_platform():
     if sys.platform.startswith('linux'):
@@ -106,58 +116,66 @@ def get_platform():
     else:
         return sys.platform
 
-def get_mac_version(run):
-    return run_and_parse_first_match(run, 'sw_vers -productVersion', r'(.*)')
 
-def get_lsb_version(run):
-    return run_and_parse_first_match(run, 'lsb_release -a', r'Description:\t(.*)')
+def get_mac_version():
+    return run_and_parse_first_match('sw_vers -productVersion', r'(.*)')
 
-def check_release_file(run):
-    return run_and_parse_first_match(run, 'cat /etc/*-release', r'PRETTY_NAME="(.*)"')
 
-def get_os(run):
+def get_lsb_version():
+    return run_and_parse_first_match('lsb_release -a', r'Description:\t(.*)')
+
+
+def check_release_file():
+    return run_and_parse_first_match('cat /etc/*-release', r'PRETTY_NAME="(.*)"')
+
+
+def get_os():
     from platform import machine
     platform = get_platform()
     if platform == 'darwin':
-        version = get_mac_version(run)
+        version = get_mac_version()
         if version is None:
             return None
         return 'Mac OSX {} ({})'.format(version, machine())
     if platform == 'linux':
         # Ubuntu/Debian based
-        desc = get_lsb_version(run)
+        desc = get_lsb_version()
         if desc is not None:
             return desc
         # Try reading /etc/*-release
-        desc = check_release_file(run)
+        desc = check_release_file()
         if desc is not None:
             return desc
         return '{} ({})'.format(platform, machine())
     # Unknown platform
     return platform
 
-def get_gcc_version(run):
-    return run_and_parse_first_match(run, 'gcc --version', r'gcc (.*)')
 
-def get_clang_version(run):
-    return run_and_parse_first_match(run, 'clang --version', r'clang version (.*)')
+def get_gcc_version():
+    return run_and_parse_first_match('gcc --version', r'gcc (.*)')
 
-def get_cmake_version(run):
-    return run_and_parse_first_match(run, 'cmake --version', r'cmake (.*)')
 
-def get_nvidia_driver_version(run):
+def get_clang_version():
+    return run_and_parse_first_match('clang --version', r'clang version (.*)')
+
+
+def get_cmake_version():
+    return run_and_parse_first_match('cmake --version', r'cmake (.*)')
+
+
+def get_nvidia_driver_version():
     if get_platform() == 'darwin':
         cmd = 'kextstat | grep -i cuda'
-        return run_and_parse_first_match(run, cmd, r'com[.]nvidia[.]CUDA [(](.*?)[)]')
-    smi = get_nvidia_smi()
-    return run_and_parse_first_match(run, smi, r'Driver Version: (.*?) ')
+        return run_and_parse_first_match(cmd, r'com[.]nvidia[.]CUDA [(](.*?)[)]')
 
-def get_gpu_info(run):
+    return run_and_parse_first_match(smi, r'Driver Version: (.*?) ')
+
+
+def get_gpu_info():
     if get_platform() == 'darwin':
         if TORCH_AVAILABLE and torch.cuda.is_available():
             return torch.cuda.get_device_name(None)
         return None
-    smi = get_nvidia_smi()
     uuid_regex = re.compile(r' \(UUID: .+?\)')
     rc, out, _ = run(smi + ' -L')
     if rc != 0:
@@ -165,10 +183,12 @@ def get_gpu_info(run):
     # Anonymize GPUs by removing their UUID
     return "\n" + re.sub(uuid_regex, '', out)
 
-def get_running_cuda_version(run):
-    return run_and_parse_first_match(run, 'nvcc --version', r'V(.*)$')
 
-def get_cudnn_version(run):
+def get_running_cuda_version():
+    return run_and_parse_first_match('nvcc --version', r'V(.*)$')
+
+
+def get_cudnn_version():
     """This will return a list of libcudnn.so; it's hard to tell which one is being used"""
     if get_platform() == 'darwin':
         # CUDA libraries and drivers can be found in /usr/local/cuda/. See
@@ -196,21 +216,20 @@ def get_cudnn_version(run):
     result = '\n'.join(files)
     return 'Probably one of the following:\n{}'.format(result)
 
-def get_nvidia_smi():
-    smi = 'nvidia-smi'
-    return smi
 
 def get_torchserve_version():
-    #fetch the torchserve version from version.txt file
-    with open("ts/version.txt", 'r') as file:
+    # fetch the torchserve version from version.txt file
+    with open(f"{os.getcwd()}/../ts/version.txt", 'r') as file:
         version = file.readline().rstrip()
     return version
 
+
 def get_torch_model_archiver():
-    #fetch the torch-model-archiver version from version.txt file  
-    with open("model-archiver/model_archiver/version.txt", 'r') as file:
+    # fetch the torch-model-archiver version from version.txt file
+    with open(f"{os.getcwd()}/../model-archiver/model_archiver/version.txt", 'r') as file:
         version = file.readline().rstrip()
     return version
+
 
 def populate_torchserve_env(torch_pkg):
     for pkg in torch_pkg:
@@ -226,52 +245,59 @@ def populate_torchserve_env(torch_pkg):
     torchserve_env["torchserve"] = "torchserve==" + get_torchserve_version()
     torchserve_env["torch_model_archiver"] = "torch-model-archiver==" + get_torch_model_archiver()
 
+
 def populate_python_env(pip_version, pip_list_output):
-    python_env["python_version"] = '{}.{} ({}-bit runtime)'.format(sys.version_info[0], sys.version_info[1], sys.maxsize.bit_length() + 1)
+    python_env["python_version"] = f'{sys.version_info[0]}.{sys.version_info[1]} ' \
+                                   f'({sys.maxsize.bit_length() + 1}-bit runtime)'
     python_env["pip_version"] = pip_version
     python_env["pip_packages"] = pip_list_output
 
+
 def populate_java_env():
-    java_env["java_version"] = get_java_version(run)
+    java_env["java_version"] = get_java_version()
+
 
 def populate_os_env():
-    os_info["os"] = get_os(run)
-    os_info["gcc_version"] = get_gcc_version(run)
-    os_info["clang_version"] = get_clang_version(run)
-    os_info["cmake_version"] = get_cmake_version(run)
+    os_info["os"] = get_os()
+    os_info["gcc_version"] = get_gcc_version()
+    os_info["clang_version"] = get_clang_version()
+    os_info["cmake_version"] = get_cmake_version()
 
-def populate_cuda_env(cuda_available_str):    
+
+def populate_cuda_env(cuda_available_str):
     cuda_env["is_cuda_available"] = cuda_available_str
-    cuda_env["cuda_runtime_version"] = get_running_cuda_version(run)
-    cuda_env["nvidia_gpu_models"] = get_gpu_info(run)
-    cuda_env["nvidia_driver_version"] = get_nvidia_driver_version(run)
-    cuda_env["cudnn_version"] = get_cudnn_version(run)
+    cuda_env["cuda_runtime_version"] = get_running_cuda_version()
+    cuda_env["nvidia_gpu_models"] = get_gpu_info()
+    cuda_env["nvidia_driver_version"] = get_nvidia_driver_version()
+    cuda_env["cudnn_version"] = get_cudnn_version()
+
 
 def populate_env_info():
-    #torchserve packages
-    _, torch_list_output = get_pip_packages(run,"torch")
+    # torchserve packages
+    _, torch_list_output = get_pip_packages("torch")
     if torch_list_output is not None:
         populate_torchserve_env(torch_list_output.split("\n"))
 
-    #python packages
-    pip_version, pip_list_output = get_pip_packages(run)
+    # python packages
+    pip_version, pip_list_output = get_pip_packages()
     populate_python_env(pip_version, pip_list_output)
-    
-    #java environment
+
+    # java environment
     populate_java_env()
 
-    #OS environment
+    # OS environment
     populate_os_env()
 
-    #cuda environment
+    # cuda environment
     if TORCH_AVAILABLE and torch.cuda.is_available():
         populate_cuda_env("Yes")
+
 
 env_info_fmt = """
 ------------------------------------------------------------------------------------------
 Environment headers
 ------------------------------------------------------------------------------------------
-Torchserve branch : {torchserve_branch}
+Torchserve branch: {torchserve_branch}
 
 {torchserve}
 {torch_model_archiver}
@@ -302,12 +328,12 @@ Nvidia driver version: {nvidia_driver_version}
 cuDNN version: {cudnn_version}
 """
 
-def get_pretty_env_info(torchserve_branch):
+
+def get_pretty_env_info(branch_name):
     global env_info_fmt
     global cuda_info_fmt
     populate_env_info()
-    env_dict = {**torchserve_env, **python_env, **java_env, **os_info}
-    env_dict["torchserve_branch"] = torchserve_branch
+    env_dict = {**torchserve_env, **python_env, **java_env, **os_info, "torchserve_branch": branch_name}
 
     if TORCH_AVAILABLE and torch.cuda.is_available():
         env_dict.update(cuda_env)
@@ -315,9 +341,11 @@ def get_pretty_env_info(torchserve_branch):
 
     return env_info_fmt.format(**env_dict)
 
-def main(torchserve_branch):
-    output = get_pretty_env_info(torchserve_branch)
+
+def main(branch_name):
+    output = get_pretty_env_info(branch_name)
     print(output)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
