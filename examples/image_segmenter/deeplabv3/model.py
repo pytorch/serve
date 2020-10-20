d7b2627eb846b24ce4b792ad47bcce63f9cd8625
alvarobartt <alvarob96@usal.es>
2020-10-20 12:50:06 +0200

    included FCNHead on DeepLabV3 pre-trained model

--- examples/image_segmenter/deeplabv3/model.py
+++ examples/image_segmenter/deeplabv3/model.py
@@ -2,6 +2,7 @@ from torchvision.models.segmentation.deeplabv3 import DeepLabV3
 from torchvision.models import resnet
 from intermediate_layer_getter import IntermediateLayerGetter
 from deeplabv3 import DeepLabHead
+from fcn import FCNHead
 
 
 class DeepLabV3ImageSegmenter(DeepLabV3):
@@ -13,9 +14,12 @@ class DeepLabV3ImageSegmenter(DeepLabV3):
         backbone = resnet.resnet101(pretrained=True,
                                     replace_stride_with_dilation=[False, True, True])
         return_layers = {'layer4': 'out'}
+        return_layers['layer3'] = 'aux'
         backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
 
+        inplanes = 1024
+        aux_classifier = FCNHead(inplanes, num_classes)
         inplanes = 2048
         classifier = DeepLabHead(inplanes, num_classes)
 
-        super(DeepLabV3ImageSegmenter, self).__init__(backbone, classifier)
+        super(DeepLabV3ImageSegmenter, self).__init__(backbone, classifier, aux_classifier)
