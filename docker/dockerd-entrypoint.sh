bfb4a3d08b6cd3836cc957c896abb324657b131f
Abishek Chiffon <abishekchiffon@gmail.com>
2020-10-28 20:23:34 +0530

    Moved the unified KF and TS dockerfile to docker folder

--- docker/dockerd-entrypoint.sh
+++ docker/dockerd-entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 if [[ "$1" = "serve" ]]; then
     shift 1
     torchserve --start --ts-config /home/model-server/config.properties
+elif [["$1" = "kfserve"]]; then
+    shift 1
+    eval "$@"
+    python /serve/kubernetes/kfserving_wrapper/__main__.py 
 else
     eval "$@"
 fi
