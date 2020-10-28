37b47b9c0f6bb677d74222c443d6b7510ae29f1d
Abishek Chiffon <abishekchiffon@gmail.com>
2020-10-28 20:17:22 +0530

    Unified dockerfile for kfserving and torchserve

--- kubernetes/kf_predictor_docker/dockerd-entrypoint_kf.sh
+++ kubernetes/kf_predictor_docker/dockerd-entrypoint_kf.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
+if [[ "$1" = "serve" ]]; then
+    shift 1
+    torchserve --start --ts-config /home/model-server/config.properties
+elif [["$1" = "kfserve"]]; then
+    shift 1
     eval "$@"
     python /serve/kubernetes/kfserving_wrapper/__main__.py 
-    
+else
+    eval "$@"
+fi
+
 # prevent docker exit
 tail -f /dev/null
