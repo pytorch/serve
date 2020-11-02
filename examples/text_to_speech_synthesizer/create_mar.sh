0bbe4bfcc18fec498950f44feeec5105b60fb958
Álvaro Bartolomé <alvarob96@usal.es>
2020-11-02 18:02:52 +0100

    included waveglow requirements before creating MAR

--- examples/text_to_speech_synthesizer/create_mar.sh
+++ examples/text_to_speech_synthesizer/create_mar.sh
@@ -14,5 +14,6 @@ cp -r /tmp/DeepLearningExamples-torchhub/PyTorch/SpeechSynthesis/* PyTorch/Speec
 zip -r tacotron.zip PyTorch
 wget https://api.ngc.nvidia.com/v2/models/nvidia/tacotron2_pyt_ckpt_fp32/versions/19.09.0/files/nvidia_tacotron2pyt_fp32_20190427 -O nvidia_tacotron2pyt_fp32_20190427.pth
 wget https://api.ngc.nvidia.com/v2/models/nvidia/waveglow_ckpt_fp32/versions/19.09.0/files/nvidia_waveglowpyt_fp32_20190427 -O nvidia_waveglowpyt_fp32_20190427.pth
+pip install numpy scipy librosa unidecode inflect librosa
 torch-model-archiver --model-name waveglow_synthesizer --version 1.0 --model-file waveglow_model.py --serialized-file nvidia_waveglowpyt_fp32_20190427.pth --handler waveglow_handler.py --extra-files tacotron.zip,nvidia_tacotron2pyt_fp32_20190427.pth
 rm -rf nvidia_*
