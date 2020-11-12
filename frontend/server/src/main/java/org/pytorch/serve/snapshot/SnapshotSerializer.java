3d9812fd3a9b47e69c995f515bb16048dd457900
dhaniram-kshirsagar <26479924+dhaniram-kshirsagar@users.noreply.github.com>
2020-11-13 01:34:51 +0530

    Update SnapshotSerializer.java

--- frontend/server/src/main/java/org/pytorch/serve/snapshot/SnapshotSerializer.java
+++ frontend/server/src/main/java/org/pytorch/serve/snapshot/SnapshotSerializer.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public interface SnapshotSerializer {
 
-    void saveSnapshot(Snapshot snapshot) throws IOException;
+    public void saveSnapshot(Snapshot snapshot) throws IOException;
 
-    Snapshot getSnapshot(String modelSnapshot) throws IOException;
+    public Snapshot getSnapshot(String modelSnapshot) throws IOException;
 }
