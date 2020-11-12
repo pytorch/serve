66b65e389441b2e96cb852fa615ab2370a81996a
dhaniram kshirsagar <dhaniram_kshirsagar@persistent.com>
2020-11-13 01:31:30 +0530

    Restoring snapshot-2

--- frontend/server/src/main/java/org/pytorch/serve/snapshot/SnapshotSerializerFactory.java
+++ frontend/server/src/main/java/org/pytorch/serve/snapshot/SnapshotSerializerFactory.java
@@ -1,7 +1,5 @@
 package org.pytorch.serve.snapshot;
 
-import org.pytorch.serve.servingsdk.snapshot.SnapshotSerializer;
-
 public final class SnapshotSerializerFactory {
 
     private SnapshotSerializerFactory() {}
