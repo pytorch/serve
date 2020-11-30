ad67539c565ea747f5c9d65e06d09aa7b65cf0d7
Abishek Chiffon <abishekchiffon@gmail.com>
2020-11-30 22:09:09 +0530

    Modified the comment in listinferenceapi

--- frontend/server/src/main/java/org/pytorch/serve/openapi/OpenApiUtils.java
+++ frontend/server/src/main/java/org/pytorch/serve/openapi/OpenApiUtils.java
@@ -35,9 +35,10 @@ public final class OpenApiUtils {
     }
 
     private static void listInferenceApis(OpenApi openApi) {
+        /* The /v1/models/{model_name}:predict prediction api is used to 
+        access torchserve from kfserving v1 predictor  */
         openApi.addPath("/", getApiDescriptionPath("apiDescription", false));
         openApi.addPath("/ping", getPingPath());
-        // The prediction api to access torchserve from kfserving v1 predictor 
         openApi.addPath("/v1/models/{model_name}:predict", getPredictionsPath(false));
         openApi.addPath("/predictions/{model_name}", getPredictionsPath(false));
         openApi.addPath("/predictions/{model_name}/{model_version}", getPredictionsPath(true));
