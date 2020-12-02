afce4f67200317d739d88090f414f7aa3c4fd9e0
Mahesh Ambule <ambulemahesh@gmail.com>
2020-12-02 12:28:40 +0530

    Support multi output

--- frontend/server/src/main/java/org/pytorch/serve/workflow/WorkflowManager.java
+++ frontend/server/src/main/java/org/pytorch/serve/workflow/WorkflowManager.java
@@ -2,7 +2,12 @@ package org.pytorch.serve.workflow;
 
 import com.google.gson.JsonObject;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.nio.charset.StandardCharsets;
@@ -247,16 +252,32 @@ public final class WorkflowManager {
             predictionFuture
                     .thenApplyAsync(
                             (predictions) -> {
-                                JsonObject result = new JsonObject();
-                                for (NodeOutput prediction : predictions) {
-                                    String val =
-                                            new String(
-                                                    (byte[]) prediction.getData(),
-                                                    StandardCharsets.UTF_8);
-                                    result.addProperty(prediction.getNodeName(), val);
-                                }
                                 if (!predictions.isEmpty()) {
-                                    NettyUtils.sendJsonResponse(ctx, result);
+                                    if (predictions.size() == 1) {
+                                        FullHttpResponse resp =
+                                                new DefaultFullHttpResponse(
+                                                        HttpVersion.HTTP_1_1,
+                                                        HttpResponseStatus.OK,
+                                                        false);
+                                        resp.headers()
+                                                .set(
+                                                        HttpHeaderNames.CONTENT_TYPE,
+                                                        HttpHeaderValues.APPLICATION_JSON);
+                                        resp.content()
+                                                .writeBytes((byte[]) predictions.get(0).getData());
+                                        NettyUtils.sendHttpResponse(ctx, resp, true);
+
+                                    } else {
+                                        JsonObject result = new JsonObject();
+                                        for (NodeOutput prediction : predictions) {
+                                            String val =
+                                                    new String(
+                                                            (byte[]) prediction.getData(),
+                                                            StandardCharsets.UTF_8);
+                                            result.addProperty(prediction.getNodeName(), val);
+                                        }
+                                        NettyUtils.sendJsonResponse(ctx, result);
+                                    }
                                 } else {
                                     throw new InternalServerException(
                                             "Workflow inference request failed!");
