fa76f1ee6e3ab593fc86ee4381302b19cde13bf8
Aaqib Ansari <maaquib@gmail.com>
2020-12-16 15:31:17 -0800

    Update cpu requirements for windows too

--- ts_scripts/install_dependencies.py
+++ ts_scripts/install_dependencies.py
@@ -72,6 +72,8 @@ class Windows(Common):
         self.sudo_cmd = ''
 
     def install_torch_packages(self, cuda_version):
+        if cuda_version is None:
+            os.system(f"pip install -U -r requirements/torch_cpu.txt -f {self.torch_stable_url}"
         if cuda_version and cuda_version != "latest":
             os.system(f"pip install -U -r requirements/torch_{cuda_version}.txt -f {self.torch_stable_url}")
         else:
