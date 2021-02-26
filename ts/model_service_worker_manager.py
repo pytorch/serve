a2f7f47955a3e1104aa88822a1319112d76fc50d
Dhanasekar <dhanak@amazon.com>
2021-02-26 08:39:37 +0000

    ts/model_service_worker_manager.py

--- ts/model_service_worker_manager.py
+++ ts/model_service_worker_manager.py
@@ -157,6 +157,7 @@ class TorchModelServiceWorkerManager(object):
         try:
             port = scale_down_request["port"].decode("utf-8")
             worker = self.workers[port]["worker"]
+            fifo_path = self.workers[port]["fifo"]
             worker.terminate()
             def delete_fifo(file_name):
                 os.remove(file_name) if os.path.exists(file_name) else None
