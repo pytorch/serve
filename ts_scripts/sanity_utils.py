92590ce929b53e3972e84ff33cd66856482b5520
Dhanasekar <dhanak@amazon.com>
2021-02-26 09:24:41 +0000

    ts_scripts/sanity_utils.py

--- ts_scripts/sanity_utils.py
+++ ts_scripts/sanity_utils.py
@@ -62,10 +62,15 @@ def test_sanity():
         {"name": "fcn_resnet_101_scripted", "inputs": ["examples/image_segmenter/fcn/persons.jpg"],
          "handler": "image_segmenter"},
         {"name": "roberta_qa_no_torchscript",
-         "inputs": ["examples/Huggingface_Transformers/QA_artifacts/sample_text.txt"], "handler": "custom"}
+         "inputs": ["examples/Huggingface_Transformers/QA_artifacts/sample_text.txt"], "handler": "custom"},
+        {"name": "bert_token_classification_no_torchscript",
+         "inputs": ["examples/Huggingface_Transformers/Token_classification_artifacts/sample_text.txt"],
+         "handler": "custom"},
+        {"name": "bert_seqc_without_torchscript",
+         "inputs": ["examples/Huggingface_Transformers/Seq_classification_artifacts/sample_text.txt"],
+         "handler": "custom"}
     ]
     ts_log_file = os.path.join("logs", "ts_console.log")
-    ts_log_file = None
     is_gpu_instance = utils.is_gpu_instance()
 
     os.makedirs("model_store", exist_ok=True)
