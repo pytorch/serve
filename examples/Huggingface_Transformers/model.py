f0f6f51511652a418b45ed93f6de4c43d680e396
Dhanasekar <dhanak@amazon.com>
2021-03-14 12:34:40 +0000

    Fix Transformers EagerModel models

---
+++ examples/Huggingface_Transformers/model.py
@@ -0,0 +1,6 @@
+
+class PlaceHolder:
+    # TODO : Refactor huggingface transformer example to use TS contract instead of manipulating model directly
+    def __init__(self, **kwargs):
+        pass
+
