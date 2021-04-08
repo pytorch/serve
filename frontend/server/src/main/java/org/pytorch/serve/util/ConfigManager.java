5442db66ee886f65a3b37d0f4f35e5896368818f
lxning <lninga@amazon.com>
2021-04-07 18:51:18 -0700

    fmt

--- frontend/server/src/main/java/org/pytorch/serve/util/ConfigManager.java
+++ frontend/server/src/main/java/org/pytorch/serve/util/ConfigManager.java
@@ -732,7 +732,7 @@ public final class ConfigManager {
     }
 
     public boolean isS3SseKmsEnabled() {
-        return prop.getProperty(S3_SSE_KMS_ENABLED, false);
+        return Boolean.getBoolean(prop.getProperty(S3_SSE_KMS_ENABLED, "false"));
     }
 
     public static final class Arguments {
