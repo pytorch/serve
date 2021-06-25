b74039ee383840410fa2bc4cf1ee211456bae196
Nikhil Kulkarni <nikhilsk@amazon.com>
2021-06-25 10:09:34 -0700

    Fix unused import

--- frontend/server/src/main/java/org/pytorch/serve/wlm/ModelManager.java
+++ frontend/server/src/main/java/org/pytorch/serve/wlm/ModelManager.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonObject;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.nio.file.Path;
