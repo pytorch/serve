7017598d32a7e3a68d3dca70579896b8d0ff10df
jagadeesh <jagadeeshj@ideas2it.com>
2021-08-05 00:33:08 +0530

    fix: kfservingv2 service envelope

--- ts/torch_handler/request_envelope/kfservingv2.py
+++ ts/torch_handler/request_envelope/kfservingv2.py
@@ -3,6 +3,7 @@ The KFServing Envelope is used to handle the KFServing
 Input Request inside Torchserve.
 """
 import json
+import numpy as np
 import logging
 from .base import BaseEnvelope
 
@@ -10,55 +11,55 @@ logger = logging.getLogger(__name__)
 
 
 class KFservingv2Envelope(BaseEnvelope):
+  """
+  This function is used to handle the input request specified in KFServing
+  format and converts it into a Torchserve readable format.
+
+  Args:
+      data - List of Input Request in KFServing Format
+  Returns:
+      [list]: Returns the list of the Input Request in Torchserve Format
+  """
+
+  def parse_input(self, data):
+    logger.info("Parsing input in KFServingv2.py")
+    self._data_list = [row.get("data") or row.get("body") for row in data]
+    self._data_list = [row for row in self._data_list[0].get("inputs")]
+    # selecting the first input from the list torchserve creates
+    logger.info("Parse input data_list %s", self._data_list)
+   
+    # If the KF Transformer and Explainer sends in data as bytesarray
+    # if isinstance(data, (bytes, bytearray)):
+
+    #     data = data.decode()
+    #     data = json.loads(data)
+    #     logger.info("Bytes array is %s", data)
+
+    self._inputs = self._data_list
+    logger.info("KFServingv2 parsed inputs %s", self._inputs)
+    return self._inputs
+
+  def format_output(self, data):
     """
-    This function is used to handle the input request specified in KFServing
-    format and converts it into a Torchserve readable format.
+    Returns the prediction response and captum explanation response of the input request.
 
     Args:
-        data - List of Input Request in KFServing Format
+        outputs (List): The outputs arguments is in the form of a list of dictionaries.
+
     Returns:
-        [list]: Returns the list of the Input Request in Torchserve Format
+        (list): The response is returned as a list of predictions and explanations
     """
 
-    def parse_input(self, data):
-        logger.info("Parsing input in KFServing.py")
-        self._data_list = [row.get("data") or row.get("body") for row in data]
-        # selecting the first input from the list torchserve creates
-        logger.info("Parse input data_list %s", self._data_list)
-        data = self._data_list[0]
+    response = {}
+    output = {}
 
-        # If the KF Transformer and Explainer sends in data as bytesarray
-        if isinstance(data, (bytes, bytearray)):
-
-            data = data.decode()
-            data = json.loads(data)
-            logger.info("Bytes array is %s", data)
-
-        self._inputs = data.get("instances")
-        logger.info("KFServing parsed inputs %s", self._inputs)
-        return self._inputs
-
-    def format_output(self, data):
-        """
-        Returns the prediction response and captum explanation response of the input request.
-
-        Args:
-            outputs (List): The outputs arguments is in the form of a list of dictionaries.
-
-        Returns:
-            (list): The response is returned as a list of predictions and explanations
-        """
-        response = {}
-        logger.info("The Response of KFServing %s", data)
-        if not self._is_explain():
-            response["predictions"] = data
-        else:
-            response["explanations"] = data
-        return [response]
-
-    def _is_explain(self):
-        if self.context and self.context.get_request_header(0, "explain"):
-            if self.context.get_request_header(0, "explain") == "True":
-                return True
-
-        return False
+    logger.info("The Response of KFServingv2 %s", data)
+    response["model_name"] = self.context.manifest.get("model").get("modelName")
+    response["model_version"] = self.context.manifest.get("model").get("modelVersion")
+    response["id"] = self.context.get_request_id(0)
+    output["name"] = "explain" if self.context.get_request_header(0, "explain") == "True" else "predict"
+    output["shape"] = np.array(data).shape
+    output["datatype"] = str(np.array(data).dtype)
+    output["data"] = data
+    response["outputs"] = [output]
+    return [response]
