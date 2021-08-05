0033d09088357a17defb81533d80abba28c69113
jagadeesh <jagadeeshj@ideas2it.com>
2021-08-05 22:59:42 +0530

    feature: Add datatype formatting for kfservingv2 envelope

--- ts/torch_handler/request_envelope/kfservingv2.py
+++ ts/torch_handler/request_envelope/kfservingv2.py
@@ -9,6 +9,38 @@ from .base import BaseEnvelope
 
 logger = logging.getLogger(__name__)
 
+_DatatypeToNumpy = {
+    "BOOL": "bool",
+    "UINT8": "uint8",
+    "UINT16": "uint16",
+    "UINT32": "uint32",
+    "UINT64": "uint64",
+    "INT8": "int8",
+    "INT16": "int16",
+    "INT32": "int32",
+    "INT64": "int64",
+    "FP16": "float16",
+    "FP32": "float32",
+    "FP64": "float64",
+    "BYTES": "byte",
+}
+
+_NumpyToDatatype = {value: key for key, value in _DatatypeToNumpy.items()}
+
+# NOTE: numpy has more types than v2 protocol
+_NumpyToDatatype["object"] = "BYTES"
+
+
+def _to_dtype(datatype: str) -> "np.dtype":
+    dtype = _DatatypeToNumpy[datatype]
+    return np.dtype(dtype)
+
+
+def _to_datatype(dtype: np.dtype) -> str:
+    as_str = str(dtype)
+    datatype = _NumpyToDatatype[as_str]
+
+    return datatype
 
 class KFservingv2Envelope(BaseEnvelope):
   """
@@ -59,7 +91,7 @@ class KFservingv2Envelope(BaseEnvelope):
     response["id"] = self.context.get_request_id(0)
     output["name"] = "explain" if self.context.get_request_header(0, "explain") == "True" else "predict"
     output["shape"] = np.array(data).shape
-    output["datatype"] = str(np.array(data).dtype)
+    output["datatype"] = _to_datatype(np.array(data).dtype)
     output["data"] = data
     response["outputs"] = [output]
     return [response]
