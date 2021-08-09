46587321f56898e00eb7b3ef6503f0bfca3006e2
jagadeesh <jagadeeshj@ideas2it.com>
2021-08-10 02:29:16 +0530

    Fix: Add batch input support to kfservingv2 envelope

--- ts/torch_handler/request_envelope/kfservingv2.py
+++ ts/torch_handler/request_envelope/kfservingv2.py
@@ -43,55 +43,62 @@ def _to_datatype(dtype: np.dtype) -> str:
     return datatype
 
 class KFservingv2Envelope(BaseEnvelope):
-  """
-  This function is used to handle the input request specified in KFServing
-  format and converts it into a Torchserve readable format.
+    def parse_input(self, data):
+        logger.info("Parsing input in KFServingv2.py")
+        inputs = self._batch_from_json(data)
+    
+        # If the KF Transformer and Explainer sends in data as bytesarray
+        if isinstance(data, (bytes, bytearray)):
 
-  Args:
-      data - List of Input Request in KFServing Format
-  Returns:
-      [list]: Returns the list of the Input Request in Torchserve Format
-  """
+            data = data.decode()
+            data = json.loads(data)
+            logger.info("Bytes array is %s", data)
 
-  def parse_input(self, data):
-    logger.info("Parsing input in KFServingv2.py")
-    self._data_list = [row.get("data") or row.get("body") for row in data]
-    self._data_list = [row for row in self._data_list[0].get("inputs")]
-    # selecting the first input from the list torchserve creates
-    logger.info("Parse input data_list %s", self._data_list)
-   
-    # If the KF Transformer and Explainer sends in data as bytesarray
-    # if isinstance(data, (bytes, bytearray)):
+        logger.info("KFServingv2 parsed inputs %s", inputs)
+        return inputs
 
-    #     data = data.decode()
-    #     data = json.loads(data)
-    #     logger.info("Bytes array is %s", data)
+    def _batch_from_json(self, rows):
+        """
+        Joins the instances of a batch of JSON objects
+        """
+        logger.info("Parse input data %s", rows)
+        body_list = rows[0].get("body")
+        data_list = self._from_json(body_list)
+        return data_list
 
-    self._inputs = self._data_list
-    logger.info("KFServingv2 parsed inputs %s", self._inputs)
-    return self._inputs
+    def _from_json(self, body_list):
+        """
+        Extracts the data from the JSON object
+        """
+        data_list = [row for row in body_list.get("inputs")]
+        return data_list
 
-  def format_output(self, data):
-    """
-    Returns the prediction response and captum explanation response of the input request.
+    def format_output(self, data):
+        logger.info("The Response of KFServingv2 %s", data)
+        response = {}
+        response["id"] = self.context.get_request_id(0)
+        response["model_name"] = self.context.manifest.get("model").get("modelName")
+        response["model_version"] = self.context.manifest.get("model").get("modelVersion")
+        response["outputs"] = self._batch_to_json(data)
+        return [response]
 
-    Args:
-        outputs (List): The outputs arguments is in the form of a list of dictionaries.
+    def _batch_to_json(self, data):
+        """
+        Splits batch output to json objects
+        """
+        output = []
+        for item in data:
+            output.append(self._to_json(item))
+        return output
 
-    Returns:
-        (list): The response is returned as a list of predictions and explanations
-    """
-
-    response = {}
-    output = {}
-
-    logger.info("The Response of KFServingv2 %s", data)
-    response["model_name"] = self.context.manifest.get("model").get("modelName")
-    response["model_version"] = self.context.manifest.get("model").get("modelVersion")
-    response["id"] = self.context.get_request_id(0)
-    output["name"] = "explain" if self.context.get_request_header(0, "explain") == "True" else "predict"
-    output["shape"] = np.array(data).shape
-    output["datatype"] = _to_datatype(np.array(data).dtype)
-    output["data"] = data
-    response["outputs"] = [output]
-    return [response]
+    def _to_json(self, data):
+        """
+        Constructs JSON object from data
+        """
+        output_data = {}
+        data_ndarray = np.ndarray(data)
+        output_data["name"] = "explain" if self.context.get_request_header(0, "explain") == "True" else "predict"
+        output_data["shape"] = data_ndarray.shape
+        output_data["datatype"] = _to_datatype(data_ndarray.dtype)
+        output_data["data"] = data_ndarray.flatten().tolist()
+        return output_data
