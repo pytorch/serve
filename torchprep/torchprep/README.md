9cc192fd16ced509fbcb1da1a4a73ab3443f2d3b
Mark Saroufim <marksaroufim@gmail.com>
2021-10-08 21:16:09 +0000

    first working prototype for quantization

--- torchprep/torchprep/README.md
+++ torchprep/torchprep/README.md
@@ -1,6 +1,6 @@
 # Torchprep
 
-A CLI tool to prepare your Pytorch models for inference
+A CLI tool to prepare your Pytorch models for efficient inference. The only prerequisite is a model trained and saved with `torch.save(model_name, model_path)`. See `example.py` for an example.
 
 ## Install from source
 
@@ -12,14 +12,21 @@ poetry install
 
 ## Install from Pypi
 
-```
+```sh
 pip install torchprep
 ```
 
 ## Usage
 
+```sh
+torchprep quantize --help
 ```
-torchprep
+
+### Example
+
+```sh
+python example.py
+torchprep quantize models/resnet152.pt int8 --profile 64,3,7,7
 ```
 
 ## Uploading to Pypi
@@ -28,28 +35,18 @@ torchprep
 
 To create binaries and test them out locally
 
-```
+```sh
 poetry build
 pip install --user /path/to/wheel
 ```
 
 ### Upload to Pypi
 
-```
+```sh
 poetry config pypi-token.pypi <SECRET_KEY>
 poetry publish --build
 ```
 
-
-
-## Usage
-* Float16 precision on GPU `python main.py quantize --precision float16 --device gpu`
-* Float16 quantization on CPU `python main.py quantize --float16`
-* Int8 quantization on CPU profiled with a random matrix of size 2x2: `python main.py quantize --precision int8 --profile [2,2]`
-
 ## Coming soon
-* Reduce parameter count by 1/3 `python main.py distil model.pt -distil 1/3`
-* And much more
-* pip installation instructions
-* Baseline models resnet18 and BERT 
+* Reduce parameter count by 1/3 `torchprep distill model.pt 1/3`
 * Automated release with github actions
--- torchprep/torchprep/torchprep/main.py
+++ torchprep/torchprep/torchprep/main.py
@@ -3,74 +3,98 @@ import torch
 from typing import List
 import time
 import os
+from enum import Enum
+from pathlib import Path
+import math
 
 
 app = typer.Typer()
 
+class Precision(Enum):
+    int8 = "int8"
+    float16 = "float16"
+
+class Device(str, Enum):
+    cpu = "cpu"
+    gpu = "gpu"
+
 
 @app.command()
-def hello():
+def hello() -> None:
     typer.echo(f"Hello torchprep")
 
+@app.command()
+def distill(model_path : Path, device : Device, parameter_scaling : int, layer_scaling : int = None, profile : List[int] = None) -> None:
+    """
+    Create a smaller student model by setting a distillation ratio and teach it how to behave exactly like your existing model
+    """
+    typer.echo(f"Coming soon")
+    typer.echo("See this notebook for more information https://colab.research.google.com/drive/1RzQtprrHx8PokLQsFiQPAKzfn_DiTpDN?usp=sharing")
 
 @app.command()
-def quantize(model_path : str, precision : str , device : str, profile : List[int]):
+def quantize(model_path : Path, precision : Precision , device : Device = Device.cpu, profile : str = typer.Option(default=None, help="Comma seperated input tensor shape")) -> None:
+    # TODO: define model output path
+    """
+    Quantize a saved torch model to a lower precision float format to reduce its size and latency
+    """
     model = load_model(model_path, device)
 
-    if device == "cpu":
+    if device == Device.cpu:
         if precision == "int8":
             dtype = torch.qint8
         else:
             dtype = torch.float16
     quantized_model = torch.quantization.quantize_dynamic(
-    model, {torch.nn.LSTM, torch.nn.Linear, torch.nn.Conv2D}, dtype=dtype
+    model, {torch.nn.LSTM, torch.nn.Linear, torch.nn.Conv2d}, dtype=dtype
 )
-
-        
-    if device == "gpu":
+    # TODO: Add AMP
+    if device == Device.gpu:
+        if precision == Precision.int8:
+            print("int8 precision is not suported for GPUs, defaulting to float16")
         quantized_model = model.half()
-        # Save model
     
-    print("Model was quantized")
+    print("Model succesfully quantized")
 
     print_size_of_model(model, label = "base model")
     print_size_of_model(quantized_model, label = "quantized_model")
     
     if profile:
-        input_tensor = torch.randn(**profile)
-        profile(model, input_tensor, label = "base model")
-        profile(quantized_model, input_tensor, label = "quantized_model")
+        profile = map(int,profile.split(','))
+        input_tensor = torch.randn(*profile)
+        profile_model(model, input_tensor, label = "base model")
+        profile_model(quantized_model, input_tensor, label = "quantized_model")
+    
+    torch.save(quantized_model, 'quantized_model.pt')
+    print(f"model quantized_model.pt was saved")
 
 
-def profile(model, input_tensor, label="model"):
+def profile_model(model, input_tensor, label="model", iterations=100):
     print("Starting profile")
 
     durations = []
-    for step in range(10):
+    for step in range(iterations):
         tic = time.time()
         model(input_tensor)
         toc = time.time()
         duration = toc - tic
+        duration = math.trunc(duration * 1000)
         durations.append(duration)
+    avg = sum(durations) / len(durations)
+    min_latency = min(durations)
+    max_latency = max(durations)
+    print(f"Average latency for {label} is: {avg} ms")
+    print(f"Min latency for {label} is: {min_latency} ms")
+    print(f"Max p99 latency for {label} is: {max_latency} ms")
 
-    print(f"Average latency for {label} is: {sum(durations / len(durations))}")
-    print(f"Min latency for {label} is: {min(durations)}")
-    print(f"Max latency for {label} is: {max(duration)} ")
-
-def print_size():
-    pass
 
 def load_model(model_path: str, device="cpu"):
-  map_location = torch.device(device)
-  if model_path.endswith(".pt"):
-       print("Unsupported Error: Cannot quantize torchscripted models")
-  else:
+    map_location = torch.device(device)
     model = torch.load(model_path, map_location=map_location)
     return model
 
 def print_size_of_model(model, label=""):
     torch.save(model.state_dict(), "temp.p")
     size=os.path.getsize("temp.p")
-    print("model: ",label,' \t','Size (MB):', size/1e6)
+    print("model: ",label,':','Size (MB):', size/1e6)
     os.remove('temp.p')
     return size
