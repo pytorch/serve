249d86aec879a82b9f71ca2fb05203c1775032b9
Nikhil Kulkarni <nikhilsk@amazon.com>
2021-10-14 14:45:34 -0700

    Use pty

--- ci/launcher/launch_test.py
+++ ci/launcher/launch_test.py
@@ -40,7 +40,7 @@ def run_commands_on_ec2_instance(ec2_connection, is_gpu):
         for command in commands_list:
             LOGGER.info(f"*** Executing command on ec2 instance: {command}")
             ret_obj = ec2_connection.run(
-                command, echo=True, warn=True, shell="/bin/bash", env={"LC_CTYPE": "en_US.utf8"}
+                command, echo=True, warn=True, pty=True, shell="/bin/bash", env={"LC_CTYPE": "en_US.utf8"}
             )
 
             if ret_obj.return_code != 0:
