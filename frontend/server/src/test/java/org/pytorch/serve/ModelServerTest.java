e79d9a21dd15cee4edb50de4185c2ca2205fb7d5
Ubuntu <ubuntu@ip-172-31-60-36.us-west-2.compute.internal>
2021-10-18 19:23:07 +0000

    fixed syntax error

--- frontend/server/src/test/java/org/pytorch/serve/ModelServerTest.java
+++ frontend/server/src/test/java/org/pytorch/serve/ModelServerTest.java
@@ -1097,7 +1097,7 @@ public class ModelServerTest {
         TestUtils.setLatch(new CountDownLatch(1));
         TestUtils.setHttpStatus(null);
 
-        for (int i = 0; i < batch_size; i++) {
+        for (int i = 0; i < batchSize; i++) {
             DefaultFullHttpRequest req =
                     new DefaultFullHttpRequest(
                             HttpVersion.HTTP_1_1, HttpMethod.POST, "/predictions/noop");
