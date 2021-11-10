d8925643fa0ecf4fdd4bd2712656bebecd5647ec
lxning <lninga@amazon.com>
2021-11-09 23:33:02 -0800

    trigger build

--- frontend/server/src/main/java/org/pytorch/serve/wlm/WorkLoadManager.java
+++ frontend/server/src/main/java/org/pytorch/serve/wlm/WorkLoadManager.java
@@ -161,6 +161,7 @@ public class WorkLoadManager {
             List<WorkerThread> threads, Model model, int count, CompletableFuture<Integer> future) {
         WorkerStateListener listener = new WorkerStateListener(future, count);
         int maxGpu = configManager.getNumberOfGpu();
+        logger.info("maxGpu=%d", maxGpu);
         for (int i = 0; i < count; ++i) {
             int gpuId = -1;
 
