69e04788f0e008a8453e8ee9e4647d0cd4174e64
Mark Saroufim <marksaroufim@fb.com>
2021-11-18 02:14:43 +0000

    added lint.sh credit

--- ts_scripts/lint.sh
+++ ts_scripts/lint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
+# Credit: https://github.com/pytorch/torchx/blob/main/scripts/lint.sh
 
 set -e
 
