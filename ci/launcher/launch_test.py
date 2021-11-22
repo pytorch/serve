ccf2f65f341e51816bb6aafc0177d8469b9c0749
Nikhil Kulkarni <knikhil29@gmail.com>
2021-11-22 08:20:44 -0800

    Remove pty

--- ci/launcher/launch_test.py
+++ ci/launcher/launch_test.py
@@ -48,7 +48,6 @@ def run_commands_on_ec2_instance(ec2_connection, is_gpu):
                     command,
                     echo=True,
                     warn=True,
-                    pty=True,
                     shell="/bin/bash",
                     env={
                         "LC_CTYPE": "en_US.utf8",
