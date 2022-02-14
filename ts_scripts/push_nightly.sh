2dd26d8a9317edc7a4d4d785faa84a7ecf76b6f9
Mark Saroufim <marksaroufim@fb.com>
2022-02-14 22:14:03 +0000

    stuff works

--- ts_scripts/push_nightly.sh
+++ ts_scripts/push_nightly.sh
@@ -5,12 +5,12 @@ rm -r dist || true
 
 python setup.py --override-name torchserve-nightly bdist_wheel
 
-if [ -z "$PYPI_TOKEN" ]; then
-    echo "must specify PYPI_TOKEN"
-    exit 1
-fi
+# if [ -z "$PYPI_TOKEN" ]; then
+#     echo "must specify PYPI_TOKEN"
+#     exit 1
+# fi
 
-python3 -m twine upload \
-    --username __token__ \
-    --password "$PYPI_TOKEN" \
-    dist/torchserve_nightly-*
+# python3 -m twine upload \
+#     --username __token__ \
+#     --password "$PYPI_TOKEN" \
+#     dist/torchserve_nightly-*
