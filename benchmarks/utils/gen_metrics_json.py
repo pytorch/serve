83928cfd240a284883c5a61bbcf863e03eed06ee
lxning <lninga@amazon.com>
2022-02-22 09:15:58 -0800

    format

--- benchmarks/utils/gen_metrics_json.py
+++ benchmarks/utils/gen_metrics_json.py
@@ -36,74 +36,6 @@ METRICS_NAME_SET = {
     "HandlerTime",
 }
 
-def extract_metrics_from_csv(csv_file_path):
-    with open(csv_file_path, 'r') as csvfile:
-        csv_reader = csv.DictReader(csvfile, delimiter=',')
-        for row in csv_reader:
-            model = row["Model"]
-            index = model.rfind('/') + 1
-            row["Model"] = model[index:-4]
-            return row
-
-    return None
-
-def gen_metrics_from_log(csv_dict, metrics_log_file_path, raw_metrics_file_path):
-    if metrics_log_file_path is None:
-        return
-
-    with open(metrics_log_file_path, 'r') as logfile:
-        lines = logfile.readlines()
-
-    metrics_dict_list = []
-    pattern = re.compile(" TS_METRICS | MODEL_METRICS ")
-    for line in lines:
-        if pattern.search(line):
-            segments = line.split("|")
-            name, unit, value = parse_segments_0(segments[0])
-            if name is None:
-                continue
-            dimensions = parse_segments_1(csv_dict, segments[1])
-            timestamp = parse_segments_2(segments[2])
-            metrics_dict_list.append({
-                "MetricName": '{}_{}'.format(csv_dict["Model"], name),
-                "Dimensions": dimensions,
-                "Unit": unit,
-                "Value": float(value),
-                "Timestamp": timestamp
-            })
-
-    if raw_metrics_file_path is not None:
-        with open(raw_metrics_file_path, 'w') as raw_file:
-            json.dump(metrics_dict_list, raw_file, indent=4)
-
-def parse_segments_0(segment):
-    index = segment.rfind(" ") + 1
-    data = segment[index:].split(":")
-    value = data[1]
-    name_unit = data[0].split('.')
-    if name_unit[0] in METRICS_NAME_SET:
-        name = name_unit[0]
-        unit = UNIT_MAP[name_unit[1]]
-    else:
-        name = None
-        unit = None
-
-    return name, unit, value
-
-def parse_segments_1(csv_dict, segment):
-    data = segment[1:].split(',')
-    dimensions = [{"Name": "batch_size", "Value": csv_dict["Batch size"]}]
-    for d in data:
-        dimension = d.split(':')
-        dimensions.append({"Name": dimension[0], "Value": dimension[1]})
-
-    return dimensions
-
-def parse_segments_2(segment):
-    index = segment.rfind(',') + 1
-    data = segment[index:].split(':')
-    return int(data[1])
-
 # Ref metrics json format
 # https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-metric-streams-formats-json.html
 def gen_metrics_from_csv(csv_dict, stats_file_path):
@@ -214,6 +146,73 @@ def gen_metrics_from_csv(csv_dict, stats_file_path):
 
     with open(stats_file_path, 'w') as stats_file:
         json.dump(metrics_dict_list, stats_file, indent = 4)
+
+def extract_metrics_from_csv(csv_file_path):
+    with open(csv_file_path, 'r') as csvfile:
+        csv_reader = csv.DictReader(csvfile, delimiter=',')
+        for row in csv_reader:
+            model = row["Model"]
+            index = model.rfind('/') + 1
+            row["Model"] = model[index:-4]
+            return row
+
+    return None
+
+def gen_metrics_from_log(csv_dict, metrics_log_file_path, raw_metrics_file_path):
+    if metrics_log_file_path is None or raw_metrics_file_path is None:
+        return
+
+    with open(metrics_log_file_path, 'r') as logfile:
+        lines = logfile.readlines()
+
+    metrics_dict_list = []
+    pattern = re.compile(" TS_METRICS | MODEL_METRICS ")
+    for line in lines:
+        if pattern.search(line):
+            segments = line.split("|")
+            name, unit, value = parse_segments_0(segments[0])
+            if name is None:
+                continue
+            dimensions = parse_segments_1(csv_dict, segments[1])
+            timestamp = parse_segments_2(segments[2])
+            metrics_dict_list.append({
+                "MetricName": '{}_{}'.format(csv_dict["Model"], name),
+                "Dimensions": dimensions,
+                "Unit": unit,
+                "Value": float(value),
+                "Timestamp": timestamp
+            })
+
+    with open(raw_metrics_file_path, 'w') as raw_file:
+        json.dump(metrics_dict_list, raw_file, indent=4)
+
+def parse_segments_0(segment):
+    index = segment.rfind(" ") + 1
+    data = segment[index:].split(":")
+    value = data[1]
+    name_unit = data[0].split('.')
+    if name_unit[0] in METRICS_NAME_SET:
+        name = name_unit[0]
+        unit = UNIT_MAP[name_unit[1]]
+    else:
+        name = None
+        unit = None
+
+    return name, unit, value
+
+def parse_segments_1(csv_dict, segment):
+    data = segment[1:].split(',')
+    dimensions = [{"Name": "batch_size", "Value": csv_dict["Batch size"]}]
+    for d in data:
+        dimension = d.split(':')
+        dimensions.append({"Name": dimension[0], "Value": dimension[1]})
+
+    return dimensions
+
+def parse_segments_2(segment):
+    index = segment.rfind(',') + 1
+    data = segment[index:].split(':')
+    return int(data[1])
 
 def main():
     parser = argparse.ArgumentParser()
