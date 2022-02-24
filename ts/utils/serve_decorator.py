435b7fb8bbf28a98777892717c89db49036b791f
Mark Saroufim <marksaroufim@fb.com>
2022-02-23 17:39:43 -0800

    update

--- ts/utils/serve_decorator.py
+++ ts/utils/serve_decorator.py
@@ -14,10 +14,15 @@ logger = logging.getLogger()
 def serve(func):
     @wraps(func)
     def serve_model(*args, **kwargs):
-        config = create_torchserve_config()
-        model = archive_model()
+        config = create_torchserve_config(inference_http_port=7070, management_http_port=7071)
+        
+        model = archive_model(serialized_file="model.pt")
+
         start_torchserve(models=model, config=config)
-        return NotImplemented
+        
+        # Error handle and
+        # Return some error code
+        return 0
 
 # TODO: Create decorator for model archiver as well?
 @serve
@@ -43,7 +48,7 @@ def start_torchserve(handler="base_handler", model_store="model_store", ts_confi
 
 
 
-def create_torchserve_config(inference_http_port : int = 8080, management_http_port : int = 8081, batch_size : int = 1):
+def create_torchserve_config(inference_http_port : int = 8080, management_http_port : int = 8081, batch_size : int = 1, config_properties="config.properties"):
     """"
     Create a torchserve config.properties
     Currently this only supports inference and management port setting but 
@@ -60,14 +65,21 @@ def create_torchserve_config(inference_http_port : int = 8080, management_http_p
         with open("config.properties", "w") as f:
             f.write(f"{key}={value}\n")
     
+    return config_properties
+    
 
-def archive_model(model_name : str = "model", handler : str = "base_handler", version : int = 1, extra_files : List[str] = []):
+def archive_model(serialized_file : str, model_file : str = None, model_name : str = "model", handler : str = "base_handler", version : int = 1, extra_files : List[str] = []):
     """
     wrapper on top of torch-model-archiver
+    model_file is only needed for eager mode execution
     """
-    arch-command = f'torch-model-archiver'
-
-    return NotImplemented
+    if model_file:
+        arch_command = f"torch-model-archiver --model_file {model_file} --serialized_file {serialized_file} --model-name {model_name} --handler {handler} --version {version} --extra-files {extra_files}"
+    else:
+        arch_command = f"torch-model-archiver --model_file {model_file} --serialized_file {serialized_file} --model-name {model_name} --handler {handler} --version {version} --extra-files {extra_files}"
+    os.system(arch_command)
+    
+    return 0
 
 def torchserve_request(endpoint : str= "", extra_params : str = ""):
     """
