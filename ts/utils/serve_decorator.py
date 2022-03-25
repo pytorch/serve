32aa0aca5329ecff33d8a5f8f70cbf7fdc61aea5
Mark Saroufim <marksaroufim@fb.com>
2022-03-25 14:51:19 -0700

    updates

--- ts/utils/serve_decorator.py
+++ ts/utils/serve_decorator.py
@@ -8,10 +8,8 @@ from functools import wraps
 from typing import List, Callable
 import os
 import logging 
-import grequest #add this to requirements.txt
+import grequest # TODO: add this to requirements.txt
 import inspect
-from retrying import retry
-
 
 logger = logging.getLogger()
 
@@ -28,17 +26,20 @@ def serve(func):
     2. Create a new torchserve config 
     3. Archive a model.pt
     4. Start torchserve
+
+    When done call torchserve_stop()
+
     """
     @wraps(func)
     def serve_model(*args, **kwargs):
         
-        handler_file = create_handler(func)
+        handler = create_handler(func)
 
         config = create_torchserve_config(inference_http_port=7070, management_http_port=7071)
         
         model = archive_model(serialized_file="model.pt")
 
-        start_torchserve(models=model, config=config)
+        start_torchserve(handler= handler, models=model, config=config)
         
         # Error handle and
         # Return some error code
@@ -49,16 +50,18 @@ def create_handler(handler : Callable, handler_file : str = "handler.py"):
     Create a new handler that inherits from the base handler
     Update the inference() function with the function captured by the decorator
     Write the handler to disk
+
+    TODO: Find a solution to make this work with workflows, pending more improvements in workflows
     """
-        inference_handler = inspect.getsource(handler)
-        with open(handler_file, "w") as f:
-            f.write("class CustomHandler(BaseHandler):\n")
-            for index, line in enumerate(inference_handler):
-                if index == 0:
-                    f.write(f"\tdef inference(self, data, *args, **kwargs):")
-                else:
-                    f.write(f"\t{line}")
-        return handler_file
+    inference_handler = inspect.getsource(handler)
+    with open(handler_file, "w") as f:
+        f.write("class CustomHandler(BaseHandler):\n")
+        for index, line in enumerate(inference_handler):
+            if index == 0:
+                f.write(f"\tdef inference(self, data, *args, **kwargs):")
+            else:
+                f.write(f"\t{line}")
+    return handler_file
 
 def writetofile(filename : str = "model.py"):
     """
@@ -81,9 +84,9 @@ def handle_test():
 def start_torchserve(handler="base_handler", model_store="model_store", ts_config="config.properties", model_mar="model.mar"):
     """
     Wrapper on top of torchserve --start args
+    If you already have your own model packaged
     """
-    ts_config = create_torchserve_config()
-    
+
     ts_command = f'torchserve --start'
     f'--model_store {model_store}'
     f'--ts_config_file {ts_config}'
@@ -159,12 +162,12 @@ def archive_model(serialized_file : str, model_file : str = None, model_name : s
     # Torchscripted model
     else:
         arch_command = f"torch-model-archiver --serialized_file {serialized_file} --model-name {model_name} --handler {handler} --version {version} --extra-files {extra_files}"
+    
     os.system(arch_command)
     
     return 0
 
-@retry(stop_max_delay=1000)
-def stop_torchserve():
+def torchserve_stop():
     os.system("torchserve --stop")
 
 def torchserve_request(endpoint : str= "", tasks : List[str] = [], extra_params : str = ""):
