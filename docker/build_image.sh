258e8c4ff9a259dd631b288bf4a72019eccb7e3a
Mark Saroufim <marksaroufim@fb.com>
2022-05-13 15:21:13 -0700

    Update build_image.sh

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -67,13 +67,13 @@ do
             BASE_IMAGE="nvidia/cuda:11.1-cudnn8-runtime-ubuntu18.04"
           elif [ $CUDA_VERSION == "cu102" ];
           then
-            BASE_IMAGE="nvidia/cuda:10.2-cudnn7-runtime-ubuntu18.04"
+            BASE_IMAGE="nvidia/cuda:10.2-cudnn8-runtime-ubuntu18.04"
           elif [ $CUDA_VERSION == "cu101" ]
           then
-            BASE_IMAGE="nvidia/cuda:10.1-cudnn7-runtime-ubuntu18.04"
+            BASE_IMAGE="nvidia/cuda:10.1-cudnn8-runtime-ubuntu18.04"
           elif [ $CUDA_VERSION == "cu92" ];
           then
-            BASE_IMAGE="nvidia/cuda:9.2-cudnn7-runtime-ubuntu18.04"
+            BASE_IMAGE="nvidia/cuda:9.2-cudnn8-runtime-ubuntu18.04"
           else
             echo "CUDA version not supported"
             exit 1
