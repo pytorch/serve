1a6a5d3af840697998a49bf5f504678dfe691a04
Mark Saroufim <marksaroufim@fb.com>
2022-06-02 21:00:55 +0000

    update test

--- ts_scripts/frontend_utils.py
+++ ts_scripts/frontend_utils.py
@@ -8,6 +8,9 @@ def test_frontend():
     frontend_gradlew_cmd = f"{frontend_gradlew_path} -p frontend clean build"
     print(f"## In directory: {os.getcwd()} | Executing command: {frontend_gradlew_cmd}")
     frontend_gradlew_exit_code = os.system(frontend_gradlew_cmd)
+    code_coverage_cmd = f"{frontend_gradlew_path} codeCoverageReport"
+    print(f"## In directory: {os.getcwd()} | Executing command: {code_coverage_cmd}")
+    os.system(code_coverage_cmd)
 
     if frontend_gradlew_exit_code != 0:
         sys.exit("## Frontend Gradle Tests Failed !")
