7bf5b8aa9e0651a8e77e73d7f230bbd395308a5a
agunapal <agunapal@berkeley.edu>
2022-06-18 01:34:14 +0000

    corrected lint errors

--- binaries/build.py
+++ binaries/build.py
@@ -1,20 +1,23 @@
 import argparse
+import glob
 import os
 import sys
-import glob
 
 # To help discover local modules
 REPO_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 sys.path.append(REPO_ROOT)
 
-from pathlib import Path
 
-from ts_scripts.utils import is_conda_env, is_conda_build_env
-from binaries.conda.build_packages import conda_build, install_miniconda, install_conda_build
+from binaries.conda.build_packages import (
+    conda_build,
+    install_conda_build,
+    install_miniconda,
+)
+from ts_scripts.utils import is_conda_build_env, is_conda_env
 
 
 def build(args):
-    
+
     print("## Started torchserve, model-archiver and workflow-archiver build")
     create_wheel_cmd = "python setup.py bdist_wheel --release --universal"
 
@@ -28,7 +31,7 @@ def build(args):
     ma_build_exit_code = os.system(create_wheel_cmd)
 
     os.chdir(REPO_ROOT)
-    
+
     # Build workflow archiver wheel
     os.chdir("workflow-archiver")
     print(f"## In directory: {os.getcwd()} | Executing command: {create_wheel_cmd}")
@@ -37,8 +40,12 @@ def build(args):
     os.chdir(REPO_ROOT)
 
     ts_wheel_path = glob.glob(os.path.join(REPO_ROOT, "dist", "*.whl"))[0]
-    ma_wheel_path = glob.glob(os.path.join(REPO_ROOT, "model-archiver", "dist", "*.whl"))[0]
-    wa_wheel_path = glob.glob(os.path.join(REPO_ROOT, "workflow-archiver", "dist", "*.whl"))[0]
+    ma_wheel_path = glob.glob(
+        os.path.join(REPO_ROOT, "model-archiver", "dist", "*.whl")
+    )[0]
+    wa_wheel_path = glob.glob(
+        os.path.join(REPO_ROOT, "workflow-archiver", "dist", "*.whl")
+    )[0]
     print(f"## TorchServe wheel location: {ts_wheel_path}")
     print(f"## Model archiver wheel location: {ma_wheel_path}")
     print(f"## Workflow archiver wheel location: {ma_wheel_path}")
@@ -47,12 +54,14 @@ def build(args):
     conda_build_exit_code = 0
     if not is_conda_env():
         install_miniconda()
-    
+
     if not is_conda_build_env():
         install_conda_build()
 
-    conda_build_exit_code = conda_build(ts_wheel_path, ma_wheel_path, wa_wheel_path, args.nightly)
-    
+    conda_build_exit_code = conda_build(
+        ts_wheel_path, ma_wheel_path, wa_wheel_path, args.nightly
+    )
+
     # If any one of the steps fail, exit with error
     if ts_build_exit_code != 0:
         sys.exit("## Torchserve Build Failed !")
@@ -65,8 +74,15 @@ def build(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Build for torchserve, torch-model-archiver and torch-workflow-archiver")
-    parser.add_argument("--nightly", action="store_true", required=False, help="specify nightly is being built")
+    parser = argparse.ArgumentParser(
+        description="Build for torchserve, torch-model-archiver and torch-workflow-archiver"
+    )
+    parser.add_argument(
+        "--nightly",
+        action="store_true",
+        required=False,
+        help="specify nightly is being built",
+    )
     args = parser.parse_args()
 
     build(args)
