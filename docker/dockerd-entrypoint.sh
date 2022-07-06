3030fd0fef10a333da9f0b8a4e16514d335f472d
Mark Saroufim <marksaroufim@fb.com>
2022-07-06 18:49:11 +0000

    [skip ci] dockerd bug

--- docker/dockerd-entrypoint.sh
+++ docker/dockerd-entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" = "serve" ]]; then
-    shift 1
-    torchserve --start --ts-config /home/model-server/config.properties
-else
-    eval "$@"
-fi
-
+shift 1
+torchserve --start --ts-config /home/model-server/config.properties
 # prevent docker exit
 tail -f /dev/null
