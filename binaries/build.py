fcda3257c08c9fff72b2fbe25c7fc59ce0303b45
agunapal <agunapal@berkeley.edu>
2022-07-08 20:04:48 +0000

    build nightly pypi

--- binaries/build.py
+++ binaries/build.py
@@ -16,26 +16,55 @@ from binaries.conda.build_packages import (
 from ts_scripts.utils import is_conda_build_env, is_conda_env
 
 
+def build_nighly_whl():
+
+    binaries = ["torchserve", "model-archiver", "workflow-archiver"]
+    print("## Started torchserve, model-archiver and workflow-archiver nightly build")
+    create_wheel_cmd = "python setup.py "
+
+    for binary in binaries:
+        if "serve" in binary:
+            cur_dir = REPO_ROOT
+        else:
+            cur_dir = REPO_ROOT + "/" + binary
+        os.chdir(cur_dir)
+
+        cur_wheel_cmd = (
+            create_wheel_cmd + "--override-name " + binary + "-nightly" + " bdist_wheel"
+        )
+
+        # Build nightly wheel
+        print(f"## In directory: {os.getcwd()} | Executing command: {cur_wheel_cmd}")
+        build_exit_code = os.system(cur_wheel_cmd)
+
+        # If any one of the steps fail, exit with error
+        if build_exit_code != 0:
+            sys.exit("## {%s} Nightly Build Failed !".format(binary))
+
+
 def build(args):
 
-    print("## Started torchserve, model-archiver and workflow-archiver build")
-    create_wheel_cmd = "python setup.py bdist_wheel --release --universal"
+    if not args.nightly:
+        print("## Started torchserve, model-archiver and workflow-archiver build")
+        create_wheel_cmd = "python setup.py bdist_wheel --release --universal"
 
-    # Build torchserve wheel
-    print(f"## In directory: {os.getcwd()} | Executing command: {create_wheel_cmd}")
-    ts_build_exit_code = os.system(create_wheel_cmd)
+        # Build torchserve wheel
+        print(f"## In directory: {os.getcwd()} | Executing command: {create_wheel_cmd}")
+        ts_build_exit_code = os.system(create_wheel_cmd)
 
-    # Build model archiver wheel
-    os.chdir("model-archiver")
-    print(f"## In directory: {os.getcwd()} | Executing command: {create_wheel_cmd}")
-    ma_build_exit_code = os.system(create_wheel_cmd)
+        # Build model archiver wheel
+        os.chdir("model-archiver")
+        print(f"## In directory: {os.getcwd()} | Executing command: {create_wheel_cmd}")
+        ma_build_exit_code = os.system(create_wheel_cmd)
 
-    os.chdir(REPO_ROOT)
+        os.chdir(REPO_ROOT)
 
-    # Build workflow archiver wheel
-    os.chdir("workflow-archiver")
-    print(f"## In directory: {os.getcwd()} | Executing command: {create_wheel_cmd}")
-    wa_build_exit_code = os.system(create_wheel_cmd)
+        # Build workflow archiver wheel
+        os.chdir("workflow-archiver")
+        print(f"## In directory: {os.getcwd()} | Executing command: {create_wheel_cmd}")
+        wa_build_exit_code = os.system(create_wheel_cmd)
+    else:
+        build_nighly_whl()
 
     os.chdir(REPO_ROOT)
 
