d1a0755cb55129dac6d797d62ee45ba16cbc3703
An <josax@bcd074174c5b.ant.amazon.com>
2022-07-11 13:50:37 -0700

    moved to pytest for unit testing

--- ts/tests/metrics_yaml_testing/metric_cache_unit_test.py
+++ ts/tests/metrics_yaml_testing/metric_cache_unit_test.py
@@ -1,15 +1,12 @@
 """
 Unit testing for MetricsCache class and yaml parsing.
 """
-import unittest
-import sys
-import yaml
-from ts.metrics.metric import Metric
+import pytest
 from ts.metrics.dimension import Dimension
 from ts.metrics.metric_cache import MetricsCache
 
 
-class TestAddMetrics(unittest.TestCase):
+class TestAddMetrics:
 
     def test_add_metric_passing(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
@@ -18,7 +15,7 @@ class TestAddMetrics(unittest.TestCase):
                                      dimensions=["dim1", "dim2"],
                                      metric_type="type")
 
-        self.assertTrue(True if "type-new_metric-dim1:dim2" in metrics_cache_obj.backend_cache else False)
+        assert "type-new_metric-dim1:dim2" in metrics_cache_obj.backend_cache
 
     def test_add_metric_dimensions_correct_obj_pass(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
@@ -30,12 +27,12 @@ class TestAddMetrics(unittest.TestCase):
         for key, metric in metrics_cache_obj.backend_cache.items():
 
             for dimensions_list in metric.dimensions:
-                self.assertTrue(isinstance(dimensions_list, Dimension))
+                assert isinstance(dimensions_list, Dimension)
 
     def test_add_metric_dimensions_correct_obj_fail(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
 
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             metrics_cache_obj.add_metric(metric_name="new_metric",
                                          unit="ms",
                                          dimensions=Dimension("hello", "world"),
@@ -43,7 +40,7 @@ class TestAddMetrics(unittest.TestCase):
 
     def test_add_metric_fail_metric_name(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             metrics_cache_obj.add_metric(metric_name=42,
                                          unit="ms",
                                          dimensions=["dim1", "dim2"],
@@ -51,7 +48,7 @@ class TestAddMetrics(unittest.TestCase):
 
     def test_add_metric_fail_unit(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             metrics_cache_obj.add_metric(metric_name="bar",
                                          unit=["foo"],
                                          dimensions=["dim1", "dim2"],
@@ -59,7 +56,7 @@ class TestAddMetrics(unittest.TestCase):
 
     def test_add_metric_fail_dimensions(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             metrics_cache_obj.add_metric(metric_name="bar",
                                          unit="ms",
                                          dimensions="sink",
@@ -67,7 +64,7 @@ class TestAddMetrics(unittest.TestCase):
 
     def test_add_metric_fail_dimensions_none(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             metrics_cache_obj.add_metric(metric_name="bar",
                                          unit="ms",
                                          dimensions=None,
@@ -75,7 +72,7 @@ class TestAddMetrics(unittest.TestCase):
 
     def test_add_metric_fail_type(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             metrics_cache_obj.add_metric(metric_name="bar",
                                          unit="ms",
                                          dimensions=["dim1", "dim2"],
@@ -83,14 +80,14 @@ class TestAddMetrics(unittest.TestCase):
 
     def test_add_metric_fail_type_unit(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             metrics_cache_obj.add_metric(metric_name="bar",
                                          unit=["ms"],
                                          dimensions=["dim1", "dim2"],
                                          metric_type={"key": 42})
 
 
-class TestGetMetrics(unittest.TestCase):
+class TestGetMetrics:
 
     def test_get_metric_passing(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
@@ -100,7 +97,10 @@ class TestGetMetrics(unittest.TestCase):
                                      metric_type="type")
 
         temp_metrics = metrics_cache_obj.get_metric("type-new_metric-dim1:dim2")
-        self.assertEquals("new_metric", temp_metrics.name)
+        assert "new_metric" == temp_metrics.name
+        assert isinstance(temp_metrics.dimensions, list)
+        assert "Milliseconds" == temp_metrics.unit
+        assert 0 == temp_metrics.value
 
     def test_get_metric_fail_not_exist(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
@@ -109,16 +109,16 @@ class TestGetMetrics(unittest.TestCase):
                                      dimensions=["dim1", "dim2"],
                                      metric_type="type")
 
-        with self.assertRaises(SystemExit):
+        with pytest.raises(SystemExit):
             metrics_cache_obj.get_metric("type-new_metric-dim1:dim3")
 
     def test_get_metric_fail_invalid_type(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
-        with self.assertRaises(SystemExit):
+        with pytest.raises(SystemExit):
             metrics_cache_obj.get_metric(["type-new_metric-dim1:dim3"])
 
 
-class TestParseYaml(unittest.TestCase):
+class TestParseYaml:
 
     def test_parse_expected_yaml(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
@@ -142,74 +142,74 @@ class TestParseYaml(unittest.TestCase):
                                         'histogram': [{'dimensions': ['model_name', 'host'],
                                                        'name': 'histogram_name',
                                                        'unit': 'ms'}]}}
-        self.assertEqual(actual_dict, expected_dict)
+        assert actual_dict == expected_dict
 
     def test_parse_yaml_errors(self):
         metrics_cache_obj = MetricsCache("metric_errors.yaml")
-        with self.assertRaises(SystemExit):
+        with pytest.raises(SystemExit):
             metrics_cache_obj._parse_yaml_file()
 
     def test_parse_yaml_none(self):
         metrics_cache_obj = MetricsCache(None)
-        with self.assertRaises(SystemExit):
+        with pytest.raises(SystemExit):
             metrics_cache_obj._parse_yaml_file()
 
     def test_parse_yaml_io_error(self):
         metrics_cache_obj = MetricsCache("doesnt_exist.yaml")
-        with self.assertRaises(SystemExit):
+        with pytest.raises(SystemExit):
             metrics_cache_obj._parse_yaml_file()
 
 
-class TestParseModelMetrics(unittest.TestCase):
+class TestParseModelMetrics:
     def test_pass_parse_model_metrics(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
-        self.assertTrue(isinstance(metrics_cache_obj._parse_specific_metric(), dict))
+        assert isinstance(metrics_cache_obj._parse_specific_metric(), dict)
 
     def test_pass_parse_ts_metrics(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
-        self.assertTrue(isinstance(metrics_cache_obj._parse_specific_metric("ts_metrics"), dict))
+        assert isinstance(metrics_cache_obj._parse_specific_metric("ts_metrics"), dict)
 
     def test_fail_parse_model_metrics(self):
         metrics_cache_obj = MetricsCache("metrics_wo_model_metrics.yaml")
-        with self.assertRaises(SystemExit):
+        with pytest.raises(SystemExit):
             metrics_cache_obj._parse_specific_metric()
 
     def test_fail_parse_none(self):
         metrics_cache_obj = MetricsCache("metrics_wo_model_metrics.yaml")
-        with self.assertRaises(SystemExit):
+        with pytest.raises(SystemExit):
             metrics_cache_obj._parse_specific_metric(None)
 
     def test_fail_parse_missing_fields(self):
         metrics_cache_obj = MetricsCache("metrics_model_empty.yaml")
-        self.assertIsNone(metrics_cache_obj._parse_specific_metric())
+        assert metrics_cache_obj._parse_specific_metric() is None
 
 
-class TestYamlCacheUtil(unittest.TestCase):
+class TestYamlCacheUtil:
     def test_yaml_to_cache_util_pass(self):
         metrics_cache_obj = MetricsCache("metrics.yaml")
         model_metrics_table = metrics_cache_obj._parse_specific_metric()
         metrics_cache_obj._yaml_to_cache_util(model_metrics_table)
-        self.assertEquals(['counter-counter_name-model_name:host',
-                           'gauge-gauge_name-model_name:host',
-                           'histogram-histogram_name-model_name:host'], list(metrics_cache_obj.backend_cache.keys()))
+        assert ['counter-counter_name-model_name:host',
+                'gauge-gauge_name-model_name:host',
+                'histogram-histogram_name-model_name:host'] == list(metrics_cache_obj.backend_cache.keys())
 
     def test_yaml_to_cache_util_fail_missing_fields(self):
         metrics_cache_obj = MetricsCache("metrics_missing_types.yaml")
         model_metrics_table = metrics_cache_obj._parse_specific_metric()
-        with self.assertRaises(SystemExit):
+        with pytest.raises(SystemExit):
             metrics_cache_obj._yaml_to_cache_util(model_metrics_table)
 
     def test_yaml_to_cache_util_fail_none(self):
         metrics_cache_obj = MetricsCache("metrics_missing_types.yaml")
-        with self.assertRaises(SystemExit):
+        with pytest.raises(SystemExit):
             metrics_cache_obj._yaml_to_cache_util(None)
 
     def test_yaml_to_cache_util_fail_empty_section(self):
         metrics_cache_obj = MetricsCache("metrics_model_empty.yaml")
         model_metrics_table = metrics_cache_obj._parse_specific_metric()
-        with self.assertRaises(SystemExit):
+        with pytest.raises(SystemExit):
             metrics_cache_obj._yaml_to_cache_util(model_metrics_table)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
