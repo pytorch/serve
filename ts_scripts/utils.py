10782236d351447e4fee879632bab014b99763b0
Mark Saroufim <marksaroufim@fb.com>
2022-07-14 23:42:04 +0000

    push

--- ts_scripts/utils.py
+++ ts_scripts/utils.py
@@ -3,6 +3,9 @@ import platform
 import subprocess
 import sys
 
+REPO_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+sys.path.append(REPO_ROOT)
+
 nvidia_smi_cmd = {
     "Windows": "nvidia-smi.exe",
     "Darwin": "nvidia-smi",
