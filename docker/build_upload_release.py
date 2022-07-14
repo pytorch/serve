cedb127a49df09df14127ab030923bd6066134c6
Mark Saroufim <marksaroufim@fb.com>
2022-07-14 20:41:12 +0000

    [skip ci] added dry run and org parameter

--- docker/build_upload_release.py
+++ docker/build_upload_release.py
@@ -1,15 +1,51 @@
-from .docker_nightly import try_and_handle
+from argparse import ArgumentParser
+
 from ts import version
 
+from .docker_nightly import try_and_handle
 
 if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--organization",
+        type=str,
+        default="pytorch",
+        help="The name of the Dockerhub organization where the images will be pushed",
+    )
+    parser.add_argument(
+        "--dry_run",
+        action="store_true",
+        help="dry_run will print the commands that will be run without running them",
+    )
+    args = parser.parse_args()
+    dry_run = args.dry_run
+    organization = args.organization
 
     # Upload pytorch/torchserve docker binaries
-    try_and_handle("./build_image.sh -bt dev -g -cv -t pytorch/torchserve:latest")
-    try_and_handle("./build_image.sh -bt dev -g -cv 102 -t pytorch/torchserve:latest-gpu")
-    try_and_handle("docker tag pytorch/torchserve-latest pytorch/torchserve:latest-cpu")
-    try_and_handle(f"docker tag pytorch/torchserve:latest pytorch/torchserve:{version()}-cpu")
-    try_and_handle(f"docker tag pytorch/torchserve:latest pytorch/torchserve:{version()}-gpu")
+    try_and_handle(
+        f"./build_image.sh -bt dev -g -cv -t pytorch/torchserve:latest", dry_run
+    )
+    try_and_handle(
+        f"./build_image.sh -bt dev -g -cv 102 -t pytorch/torchserve:latest-gpu", dry_run
+    )
+    try_and_handle(
+        f"docker tag {organization}/torchserve-latest pytorch/torchserve:latest-cpu",
+        dry_run,
+    )
+    try_and_handle(
+        f"docker tag pytorch/torchserve:latest pytorch/torchserve:{version()}-cpu",
+        dry_run,
+    )
+    try_and_handle(
+        f"docker tag pytorch/torchserve:latest pytorch/torchserve:{version()}-gpu",
+        dry_run,
+    )
 
-    for image in ["pytorch/torchserve:latest", "pytorch/torchserve:latest-cpu", "pytorch/torchserve:latest-gpu", f"pytorch/torchserve:{version()}-cpu", f"pytorch/torchserve:{version()}-gpu"]:
+    for image in [
+        "pytorch/torchserve:latest",
+        "pytorch/torchserve:latest-cpu",
+        "pytorch/torchserve:latest-gpu",
+        f"pytorch/torchserve:{version()}-cpu",
+        f"pytorch/torchserve:{version()}-gpu",
+    ]:
         try_and_handle(f"docker push {image}")
--- kubernetes/kserve/build_upload_release.py
+++ kubernetes/kserve/build_upload_release.py
@@ -1,18 +1,47 @@
 # Not sure I can assume ts is installed on the dev machine, alternative is reading ts/version.txt
+import subprocess
+from argparse import ArgumentParser
+
 from ts import version
 
+
 # Move this to common utils function?
-def try_and_handle(cmd, dry_run = False):
+def try_and_handle(cmd, dry_run=False):
     if dry_run:
         print(f"Executing command: {cmd}")
     else:
         try:
-            subprocess.run([cmd], shell=True, check = True)
+            subprocess.run([cmd], shell=True, check=True)
         except subprocess.CalledProcessError as e:
-            raise(e)
+            raise (e)
 
-try_and_handle(f"./build_image.sh  -t pytorch/torchserve-kfs:{version()}")
-try_and_handle(f"./build_image.sh -g -t pytorch/torchserve-kfs:{version()}-gpu")
 
-for image in [f"pytorch/torchserve-kfs:{version()}", f"pytorch/torchserve-kfs:{version()}-gpu"]:
-    try_and_handle(f"docker push {image}")
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--organization",
+        type=str,
+        default="pytorch",
+        help="The name of the Dockerhub organization where the images will be pushed",
+    )
+    parser.add_argument(
+        "--dry_run",
+        action="store_true",
+        help="dry_run will print the commands that will be run without running them",
+    )
+    args = parser.parse_args()
+    dry_run = args.dry_run
+    organization = args.organization
+
+    try_and_handle(
+        f"./build_image.sh  -t {organization}/torchserve-kfs:{version()}", dry_run
+    )
+    try_and_handle(
+        f"./build_image.sh -g -t {organization}/torchserve-kfs:{version()}-gpu", dry_run
+    )
+
+    for image in [
+        f"{organization}/torchserve-kfs:{version()}",
+        f"{organization}/torchserve-kfs:{version()}-gpu",
+    ]:
+        try_and_handle(f"docker push {image}", dry_run)
