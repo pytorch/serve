81d237be2c35ad2d18fd7ecb624acc9b72c0f88f
Mark Saroufim <marksaroufim@fb.com>
2022-07-25 17:05:16 -0700

    Update install_dependencies.py

--- ts_scripts/install_dependencies.py
+++ ts_scripts/install_dependencies.py
@@ -91,6 +91,15 @@ class Linux(Common):
     def install_wget(self):
         if os.system("wget --version") != 0 or args.force:
             os.system(f"{self.sudo_cmd}apt-get install -y wget")
+            
+            
+    def install_libgit2(self):
+        os.system(
+            f"wget https://github.com/libgit2/libgit2/archive/refs/tags/v1.3.0.tar.gz -O libgit2-1.3.0.tar.gz"
+        )
+        os.system(f"tar xzf libgit2-1.3.0.tar.gz")
+        os.system(f"cd libgit2-1.3.0 && cmake . && make && sudo make install && cd ..")
+        os.system(f"rm -rf libgit2-1.3.0 && rm libgit2-1.3.0.tar.gz")
 
     def install_maven(self):
         if os.system("mvn --version") != 0 or args.force:
