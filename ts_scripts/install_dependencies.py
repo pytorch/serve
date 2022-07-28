82d2c8d3c02c6d7c94d427b51c585ef4c1b1752f
rohithkrn <rohith.nallamaddi@gmail.com>
2022-07-28 00:00:33 +0000

    lint and fix JAVA_HOME

--- ts_scripts/install_dependencies.py
+++ ts_scripts/install_dependencies.py
@@ -103,15 +103,16 @@ class Linux(Common):
     def install_maven(self):
         # default maven version that comes with apt install is 3.6 which is incomaptible with jdk17
         # https://askubuntu.com/questions/1367854/when-compiling-a-project-maven-throws-an-error
-        # therefore, we need to manually install maven version 3.8.6 that is compatible with 3.8.6
+        # therefore, we need to manually install maven version 3.8.6 that is compatible with jdk17
         MAVEN_VERSION = "3.8.6"
         MAVEN_CONFIG_FILE = "/etc/profile.d/maven.sh"
-        os.system(f"wget https://apache.org/dist/maven/maven-3/{MAVEN_VERSION}/binaries/apache-maven-{MAVEN_VERSION}-bin.tar.gz -P /tmp")
+        os.system(
+            f"wget https://apache.org/dist/maven/maven-3/{MAVEN_VERSION}/binaries/apache-maven-{MAVEN_VERSION}-bin.tar.gz -P /tmp"
+        )
         os.system(f"{self.sudo_cmd}tar xf /tmp/apache-maven-*.tar.gz -C /opt")
         os.system(f"{self.sudo_cmd}rm /tmp/apache-maven-*-bin.tar.gz")
         os.system(f"{self.sudo_cmd}ln -s /opt/apache-maven-{MAVEN_VERSION} /opt/maven")
         with open("maven.sh", "w") as file:
-            file.write("export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64\n")
             file.write("export M2_HOME=/opt/maven\n")
             file.write("export MAVEN_HOME=/opt/maven\n")
             file.write("export PATH=${M2_HOME}/bin:${PATH}")
