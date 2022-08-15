45d3faccde6a5c673677f59d167eb8628d29520a
An <josax@bcd074174c5b.ant.amazon.com>
2022-08-15 15:00:41 -0700

    editing custom handler

--- ts/metrics/metrics_handler.py
+++ ts/metrics/metrics_handler.py
@@ -13,25 +13,13 @@ class MetricsHandler(BaseHandler):
     Custom model handler for MetricsCache object
     """
 
-    def handle(self, data, context):
-        """Entry point for default handler. It takes the data from the input request and returns
-           the predicted outcome for the input.
-
-        Args:
-            data (list): The input data that needs to be made a prediction request on.
-            context (Context): It is a JSON Object containing information pertaining to
-                               the model artefacts parameters.
-
-        Returns:
-            list : Returns a list of dictionary with the predicted response.
-        """
+    def initialize(self, context):
 
         # It can be used for pre or post processing if needed as additional request
         # information is available in context
         start_time = time.time()
 
-        self.context = context
-        metrics = self.context.metrics
+        metrics = context.metrics
         time.sleep(3)
         stop_time = time.time()
         metrics.add_time(
