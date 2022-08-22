aec424add930939e323af6ab007e275e5c7eaf63
lxning <lninga@amazon.com>
2022-08-22 11:26:22 -0700

    [skip ci] torch_scripted/torch_scripted_backend_test refactory

--- cpp/test/backends/torch_scripted/torch_scripted_backend_test.cc
+++ cpp/test/backends/torch_scripted/torch_scripted_backend_test.cc
@@ -32,64 +32,58 @@ namespace torchserve {
         false
       );
     };
+
+    void LoadPredict(
+      const std::string& model_dir,
+      const std::string& inference_input_file_path,
+      const std::string& inference_request_id_prefix
+    ) {
+      backend_->Initialize(model_dir);
+      auto result = backend_->LoadModel(std::move(load_model_request_base_));
+      ASSERT_EQ(result->code, 200);
+
+      std::ifstream input(inference_input_file_path, std::ios::in | std::ios::binary);
+      std::vector<char> image(
+        (std::istreambuf_iterator<char>(input)),
+        (std::istreambuf_iterator<char>()));
+      input.close();
+
+      auto inference_request_batch = std::make_shared<torchserve::InferenceRequestBatch>();
+      for (uint8_t i = 0; i < batch_size_; i++) {
+        torchserve::InferenceRequest inference_request;
+        inference_request.request_id = fmt::format("{}_{}", inference_request_id_prefix, i);
+        inference_request.headers[torchserve::PayloadType::kHEADER_NAME_DATA_TYPE] = 
+          torchserve::PayloadType::kDATA_TYPE_BYTES;
+        inference_request.parameters[torchserve::PayloadType::kPARAMETER_NAME_DATA] = image;
+
+        (*inference_request_batch).emplace_back(inference_request);
+      }
+      
+      auto inference_response_batch = backend_->GetModelInstance()->Predict(inference_request_batch);
+      for (const auto& kv : *inference_response_batch) {
+        ASSERT_EQ(kv.second->code, 200);
+      }
+    };
+
     uint8_t batch_size_ = 2;
     std::shared_ptr<torchserve::Backend> backend_;
     std::shared_ptr<torchserve::LoadModelRequest> load_model_request_base_;
     std::shared_ptr<torchserve::LoadModelRequest> load_model_request_mnist_;
   };
 
-  TEST_F(TorchScriptedBackendTest, TestPredictBase) {
-    backend_->Initialize("test/resources/torchscript_model/mnist/base_handler");
-    auto result = backend_->LoadModel(std::move(load_model_request_base_));
-    ASSERT_EQ(result->code, 200);
-
-    std::ifstream input("test/resources/torchscript_model/mnist/0_png.pt", std::ios::in | std::ios::binary);
-    std::vector<char> image(
-      (std::istreambuf_iterator<char>(input)),
-      (std::istreambuf_iterator<char>()));
-    input.close();
-
-    auto inference_request_batch = std::make_shared<torchserve::InferenceRequestBatch>();
-    for (uint8_t i = 0; i < batch_size_; i++) {
-      torchserve::InferenceRequest inference_request;
-      inference_request.request_id = fmt::format("mnist_ts_{}", i);
-      inference_request.headers[torchserve::PayloadType::kHEADER_NAME_DATA_TYPE] = 
-        torchserve::PayloadType::kDATA_TYPE_BYTES;
-      inference_request.parameters[torchserve::PayloadType::kPARAMETER_NAME_DATA] = image;
-
-      (*inference_request_batch).emplace_back(inference_request);
-    }
-    
-    auto inference_response_batch = backend_->GetModelInstance()->Predict(inference_request_batch);
-    for (const auto& kv : *inference_response_batch) {
-      ASSERT_EQ(kv.second->code, 200);
-    }
+  TEST_F(TorchScriptedBackendTest, TestLoadPredictBase) {
+    this->LoadPredict(
+      "test/resources/torchscript_model/mnist/base_handler",
+      "test/resources/torchscript_model/mnist/0_png.pt",
+      "mnist_ts"
+    );
   }
 
-  TEST_F(TorchScriptedBackendTest, TestPredictMnist) {
-    backend_->Initialize("test/resources/torchscript_model/mnist/mnist_handler");
-    auto result = backend_->LoadModel(std::move(load_model_request_mnist_));
-    ASSERT_EQ(result->code, 200);
-
-    std::ifstream input("test/resources/torchscript_model/mnist/0_png.pt", std::ios::in | std::ios::binary);
-    std::vector<char> image(
-      (std::istreambuf_iterator<char>(input)),
-      (std::istreambuf_iterator<char>()));
-    input.close();
-
-    auto inference_request_batch = std::make_shared<torchserve::InferenceRequestBatch>();
-    for (uint8_t i = 0; i < batch_size_; i++) {
-      torchserve::InferenceRequest inference_request;
-      inference_request.request_id = fmt::format("mnist_ts_{}", i);
-      inference_request.headers[torchserve::PayloadType::kHEADER_NAME_DATA_TYPE] = 
-        torchserve::PayloadType::kDATA_TYPE_BYTES;
-      inference_request.parameters[torchserve::PayloadType::kPARAMETER_NAME_DATA] = image;
-
-      (*inference_request_batch).emplace_back(inference_request);
-    }
-    auto inference_response_batch = backend_->GetModelInstance()->Predict(inference_request_batch);
-    for (const auto& kv : *inference_response_batch) {
-      ASSERT_EQ(kv.second->code, 200);
-    }
+  TEST_F(TorchScriptedBackendTest, TestLoadPredictMnist) {
+    this->LoadPredict(
+      "test/resources/torchscript_model/mnist/mnist_handler",
+      "test/resources/torchscript_model/mnist/0_png.pt",
+      "mnist_ts"
+    );
   }
 } //namespace
