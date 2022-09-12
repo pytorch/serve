270d300f51e68118a6559aca352ef73a77f28de3
Mark Saroufim <marksaroufim@fb.com>
2022-09-12 11:38:42 +0000

    lint

--- ts_scripts/install_from_src.py
+++ ts_scripts/install_from_src.py
@@ -1,9 +1,6 @@
+import argparse
 import os
 import sys
-import time
-import shutil
-import argparse
-
 
 # To help discover local modules
 REPO_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
@@ -12,13 +9,19 @@ sys.path.append(REPO_ROOT)
 from ts_scripts import print_env_info as build_hdr_printer
 from ts_scripts.utils import check_python_version
 
+
 def install_binaries(dev):
     for binary in [".", "model-archiver", "workflow-archiver"]:
-        cmd = f"pip install --force-reinstall -e {binary}" if dev else f"pip install --force-reinstall {binary}"
+        cmd = (
+            f"pip install --force-reinstall -e {binary}"
+            if dev
+            else f"pip install --force-reinstall {binary}"
+        )
         print(f"## In directory {os.getcwd()} | Executing command {cmd}")
         os.system(cmd)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--environment",
@@ -29,6 +32,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     check_python_version()
     from pygit2 import Repository
-    git_branch = Repository('.').head.shorthand
+
+    git_branch = Repository(".").head.shorthand
     build_hdr_printer.main(git_branch)
     install_binaries(args.environment == "dev")
