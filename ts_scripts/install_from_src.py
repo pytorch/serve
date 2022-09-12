7e26c04dafa7c5b09eeb5a18b0a49e5bec5a0d70
Mark Saroufim <marksaroufim@fb.com>
2022-09-12 11:34:56 +0000

    Added dev mode to install_from_src.py

--- ts_scripts/install_from_src.py
+++ ts_scripts/install_from_src.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import shutil
+import argparse
 
 
 # To help discover local modules
@@ -10,64 +11,24 @@ sys.path.append(REPO_ROOT)
 
 from ts_scripts import print_env_info as build_hdr_printer
 from ts_scripts.utils import check_python_version
-from ts_scripts.utils import is_conda_env
 
-
-def clean_slate(): 
-    print("## Uninstall existing torchserve and model archiver")
-    if is_conda_env():
-        cmd = "conda uninstall -y torchserve torch-model-archiver workflow-model-archiver"
-    else:
-        cmd = "pip uninstall -y torchserve torch-model-archiver workflow-model-archiver"
-    print(f"## In directory: {os.getcwd()} | Executing command: {cmd}")
-    os.system(cmd)
-    time.sleep(5)
-
-
-def install_torchserve():
-    print("## Install torchserve from source")
-    cmd = "pip install ."
-    print(f"## In directory: {os.getcwd()} | Executing command: {cmd}")
-    os.system(cmd)
-
-
-def install_torch_model_archiver():
-    print("## Install torch-model-archiver from source")
-    cmd = "pip install model-archiver/."
-    print(f"## In directory: {os.getcwd()} | Executing command: {cmd}")
-    os.system(cmd)
-
-
-def install_torch_workflow_archiver():
-    print("## Install torch-workflow-archiver from source")
-    cmd = "pip install workflow-archiver/."
-    print(f"## In directory: {os.getcwd()} | Executing command: {cmd}")
-    os.system(cmd)
-
-
-def clean_up_build_residuals():
-    print("## Cleaning build residuals (__pycache__)")
-    try:
-        for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
-            if "__pycache__" in dirnames:
-                cache_dir = os.path.join(dirpath, "__pycache__")
-                print(f"## Removing - {cache_dir}")
-                shutil.rmtree(cache_dir)
-    except Exception as e:
-        print(f"#Error while cleaning cache file. Details - {str(e)}")
-
-
-def install_from_src():
-    clean_slate()
-    install_torch_model_archiver()
-    install_torch_workflow_archiver()
-    install_torchserve()    
-    clean_up_build_residuals()
-
+def install_binaries(dev):
+    for binary in [".", "model-archiver", "workflow-archiver"]:
+        cmd = f"pip install --force-reinstall -e {binary}" if dev else f"pip install --force-reinstall {binary}"
+        print(f"## In directory {os.getcwd()} | Executing command {cmd}")
+        os.system(cmd)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="production",
+        help="options: dev|prod",
+    )
+    args = parser.parse_args()
     check_python_version()
     from pygit2 import Repository
     git_branch = Repository('.').head.shorthand
     build_hdr_printer.main(git_branch)
-    install_from_src()
+    install_binaries(args.environment == "dev")
