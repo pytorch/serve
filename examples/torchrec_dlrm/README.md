b8113b0e54867a34f609a8d5d023818d87679272
Ankith Gunapal <agunapal@fb.com>
2022-09-13 18:56:56 +0000

    Handle dynamic batching

--- examples/torchrec_dlrm/README.md
+++ examples/torchrec_dlrm/README.md
@@ -1,11 +1,11 @@
 
-### TorchRec DLRM Example
+# TorchRec DLRM Example
 
 This example shows how to serve a Deep Learning Recommendation Model (DLRM) with TorchRec on a single GPU (CPU is currently not yet supported by this example). It requires at least 15GB of free GPU memory to run.
 The DLRM is an open source model for personalization and recommendation use cases published by Meta. More information can be found in this [paper](https://arxiv.org/abs/1906.00091) and this [blog post](https://ai.facebook.com/blog/dlrm-an-advanced-open-source-deep-learning-recommendation-model/).
 TorchRec is Meta's open source library for recommender systems in Pytorch. More information on TorchRec can be found in the [official docs](https://pytorch.org/torchrec/).
 
-# Install torchrec using the following command
+## Install torchrec using the following command
 This needs pytorch 1.12 to work
 
 ```bash
@@ -46,12 +46,44 @@ curl -H "Content-Type: application/json" --data @sample_data.json http://127.0.0
 ```sample_data.json``` is an example of input in the format of criteo [dataset](https://ailab.criteo.com/download-criteo-1tb-click-logs-dataset/).
 This has 13 integer features(dense) and 26 categorical features(sparse)
 
-The output of the model is the probability of a click ( based on the features) More details can be found [here](https://github.com/facebookresearch/dlrm)
+The output of the model is the log odds of a click ( based on the features) More details can be found [here](https://github.com/facebookresearch/dlrm)
 Our output should look like this:
 ```
 {
-    "default": [
-        0.1051536425948143
-    ]
+    "score":  0.1051536425948143
+}
+```
+
+## TorchRec DLRM with TorchServe dynamic batching
+
+
+We start TorchServe with:
+
+```
+torchserve --start --model-store model_store
+curl -X POST "localhost:8081/models?model_name=dlrm&url=dlrm.mar&batch_size=4&max_batch_delay=5000&initial_workers=1&synchronous=true"
+```
+
+The above commands will create the mar file and register the dlrm model with torchserve with following configuration :
+
+ - model_name : dlrm
+ - batch_size : 4
+ - max_batch_delay : 5000 ms
+ - workers : 1
+
+To test batch inference execute the following commands within the specified max_batch_delay time :
+
+```bash
+curl -H "Content-Type: application/json" --data @sample_data.json http://127.0.0.1:8080/predictions/dlrm & curl -H "Content-Type: application/json" --data @sample_data.json http://127.0.0.1:8080/predictions/dlrm & curl -H "Content-Type: application/json" --data @sample_data.json http://127.0.0.1:8080/predictions/dlrm
+```
+
+The output looks like this
+```
+{
+  "score": 0.010169986635446548
+}{
+  "score": 0.010169986635446548
+}{
+  "score": 0.010169986635446548
 }
 ```
--- examples/torchrec_dlrm/dlrm_handler.py
+++ examples/torchrec_dlrm/dlrm_handler.py
@@ -85,27 +85,37 @@ class TorchRecDLRMHandler(BaseHandler, ABC):
                 (KeyJaggedTensor): Sparse features
         """
 
-        line = data[0]
-        batch = line.get("data") or line.get("body")
+        float_features, id_list_features_lengths, id_list_features_values = [], [], []
 
-        if not isinstance(batch, dict):
-            batch = json.loads(batch)
+        for row in data:
 
-        # This is the dense feature part
-        assert "float_features" in batch
-        # The sparse input consists of a length vector and the values.
-        # The length vector contains the number of elements which are part fo the same entry in the linear list provided as input.
-        assert "id_list_features.lengths" in batch
-        assert "id_list_features.values" in batch
+            input = row.get("data") or row.get("body")
 
-        dense_features = torch.FloatTensor(batch["float_features"])
+            if not isinstance(input, dict):
+                input = json.loads(input)
 
+            # This is the dense feature part
+            assert "float_features" in input
+            # The sparse input consists of a length vector and the values.
+            # The length vector contains the number of elements which are part fo the same entry in the linear list provided as input.
+            assert "id_list_features.lengths" in input
+            assert "id_list_features.values" in input
+
+            float_features.append(input["float_features"])
+            id_list_features_lengths.extend(input["id_list_features.lengths"])
+            id_list_features_values.append(input["id_list_features.values"])
+
+        # Reformat the values input for KeyedJaggedTensor
+        id_list_features_values = torch.FloatTensor(id_list_features_values)
+        id_list_features_values = torch.transpose(id_list_features_values, 0, 1)
+        id_list_features_values = [value for value in id_list_features_values]
+
+        # Dense and Sparse Features for DLRM model
+        dense_features = torch.FloatTensor(float_features)
         sparse_features = KeyedJaggedTensor(
             keys=DEFAULT_CAT_NAMES,
-            lengths=torch.LongTensor(batch["id_list_features.lengths"]),
-            values=torch.zeros_like(
-                torch.FloatTensor(batch["id_list_features.values"])
-            ),
+            lengths=torch.LongTensor(id_list_features_lengths),
+            values=torch.cat(id_list_features_values),
         )
 
         return dense_features, sparse_features
@@ -123,6 +133,7 @@ class TorchRecDLRMHandler(BaseHandler, ABC):
             (Torch Tensor): The predicted response from the model is returned
                             in this function.
         """
+
         with torch.no_grad():
             data = map(lambda x: x.to(self.device), data)
             results = self.model(*data)
@@ -138,10 +149,13 @@ class TorchRecDLRMHandler(BaseHandler, ABC):
             output_explain (None): Defaults to None.
 
         Returns:
-            (list): Returns the response containing the predictions which consist of a single score per batch entry.
+            (list): Returns the response containing the predictions which consist of a single score per input entry.
         """
-        res = {"default": data.squeeze().float().tolist()}
-        if data.shape[0] == 1:
-            res["default"] = [res["default"]]
 
-        return [res]
+        result = []
+        for item in data:
+            res = {}
+            res["score"] = item.squeeze().float().tolist()
+            result.append(res)
+
+        return result
