fd1757d109565938c611025db8e54118fe623ab0
Matthias Reso <13337103+mreso@users.noreply.github.com>
2022-10-10 22:15:11 +0000

    Remove duplicate namespace definition

--- cpp/src/backends/process/model_worker.cc
+++ cpp/src/backends/process/model_worker.cc
@@ -1,6 +1,5 @@
 #include "src/backends/process/model_worker.hh"
 
-namespace fs = std::filesystem;
 #if __cpp_lib_filesystem >= 201703
 namespace fs = std::filesystem;
 #elif __cpp_lib_experimental_filesystem >= 201406
