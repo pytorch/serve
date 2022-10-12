b82d85ee1d74ce4616d682591a8f5626522825d8
jagadeesh <jagadeeshj@ideas2it.com>
2022-10-13 01:07:32 +0530

    feat: add handler for vision diffuser models
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>

---
+++ examples/Huggingface_Transformers/stbl_diff_handler.py
@@ -0,0 +1,96 @@
+import logging
+import zipfile
+from abc import ABC
+
+import torch
+import transformers
+from diffusers import StableDiffusionPipeline
+
+from ts.torch_handler.base_handler import BaseHandler
+
+logger = logging.getLogger(__name__)
+logger.info("Transformers version %s", transformers.__version__)
+
+
+class TransformersSeqClassifierHandler(BaseHandler, ABC):
+    """
+    Transformers handler class for sequence, token classification and question answering.
+    """
+
+    def __init__(self):
+        super(TransformersSeqClassifierHandler, self).__init__()
+        self.initialized = False
+
+    def initialize(self, ctx):
+        """In this initialize function, the BERT model is loaded and
+        the Layer Integrated Gradients Algorithm for Captum Explanations
+        is initialized here.
+        Args:
+            ctx (context): It is a JSON Object containing information
+            pertaining to the model artefacts parameters.
+        """
+        self.manifest = ctx.manifest
+        properties = ctx.system_properties
+        model_dir = properties.get("model_dir")
+
+        self.device = torch.device(
+            "cuda:" + str(properties.get("gpu_id"))
+            if torch.cuda.is_available() and properties.get("gpu_id") is not None
+            else "cpu"
+        )
+        # Loading the model and tokenizer from checkpoint and config files based on the user's choice of mode
+        # further setup config can be added.
+        with zipfile.ZipFile(model_dir + "model.zip", "r") as zip_ref:
+            zip_ref.extractall(model_dir + "/model")
+
+        self.pipe = StableDiffusionPipeline.from_pretrained(model_dir + "/model")
+        self.pipe.to(self.device)
+        logger.info("Diffusion model from path %s loaded successfully", model_dir)
+
+        self.initialized = True
+
+    def preprocess(self, requests):
+        """Basic text preprocessing, based on the user's chocie of application mode.
+        Args:
+            requests (str): The Input data in the form of text is passed on to the preprocess
+            function.
+        Returns:
+            list : The preprocess function returns a list of Tensor for the size of the word tokens.
+        """
+        for idx, data in enumerate(requests):
+            input_text = data.get("data")
+            if input_text is None:
+                input_text = data.get("body")
+            if isinstance(input_text, (bytes, bytearray)):
+                input_text = input_text.decode("utf-8")
+            logger.info("Received text: '%s'", input_text)
+        return input_text
+
+    def inference(self, input_text):
+        """Predict the class (or classes) of the received text using the
+        serialized transformers checkpoint.
+        Args:
+            input_batch (list): List of Text Tensors from the pre-process function is passed here
+        Returns:
+            list : It returns a list of the predicted value for the input text
+        """
+        inferences = []
+        # Handling inference for sequence_classification.
+        generator = torch.Generator("cuda").manual_seed(1024)
+        image = self.pipe(
+            input_text, guidance_scale=7.5, num_inference_steps=50, generator=generator
+        ).images[0]
+        inferences.append(image)
+
+        logger.info("Model predicted: '%s'", image)
+        print("Generated text", inferences)
+        return inferences
+
+    def postprocess(self, inference_output):
+        """Post Process Function converts the predicted response into Torchserve readable format.
+        Args:
+            inference_output (list): It contains the predicted response of the input text.
+        Returns:
+            (list): Returns a list of the Predictions and Explanations.
+        """
+        return inference_output
