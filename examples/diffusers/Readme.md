4e8ec5a031db3daebcd1f75e9812f3bd52dc4117
jagadeesh <jagadeeshj@ideas2it.com>
2022-10-18 00:03:48 +0530

    feat: add kserve support
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>

--- examples/diffusers/Readme.md
+++ examples/diffusers/Readme.md
@@ -1,1 +1,54 @@
-# Huggingface Vision Diffusion Models
+# Running Stable diffusion model using Huggingface diffusers in Torchserve.
+
+### Step 1: Download model
+
+Set access token generated form Huggingface in `Download_model.py` file
+
+```bash
+python Download_model.py
+```
+
+### Compress downloaded model
+
+```bash
+cd Diffusion_model
+zip -r ../model.zip *
+```
+
+### Generate MAR file
+
+Navigate back to `diffusers` directory.
+
+```bash
+torch-model-archiver --model-name stable-diffusion --version 1.0 --handler stable_diffusion_handler.py --extra-files model.zip -r requirements.txt
+```
+
+### Start torchserve
+
+```bash
+torchserve --start --ts-config config.properties
+```
+
+### Run inference
+
+```bash
+curl -v http://localhost:8080/predictions/stable-diffusion -T sample.txt > output.txt
+```
+
+Note: `sample_v1.json` and `sample_v2.json` are kserve inputs.
+
+### Restore image
+
+```python
+import json
+from PIL import Image
+import numpy as np
+
+# read file
+with open('example.json', 'r') as myfile:
+    data=myfile.read()
+
+# parse file
+json_data = json.loads(data)
+new_image = Image.fromarray(np.array(json.loads(json_data["predictions"]), dtype='uint8'))
+```
--- examples/diffusers/stable_diffusion_handler.py
+++ examples/diffusers/stable_diffusion_handler.py
@@ -2,30 +2,28 @@ import logging
 import zipfile
 from abc import ABC
 
+import diffusers
+import numpy as np
 import torch
-import transformers
 from diffusers import StableDiffusionPipeline
-from torchvision import transforms
 
 from ts.torch_handler.base_handler import BaseHandler
 
 logger = logging.getLogger(__name__)
-logger.info("Transformers version %s", transformers.__version__)
+logger.info("Diffusers version %s", diffusers.__version__)
 
 
-class TransformersSeqClassifierHandler(BaseHandler, ABC):
+class DiffusersHandler(BaseHandler, ABC):
     """
-    Transformers handler class for sequence, token classification and question answering.
+    Diffusers handler class for text to image generation.
     """
 
     def __init__(self):
-        super(TransformersSeqClassifierHandler, self).__init__()
         self.initialized = False
 
     def initialize(self, ctx):
-        """In this initialize function, the BERT model is loaded and
-        the Layer Integrated Gradients Algorithm for Captum Explanations
-        is initialized here.
+        """In this initialize function, the Stable Diffusion model is loaded and
+        initialized here.
         Args:
             ctx (context): It is a JSON Object containing information
             pertaining to the model artefacts parameters.
@@ -51,15 +49,15 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         self.initialized = True
 
     def preprocess(self, requests):
-        """Basic text preprocessing, based on the user's chocie of application mode.
+        """Basic text preprocessing, of the user's prompt.
         Args:
             requests (str): The Input data in the form of text is passed on to the preprocess
             function.
         Returns:
-            list : The preprocess function returns a list of Tensor for the size of the word tokens.
+            list : The preprocess function returns a list of prompts.
         """
         inputs = []
-        for idx, data in enumerate(requests):
+        for _, data in enumerate(requests):
             input_text = data.get("data")
             if input_text is None:
                 input_text = data.get("body")
@@ -70,37 +68,29 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         return inputs
 
     def inference(self, inputs):
-        """Predict the class (or classes) of the received text using the
-        serialized transformers checkpoint.
+        """Generates the image relevant to the received text.
         Args:
-            input_batch (list): List of Text Tensors from the pre-process function is passed here
+            input_batch (list): List of Text from the pre-process function is passed here
         Returns:
-            list : It returns a list of the predicted value for the input text
+            list : It returns a list of the generate images for the input text
         """
-        inferences = []
         # Handling inference for sequence_classification.
         generator = torch.Generator("cuda").manual_seed(1024)
-        image = self.pipe(
+        inferences = self.pipe(
             inputs, guidance_scale=7.5, num_inference_steps=50, generator=generator
-        ).images[0]
-        inferences.append(image)
+        ).images
 
-        logger.info("Generated image: '%s'", image)
+        logger.info("Generated image: '%s'", inferences)
         return inferences
 
     def postprocess(self, inference_output):
-        """Post Process Function converts the predicted response into Torchserve readable format.
+        """Post Process Function converts the generated image into Torchserve readable format.
         Args:
-            inference_output (list): It contains the predicted response of the input text.
+            inference_output (list): It contains the generated image of the input text.
         Returns:
-            (list): Returns a list of the Predictions and Explanations.
+            (list): Returns a list of the images.
         """
         images = []
-        transform = transforms.Compose(
-            [
-                transforms.ToTensor(),
-            ]
-        )
         for image in inference_output:
-            images.append(transform(image))
+            images.append(np.array(image).tolist())
         return images
