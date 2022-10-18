6570e5121f9601e532a216441d7bbdf3626779ae
lxning <lninga@amazon.com>
2022-10-18 14:13:12 -0700

    add clang-format

--- cpp/src/backends/process/model_worker.hh
+++ cpp/src/backends/process/model_worker.hh
@@ -2,15 +2,16 @@
 #define TS_CPP_BACKENDS_PROCESS_MODEL_WORKER_HH_
 
 #include <arpa/inet.h>
-#include <cstdio>
-#include <filesystem>
 #include <netinet/in.h>
-#include <string>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <tuple>
 #include <unistd.h>
+
+#include <cstdio>
+#include <filesystem>
 #include <memory>
+#include <string>
+#include <tuple>
 
 #include "src/backends/core/backend.hh"
 #include "src/backends/protocol/otf_message.hh"
@@ -20,56 +21,54 @@
 #include "src/utils/model_archive.hh"
 
 namespace torchserve {
-  class SocketServer {
-    public:
-    static SocketServer GetInstance() {
-      static SocketServer instance;
-      return instance;
-    };
-    ~SocketServer() {
-       if (server_socket_ >= 0) {
-          close(server_socket_);
-       }
-     };
-
-    void Initialize(
-      const std::string& socket_type,
-      const std::string& socket_name,
-      const std::string& host_addr,
-      const std::string& port_num,
-      const torchserve::Manifest::RuntimeType& runtime_type,
-      torchserve::DeviceType device_type,
-      const std::string& model_dir);
-    
-    void Run();
-
-    private:
-    SocketServer() {};
-    bool CreateBackend(
-      const torchserve::Manifest::RuntimeType& runtime_type,
-      const std::string& model_dir);
-
-    // TODO; impl.
-    //short MAX_FAILURE_THRESHOLD = 5;
-    //float SOCKET_ACCEPT_TIMEOUT = 30.0f;
-    int server_socket_;
-    std::string socket_type_;
-    std::string socket_name_;
-    ushort port_;
-    std::shared_ptr<torchserve::Backend> backend_;
+class SocketServer {
+ public:
+  static SocketServer GetInstance() {
+    static SocketServer instance;
+    return instance;
+  };
+  ~SocketServer() {
+    if (server_socket_ >= 0) {
+      close(server_socket_);
+    }
   };
 
-  class SocketModelWorker {
-    public:
-    SocketModelWorker(int client_socket, std::shared_ptr<torchserve::Backend> backend) : 
-    client_socket_(client_socket), backend_(backend) {};
-    ~SocketModelWorker() = default;
+  void Initialize(const std::string& socket_type,
+                  const std::string& socket_name, const std::string& host_addr,
+                  const std::string& port_num,
+                  const torchserve::Manifest::RuntimeType& runtime_type,
+                  torchserve::DeviceType device_type,
+                  const std::string& model_dir);
 
-    [[noreturn]] void Run();
+  void Run();
 
-    private:
-    Socket client_socket_;
-    std::shared_ptr<torchserve::Backend> backend_;
-  };
-} // namespace torchserve
-#endif // TS_CPP_BACKENDS_PROCESS_MODEL_WORKER_HH_
+ private:
+  SocketServer(){};
+  bool CreateBackend(const torchserve::Manifest::RuntimeType& runtime_type,
+                     const std::string& model_dir);
+
+  // TODO; impl.
+  // short MAX_FAILURE_THRESHOLD = 5;
+  // float SOCKET_ACCEPT_TIMEOUT = 30.0f;
+  int server_socket_;
+  std::string socket_type_;
+  std::string socket_name_;
+  ushort port_;
+  std::shared_ptr<torchserve::Backend> backend_;
+};
+
+class SocketModelWorker {
+ public:
+  SocketModelWorker(int client_socket,
+                    std::shared_ptr<torchserve::Backend> backend)
+      : client_socket_(client_socket), backend_(backend){};
+  ~SocketModelWorker() = default;
+
+  [[noreturn]] void Run();
+
+ private:
+  Socket client_socket_;
+  std::shared_ptr<torchserve::Backend> backend_;
+};
+}  // namespace torchserve
+#endif  // TS_CPP_BACKENDS_PROCESS_MODEL_WORKER_HH_
--- cpp/src/backends/process/model_worker_socket.cc
+++ cpp/src/backends/process/model_worker_socket.cc
@@ -1,9 +1,10 @@
-#include <iostream>
-#include <memory>
 #include <gflags/gflags.h>
 
-#include "src/utils/logging.hh"
+#include <iostream>
+#include <memory>
+
 #include "src/backends/process/model_worker.hh"
+#include "src/utils/logging.hh"
 
 DEFINE_string(socket_type, "tcp", "socket type");
 DEFINE_string(socket_name, "", "socket name for uds");
@@ -14,7 +15,8 @@ DEFINE_string(device_type, "cpu", "cpu, or gpu");
 // TODO: discuss multiple backends support
 DEFINE_string(model_dir, "", "model path");
 // TODO: change to file based config
-DEFINE_string(logger_config_path, "./_build/resources/logging.config", "Logging config file path");
+DEFINE_string(logger_config_path, "./_build/resources/logging.config",
+              "Logging config file path");
 
 int main(int argc, char* argv[]) {
   try {
@@ -22,14 +24,14 @@ int main(int argc, char* argv[]) {
     torchserve::Logger::InitLogger(FLAGS_logger_config_path);
 
     torchserve::SocketServer server = torchserve::SocketServer::GetInstance();
-    server.Initialize(FLAGS_socket_type, FLAGS_socket_name, 
-    FLAGS_host, FLAGS_port, FLAGS_runtime_type, FLAGS_device_type, FLAGS_model_dir);
+    server.Initialize(FLAGS_socket_type, FLAGS_socket_name, FLAGS_host,
+                      FLAGS_port, FLAGS_runtime_type, FLAGS_device_type,
+                      FLAGS_model_dir);
 
     server.Run();
 
     gflags::ShutDownCommandLineFlags();
-  } catch(...) {
+  } catch (...) {
     std::cout << "cpp backend failed to start\n";
   }
-
 }
