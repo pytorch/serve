54286c94cd80c8b773311406592f430cd06f93d0
Haoran Peng <haoranpeng@meta.com>
2022-11-06 20:36:46 +0000

    do not raise

--- ts/utils/redis_cache.py
+++ ts/utils/redis_cache.py
@@ -2,7 +2,12 @@ import logging
 import pickle
 from functools import wraps
 
-import redis
+try:
+    import redis
+
+    _has_redis = True
+except ImportError:
+    _has_redis = False
 
 from ts.context import Context
 
@@ -25,14 +30,28 @@ def _no_op_decorator(func):
 
 
 def handler_cache(host, port, db, maxsize=128):
+    """Decorator for handler's handle() method that cache input/output to a redis database.
+
+    A typical usage would be:
+
+    class SomeHandler(BaseHandler):
+        def __init__(self):
+            ...
+            self.handle = handler_cache(host='localhost', port=6379, db=0, maxsize=128)(self.handle)
+
+    The user should ensure that both the input and the output can be pickled.
+    """
+    if not _has_redis:
+        logging.error(f"Cannot import redis, try pip install redis.")
+        return _no_op_decorator
     r = redis.Redis(host=host, port=port, db=db)
     try:
         r.ping()
-    except ConnectionError as e:
-        logging.info(
-            f"Cannot connect to a redis server, ensure a server is running on {host}:{port}"
+    except redis.exceptions.ConnectionError:
+        logging.error(
+            f"Cannot connect to a redis server, ensure a server is running on {host}:{port}."
         )
-        raise e
+        return _no_op_decorator
 
     def decorating_function(func):
         @wraps(func)
