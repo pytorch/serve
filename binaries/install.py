8c020f8e889696e58e715a783486f3a796519e9c
Apoorv Vyas <vyasapoorv@fb.com>
2022-11-08 15:37:12 -0800

    [Bootcamp] Replace os.system() by subprocess.run()
    Relplaces os.system() calls in binaries/install.py by
    subprocess.run()

--- binaries/install.py
+++ binaries/install.py
@@ -16,14 +16,16 @@ def install():
         channel_dir = os.path.abspath(os.path.join(REPO_ROOT, "binaries", "conda", "output"))
         conda_cmd = f"conda install --channel {channel_dir} -y torchserve torch-model-archiver"
         print(f"## In directory: {os.getcwd()} | Executing command: {conda_cmd}")
-        install_exit_code = subprocess.run(conda_cmd)
+        install_process = subprocess.run(conda_cmd, shell=True)
+        install_exit_code = install_process.returncode
     else:
         print("## Using pip to install torchserve and torch-model-archiver")
         ts_wheel = glob.glob(os.path.join(REPO_ROOT, "dist", "*.whl"))[0]
         ma_wheel = glob.glob(os.path.join(REPO_ROOT, "model-archiver", "dist", "*.whl"))[0]
         pip_cmd = f"pip install {ts_wheel} {ma_wheel}"
         print(f"## In directory: {os.getcwd()} | Executing command: {pip_cmd}")
-        install_exit_code = subprocess.run(pip_cmd)
+        install_process = subprocess.run(pip_cmd, shell=True)
+        install_exit_code = install_process.returncode
 
     if install_exit_code != 0:
         sys.exit("## Torchserve \ Model archiver Installation Failed !")
