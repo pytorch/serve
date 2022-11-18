522071e2d6baebf0ad0e106cc1f2e610d160b38a
Matthias Reso <13337103+mreso@users.noreply.github.com>
2022-11-18 06:14:40 +0000

    Remove verbose output

--- cpp/test/resources/torchscript_model/text_classifier/create_bert.py
+++ cpp/test/resources/torchscript_model/text_classifier/create_bert.py
@@ -1,16 +1,16 @@
-import requests
 import pathlib
-from argparse import ArgumentParser
-from typing import Any, List, Tuple
-
 import time
+from argparse import ArgumentParser
+from typing import Any, List
+
+import requests
 import torch
 import torch.nn as nn
+import torchtext.functional as F
 from torch import Tensor
 from torchtext import transforms
-import torchtext.functional as F
-import torchtext.transforms as T
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoModelForSequenceClassification
+
 
 class CombinedModel(nn.Module):
     def __init__(self, tokenizer: nn.Module, model: nn.Module):
@@ -25,15 +25,15 @@ class CombinedModel(nn.Module):
     def _forward(self, batch_1: List[str], batch_2: List[str]) -> Tensor:
         tokens_1: List[List[str]] = []
         tokens_2: List[List[str]] = []
-        
+
         tokens_1 = self._tokenizer._batch_encode(batch_1)
         tokens_2 = self._tokenizer._batch_encode(batch_2)
 
         lengths_1 = [len(t) for t in tokens_1]
         lengths_2 = [len(t) for t in tokens_2]
 
-        max_length = max([l1+l2 for l1, l2 in zip(lengths_1, lengths_2)])
-       
+        max_length = max([l1 + l2 for l1, l2 in zip(lengths_1, lengths_2)])
+
         tokens_1 = [[int(i) for i in o] for o in tokens_1]
         tokens_2 = [[int(i) for i in o] for o in tokens_2]
 
@@ -41,40 +41,55 @@ class CombinedModel(nn.Module):
         tokens_2 = [s + [self._seq_end] for s in tokens_2]
 
         tokens = [s1 + s2 for s1, s2 in zip(tokens_1, tokens_2)]
-        tokens = F.to_tensor(tokens, padding_value=0).to(
-            self._dummy_param.device
+        tokens = F.to_tensor(tokens, padding_value=0).to(self._dummy_param.device)
+
+        ids = [
+            (l1 + 2)
+            * [
+                0,
+            ]
+            + (l2 + 1)
+            * [
+                1,
+            ]
+            for l1, l2 in zip(lengths_1, lengths_2)
+        ]
+        masks = [
+            len(i)
+            * [
+                1,
+            ]
+            for i in ids
+        ]
+
+        ids = F.to_tensor(ids, padding_value=0).to(self._dummy_param.device)
+
+        masks = F.to_tensor(masks, padding_value=0).to(self._dummy_param.device)
+
+        return torch.softmax(
+            self._model(input_ids=tokens, token_type_ids=ids, attention_mask=masks)[0],
+            1,
         )
-
-        ids = [(l1+2)*[0,] + (l2+1)*[1,] for l1, l2 in zip(lengths_1, lengths_2)]
-        masks = [len(i)*[1,] for i in ids]
-
-        ids = F.to_tensor(ids, padding_value=0).to(
-            self._dummy_param.device
-        )
-
-        masks = F.to_tensor(masks, padding_value=0).to(
-            self._dummy_param.device
-        )
-
-        return torch.softmax(self._model(input_ids=tokens, token_type_ids=ids, attention_mask=masks)[0], 1)
 
     def forward(self, batch_1: Any, batch_2: Any) -> Tensor:
         if torch.jit.isinstance(batch_1, str) and torch.jit.isinstance(batch_2, str):
             return self._forward([batch_1], [batch_2])
-        if torch.jit.isinstance(batch_1, List[str]) and torch.jit.isinstance(batch_2, List[str]):
+        if torch.jit.isinstance(batch_1, List[str]) and torch.jit.isinstance(
+            batch_2, List[str]
+        ):
             return self._forward(batch_1, batch_2)
         else:
             raise TypeError("Input type not supported")
 
-        
-
 
 def main(args):
     tokenizer_file = args.tokenizer_file
-    
+
     LOCAL_FILE = str(pathlib.Path(__file__).parent / "vocab.txt")
     if not pathlib.Path(LOCAL_FILE).exists():
-        VOCAB_FILE = "https://huggingface.co/bert-base-cased-finetuned-mrpc/raw/main/vocab.txt"
+        VOCAB_FILE = (
+            "https://huggingface.co/bert-base-cased-finetuned-mrpc/raw/main/vocab.txt"
+        )
         response = requests.get(VOCAB_FILE)
         with open(LOCAL_FILE, "wb") as f:
             f.write(response.content)
@@ -89,13 +104,21 @@ def main(args):
     def run_model(model_, s0, s1):
         st = time.time()
         classification_logits = model_(s0, s1)
-        print(f"Execution time: {1000*(time.time() - st):.2f} ms")
-
         paraphrase_results = classification_logits.cpu().tolist()
 
-        return "\n".join([f"{round(p[1] * 100)}% paraphrase" for p in paraphrase_results])
+        if args.verbose:
+            print(f"Execution time: {1000*(time.time() - st):.2f} ms")
+            print(
+                "\n".join(
+                    [f"{round(p[1] * 100)}% paraphrase" for p in paraphrase_results]
+                )
+            )
 
-    bert = AutoModelForSequenceClassification.from_pretrained("bert-base-cased-finetuned-mrpc", torchscript=True)
+        return classification_logits
+
+    bert = AutoModelForSequenceClassification.from_pretrained(
+        "bert-base-cased-finetuned-mrpc", torchscript=True
+    )
 
     max_sequence_length = 64
 
@@ -103,26 +126,41 @@ def main(args):
         torch.zeros([1, max_sequence_length], dtype=torch.long, device=bert.device),
         torch.zeros([1, max_sequence_length], dtype=torch.long, device=bert.device),
         torch.ones([1, max_sequence_length], dtype=torch.long, device=bert.device),
-        ]
+    ]
 
     bert = torch.jit.trace(bert, dummy_input)
-    
+
     model = CombinedModel(tokenizer, bert)
 
     model.eval()
 
-    print(run_model(model, [sequence_0, sequence_0], [sequence_1, sequence_2]))
-    print(run_model(model, [sequence_0], [sequence_2]))
+    logits_eager = run_model(
+        model,
+        [sequence_0, sequence_1, sequence_2],
+        [sequence_1, sequence_2, sequence_0],
+    )
 
     jit_model = torch.jit.script(model)
-    print(run_model(jit_model, [sequence_0, sequence_0], [sequence_1, sequence_2]))
+    logits_script = run_model(
+        jit_model,
+        [sequence_0, sequence_1, sequence_2],
+        [sequence_1, sequence_2, sequence_0],
+    )
     torch.jit.save(jit_model, tokenizer_file)
 
     loaded_jit_model = torch.jit.load(tokenizer_file)
-    print(run_model(loaded_jit_model, [sequence_0, sequence_1, sequence_2], [sequence_1, sequence_2, sequence_0]))
+    logits_loaded = run_model(
+        loaded_jit_model,
+        [sequence_0, sequence_1, sequence_2],
+        [sequence_1, sequence_2, sequence_0],
+    )
+
+    assert torch.allclose(logits_eager, logits_script)
+    assert torch.allclose(logits_eager, logits_loaded)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--tokenizer-file", default="tokenizer.pt", type=str)
+    parser.add_argument("--verbose", action="store_true")
     main(parser.parse_args())
