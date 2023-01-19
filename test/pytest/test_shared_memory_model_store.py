e5a6534f6db61890edc94920ea5ac6fd33435db3
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-01-19 18:15:41 +0000

    Added comments to shared memorymodel  store

--- test/pytest/test_shared_memory_model_store.py
+++ test/pytest/test_shared_memory_model_store.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 import numpy as np
 import torch
 import torch.multiprocessing as mp
@@ -9,7 +11,14 @@ MODEL_NAME = "some_model"
 MODEL_VERSION = "v1"
 
 
+def modules_are_equal(model1: nn.Module, model2: nn.Module) -> bool:
+    params = zip(model1.parameters(), model2.parameters())
+    buffer = zip(model1.buffers(), model2.buffers())
+    return all(torch.equal(a[0], b[0]) for a, b in chain(params, buffer))
+
+
 def test_shm_model_store_simple():
+    # This test covers the basic functionallity within thew same process
     torch.manual_seed(42)
 
     shm_model_store = SharedMemoryModelStore(f"{MODEL_NAME}_{MODEL_VERSION}")
@@ -22,12 +31,12 @@ def test_shm_model_store_simple():
     )
     model.train(False)
 
+    # Round trip through store
     model = shm_model_store.set(MODEL_NAME, model)
     model2 = shm_model_store.get(MODEL_NAME)
 
-    assert all(
-        torch.equal(a[0], b[0]) for a, b in zip(model.parameters(), model2.parameters())
-    )
+    # models should be equivalent
+    assert modules_are_equal(model, model2)
 
     input_1 = torch.rand(
         10,
@@ -39,12 +48,12 @@ def test_shm_model_store_simple():
         for p in model.parameters():
             p[:] = -p[:]
 
+    # Modification should lead to different result
     output_2 = model(input_1)
     assert not torch.equal(output_1, output_2)
 
-    assert all(
-        torch.equal(a[0], b[0]) for a, b in zip(model.parameters(), model2.parameters())
-    )
+    # after modification models should still be equivalent
+    assert modules_are_equal(model, model2)
 
     input_2 = torch.rand(
         10,
@@ -53,6 +62,7 @@ def test_shm_model_store_simple():
 
 
 def test_shm_model_store_mp():
+    # This test runs a seperate process and shares the model between main and subprocess
     def func(data, terminate, setup_complete):
         torch.manual_seed(42)
         shm_model_store = SharedMemoryModelStore(f"{MODEL_NAME}_{MODEL_VERSION}")
@@ -73,15 +83,18 @@ def test_shm_model_store_mp():
 
         terminate.wait()
 
-    p1_data = mp.Value("d")
+    # Setup process and communication primitives
+    result = mp.Value("d")
     terminate = mp.Event()
     setup_complete = mp.Event()
-    p1 = mp.Process(target=func, args=(p1_data, terminate, setup_complete))
-    p1.daemon = True
 
-    p1.start()
+    subprocess = mp.Process(target=func, args=(result, terminate, setup_complete))
+    subprocess.daemon = True
+
+    subprocess.start()
     setup_complete.wait()
 
+    # Get model created in subprocess
     model_store = SharedMemoryModelStore(f"{MODEL_NAME}_{MODEL_VERSION}")
     model = model_store.get(MODEL_NAME)
 
@@ -89,7 +102,9 @@ def test_shm_model_store_mp():
 
     value = model(-torch.as_tensor(np.ones((10,), dtype=np.float32))).item()
 
+    # Signal subprocess to terminate itself and join
     terminate.set()
-    p1.join()
+    subprocess.join()
 
-    assert p1_data.value == value
+    # Model should have the same output
+    assert result.value == value
--- ts/shared_memory_model_store/shared_memory_model_store.py
+++ ts/shared_memory_model_store/shared_memory_model_store.py
@@ -1,3 +1,7 @@
+"""
+SharedMemoryModelStore is a class to exchange module between processes by storing the weights on shared memory
+"""
+
 import copy
 import pickle
 from collections import namedtuple
@@ -11,16 +15,25 @@ import torch
 import torch.distributed as dist
 import torch.nn as nn
 
+# Shared memory info contains a pointer to shared memory
+# as well as type and information necessary to recreate the tensor from it
 ShmInfo = namedtuple("ShmInfo", ["shape", "dtype", "shm"])
 
 
 def tensor_to_shm(name: str, t: torch.Tensor) -> ShmInfo:
+    """
+    This method moves the data of a tensor into shared memory
+    The returned information can be used to recreate the tnesor
+
+    :param name: Unique identifier
+    :param t: a tensor whole data will be moves to shared memory
+    :return: info about location, shape and type of data
+    """
+
     t_np = t.detach().numpy()
     d_size = np.dtype(t_np.dtype).itemsize * np.prod(t_np.shape)
-    try:
-        shm = SharedMemory(create=True, size=d_size, name=name)
-    except FileNotFoundError:
-        shm = SharedMemory(create=False, size=d_size, name=name)
+
+    shm = SharedMemory(create=True, size=d_size, name=name)
 
     t_np_shared = np.ndarray(shape=t_np.shape, dtype=t_np.dtype, buffer=shm.buf)
     t_np_shared[:] = t_np[:]
@@ -31,9 +44,17 @@ def tensor_to_shm(name: str, t: torch.Tensor) -> ShmInfo:
 
 
 def strip_model(name: str, model: nn.Module) -> Tuple:
-    arrays = []
-    shm = []
+    """
+    This method moves all parameters and buffers to shared memory
+    and return them separately to a module with stripped parameters and buffers
 
+    :param name: Unique identifier
+    :param model: module to be stripped of parameters
+    :return: empty module, the parameters and buffer in shared memory
+    """
+
+    # Move all parameters and buffers to shared memory
+    arrays = []
     for m_name, m in model.named_modules():
         params = {
             n: tensor_to_shm(f"{name}_{m_name}_{n}", t)
@@ -43,15 +64,15 @@ def strip_model(name: str, model: nn.Module) -> Tuple:
             n: tensor_to_shm(f"{name}_{m_name}_{n}", t)
             for n, t in m.named_buffers(recurse=False)
         }
-        shm += [info.shm for _, info in chain(params.items(), buffers.items())]
         arrays.append({"params": params, "buffers": buffers})
 
+    # Create empty hull of model
     model_empty = copy.deepcopy(model)
     for m in model_empty.modules():
         for n, _ in chain(m.named_parameters(), m.named_buffers()):
             setattr(m, n, None)
 
-    return model_empty, arrays, shm
+    return model_empty, arrays
 
 
 def shm_info_to_np(info: ShmInfo) -> np.ndarray:
@@ -63,6 +84,13 @@ def shm_info_to_np(info: ShmInfo) -> np.ndarray:
 
 
 def restore_model(model: nn.Module, arrays: Tuple) -> nn.Module:
+    """
+    This method fills the parameter and buffers with tensors pointing to shared memory
+
+    :param model: Module stripped of parameters
+    :param arrays: a list with information about the weights in shared memory
+    :return: None
+    """
     for (_, m), pb in zip(model.named_modules(), arrays):
         for name, info in pb["params"].items():
             m.register_parameter(
@@ -77,9 +105,9 @@ class SharedMemoryModelStore(object):
     def __init__(self, store_name: str):
         self.store_name = store_name
         self.store = dist.FileStore(str(Path("/tmp") / store_name), -1)
+        # Pointers to shared memory created within the store
         self.shm = []
         self.shm_copies = []
-        self.arrays = []
 
     def __del__(self):
         for s in self.shm_copies:
@@ -92,27 +120,30 @@ class SharedMemoryModelStore(object):
     def set(self, model_name: str, model: nn.Module) -> nn.Module:
         # assert self.store.get(model_name) is None
 
-        m_empty, arrays, shm = strip_model(f"{self.store_name}_{model_name}", model)
-        self.shm += shm
-        self.arrays += arrays
+        m_empty, arrays = strip_model(f"{self.store_name}_{model_name}", model)
 
+        # Keep records of shared memory to keep it available until deletion of store
+        for a in arrays:
+            self.shm += [
+                info.shm for _, info in chain(a["params"].items(), a["buffers"].items())
+            ]
+
+        # Move empty model and pointers to shared memory the common store
         self.store.set(model_name, pickle.dumps((m_empty, arrays)))
 
+        # Recreate model with weights in shared memory
         restore_model(m_empty, arrays)
 
         return m_empty
 
     def get(self, model_name: str) -> nn.Module:
-
         m_empty, arrays = pickle.loads(self.store.get(model_name))
 
         # We need to keep the shared memory objects in memory to use the buf memory view
-        shm = []
         for a in arrays:
-            shm += [
+            self.shm_copies += [
                 info.shm for _, info in chain(a["params"].items(), a["buffers"].items())
             ]
-        self.shm_copies += shm
 
         restore_model(m_empty, arrays)
 
