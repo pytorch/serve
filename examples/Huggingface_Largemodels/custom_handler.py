637a763dd4858d98d6387dc159cdb1c72f79514c
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-01-23 20:02:30 -0800

    remove padding for the prompt

--- examples/Huggingface_Largemodels/custom_handler.py
+++ examples/Huggingface_Largemodels/custom_handler.py
@@ -106,7 +106,6 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
             inputs = self.tokenizer.encode_plus(
                 input_text,
                 max_length=int(max_length),
-                pad_to_max_length=True,
                 add_special_tokens=True,
                 return_tensors="pt",
             )
