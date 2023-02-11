bb63ec8ce896a8ac55e672a5392852d2aae2f2b3
Ankith Gunapal <agunapal@fb.com>
2023-02-11 00:36:34 +0000

    working for mnist gpu

--- benchmarks/utils/report.py
+++ benchmarks/utils/report.py
@@ -1,9 +1,15 @@
+from collections import defaultdict
+
 import pandas as pd
+import yaml
 
 
 class Report:
-    def __init__(self, csv_file):
+    def __init__(self):
+        self.properties = defaultdict(int)
         self.throughput = 0
+        self.batch_size = 0
+        self.workers = 0
         self.total_latency_p50 = 0
         self.total_latency_p90 = 0
         self.total_latency_p99 = 0
@@ -15,24 +21,41 @@ class Report:
         self.cpu_percentage_mean = 0
         self.gpu_percentage_mean = 0
 
-        self._read_csv(csv_file)
-
-    def _read_csv(self, csv_file):
+    def read_csv(self, csv_file):
 
         df = pd.read_csv(csv_file)
         values = df.values.tolist()
-        self._populate(values[0])
+        self._populate_csv(values[0])
 
-    def _populate(self, values):
-        self.throughput = values[9]
-        self.total_latency_p50 = values[10]
-        self.total_latency_p90 = values[11]
-        self.total_latency_p99 = values[12]
-        self.model_latency_p50 = values[15]
-        self.model_latency_p90 = values[16]
-        self.model_latency_p99 = values[17]
-        self.memory_percentage_mean = values[23]
-        self.gpu_used_memory_mean = values[26]
-        self.cpu_percentage_mean = values[22]
-        self.gpu_percentage_mean = values[24]
-        print(f"Throughput {self.throughput}")
+    def read_yaml(self, yaml_file, config):
+
+        with open(yaml_file, "r") as f:
+            yaml_dict = yaml.safe_load(f)
+        self._populate_yaml(yaml_dict, config)
+
+    def _populate_yaml(self, yaml_dict, config):
+        for model, cfg in yaml_dict.items():
+
+            values = cfg["batch_size"][config["batch_size"]]
+            self.properties["deviation"] = cfg["deviation"]
+            self.properties["throughput"] = values["throughput"]
+            self.properties["total_latency_p50"] = values["total_latency_p50"]
+            self.properties["model_latency_p50"] = values["model_latency_p50"]
+            self.properties["total_latency_p90"] = values["total_latency_p90"]
+            self.properties["model_latency_p90"] = values["model_latency_p90"]
+            self.properties["total_latency_p99"] = values["total_latency_p99"]
+            self.properties["model_latency_p99"] = values["model_latency_p99"]
+
+    def _populate_csv(self, values):
+        self.properties["throughput"] = values[9]
+        self.properties["batch_size"] = values[1]
+        self.properties["total_latency_p50"] = values[10]
+        self.properties["total_latency_p90"] = values[11]
+        self.properties["total_latency_p99"] = values[12]
+        self.properties["model_latency_p50"] = values[15]
+        self.properties["model_latency_p90"] = values[16]
+        self.properties["model_latency_p99"] = values[17]
+        self.properties["memory_percentage_mean"] = values[23]
+        self.properties["gpu_used_memory_mean"] = values[26]
+        self.properties["cpu_percentage_mean"] = values[22]
+        self.properties["gpu_percentage_mean"] = values[24]
---
+++ benchmarks/validate_report.py
@@ -0,0 +1,146 @@
+import argparse
+import copy
+import os
+
+import yaml
+from auto_benchmark import load_benchmark_config
+from utils.gen_model_config_json import MODEL_CONFIG_KEY
+from utils.report import Report
+
+BENCHMARK_REPORT_CSV = "ab_report.csv"
+CWD = os.getcwd()
+VALIDATION_KEYS = [
+    "throughput",
+    "total_latency_p50",
+    "model_latency_p50",
+    "total_latency_p90",
+    "model_latency_p90",
+    "total_latency_p99",
+    "model_latency_p99",
+]
+
+
+def conv_model_yaml_dict(model_file):
+
+    with open(model_file, "r") as f:
+        yaml_dict = yaml.safe_load(f)
+
+        for model, config in yaml_dict.items():
+            for mode, mode_config in config.items():
+                model_name = mode + "_" + model
+                benchmark_config = {}
+                batch_size_list = None
+                processors = None
+                workers_list = None
+                benchmark_config["model"] = model
+                benchmark_config["mode"] = mode
+                for key, value in mode_config.items():
+                    if key == "batch_size":
+                        batch_size_list = value
+                    elif key == "processors":
+                        processors = value
+                    elif key == "workers":
+                        workers_list = value
+                    elif key in MODEL_CONFIG_KEY:
+                        benchmark_config[key] = value
+
+                batch_worker_list = []
+                for batch_size in batch_size_list:
+                    for workers in workers_list:
+                        batch_worker_list.append(
+                            {"batch_size": batch_size, "workers": workers}
+                        )
+
+                benchmark_configs = []
+                for batch_worker in batch_worker_list:
+                    benchmark_config["batch_size"] = batch_worker["batch_size"]
+                    benchmark_config["workers"] = batch_worker["workers"]
+                    benchmark_configs.append(copy.deepcopy(benchmark_config))
+
+    return benchmark_configs
+
+
+def check_if_within_range(value1, value2, threshold):
+    print(value1, value2, threshold)
+    return abs((value1 - value2) / float(value1)) <= threshold
+
+
+def validate_reports(args):
+    input_dir = args.input
+    if not os.path.isdir(input_dir):
+        print("No report generated")
+        return -1
+
+    # Read baseline reports
+    bm_config = load_benchmark_config(args.input_cfg, True, True)
+    baseline_reports = {}
+    for model in bm_config["models"]:
+        model_file = CWD + "/benchmarks/models_config/{}".format(model)
+        benchmark_configs = conv_model_yaml_dict(model_file)
+
+        for config in benchmark_configs:
+            yaml_file = CWD + "/benchmarks/models_baseline/{}".format(model)
+            report = Report()
+            report.read_yaml(yaml_file, config)
+            key = (
+                config["mode"]
+                + "_"
+                + config["model"]
+                + "_w"
+                + str(config["workers"])
+                + "_b"
+                + str(config["batch_size"])
+            )
+            baseline_reports[key] = report
+    print(baseline_reports)
+    # Read generated reports
+    generated_reports = {}
+    for subdir in sorted(os.listdir(input_dir)):
+        if os.path.isdir(os.path.join(input_dir, subdir)):
+            csv_file = os.path.join(input_dir, subdir, BENCHMARK_REPORT_CSV)
+            report = Report()
+            report.read_csv(csv_file)
+            generated_reports[subdir] = report
+    print(generated_reports)
+
+    for model, report in generated_reports.items():
+
+        for key in VALIDATION_KEYS:
+
+            if not check_if_within_range(
+                report.properties[key],
+                baseline_reports[model].properties[key],
+                baseline_reports[model].properties["deviation"],
+            ):
+                print("Error ", key)
+                return -1
+    print("Success")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--input",
+        action="store",
+        help="the dir of a list of model benchmark result subdir ",
+    )
+
+    parser.add_argument(
+        "--input_cfg",
+        action="store",
+        help="benchmark config yaml file path",
+    )
+
+    parser.add_argument(
+        "--output",
+        action="store",
+        help="the file path of final report ",
+    )
+
+    arguments = parser.parse_args()
+    validate_reports(arguments)
+
+
+if __name__ == "__main__":
+    main()
