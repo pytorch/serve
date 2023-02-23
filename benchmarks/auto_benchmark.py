62efbff45527f4ad62c82cbaf13c37d4949ab96f
min-jean-cho <min.jean.cho@intel.com>
2023-02-22 20:34:43 -0800

    fix

--- benchmarks/auto_benchmark.py
+++ benchmarks/auto_benchmark.py
@@ -175,7 +175,7 @@ def run_benchmark(bm_config):
             shutil.rmtree(TS_LOGS_PATH, ignore_errors=True)
             shutil.rmtree(BENCHMARK_TMP_PATH, ignore_errors=True)
             
-            execute("python -c 'import torch; print(torch.get_num_threads())'", wait=True)
+            execute("python -c 'import torch; print(\"OMP_NUM_THREADS: \", torch.get_num_threads())'", wait=True)
             
             cmd = (
                 "python ./benchmarks/benchmark-ab.py --tmp_dir /tmp --report_location /tmp --config_properties "
