2263e4fb8e81278f4757a3b35b2c2a1b7fa8545e
jagadeesh <jagadeeshj@ideas2it.com>
2023-03-12 23:45:40 +0530

    fix: kserve tornado to fastapi migration
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>

--- kubernetes/kserve/kserve_wrapper/TorchserveModel.py
+++ kubernetes/kserve/kserve_wrapper/TorchserveModel.py
@@ -1,24 +1,22 @@
 """ The torchserve side inference end-points request are handled to
     return a KServe side response """
-import orjson
 import logging
 import pathlib
 from typing import Dict, Union
 
 import kserve
-import httpx
+import orjson
 from httpx import HTTPStatusError
-from kserve.model import Model as Model
 from kserve.errors import ModelMissingError
-from kserve.protocol.infer_type import InferRequest, InferResponse
+from kserve.model import Model as Model
+from kserve.protocol.infer_type import InferRequest
 
 logging.basicConfig(level=kserve.constants.KSERVE_LOGLEVEL)
 
+PREDICTOR_URL_FORMAT = "http://{0}/predictions/{1}"
+EXPLAINER_URL_FORMAT = "http://{0}/explanations/{1}"
 REGISTER_URL_FORMAT = "{0}/models?initial_workers=1&url={1}"
 UNREGISTER_URL_FORMAT = "{0}/models/{1}"
-
-PREDICTOR_URL_FORMAT = "http://{0}/v1/models/{1}:predict"
-EXPLAINER_URL_FORMAT = "http://{0}/v1/models/{1}:explain"
 
 
 class TorchserveModel(Model):
@@ -51,101 +49,38 @@ class TorchserveModel(Model):
         self.management_address = management_address
         self.model_dir = model_dir
 
-        logging.info("kfmodel Predict URL set to %s", self.predictor_host)
+        logging.info("Predict URL set to %s", self.predictor_host)
         self.explainer_host = self.predictor_host
-        logging.info("kfmodel Explain URL set to %s", self.explainer_host)
+        logging.info("Explain URL set to %s", self.explainer_host)
 
-    async def predict(self, payload: Union[Dict, InferRequest], headers: Dict) -> Dict:
-        """The predict method is called when we hit the inference endpoint and handles
-        the inference request and response from the Torchserve side and passes it on
-        to the KServe side.
+    async def _http_predict(
+        self, payload: Union[Dict, InferRequest], headers: Dict[str, str] = None
+    ) -> Dict:
+        predict_url = PREDICTOR_URL_FORMAT.format(self.predictor_host, self.name)
 
-        Args:
-            payload (Dict|InferRequest): Input payload from the http client side.
-            headers (Dict): Request headers.
-
-        Raises:
-            NotImplementedError: If the predictor host on the KServe side is not
-                                 available.
-
-            HTTPStatusError: If there is a bad response from the http client.
-
-        Returns:
-            Dict: The Response from the input from the inference endpoint.
-        """
-        if not self.predictor_host:
-            raise NotImplementedError
+        # Adjusting headers. Inject content type if not exist.
+        # Also, removing host, as the header is the one passed to transformer and contains transformer's host
+        predict_headers = {"Content-Type": "application/json"}
+        if headers is not None:
+            if "x-request-id" in headers:
+                predict_headers["x-request-id"] = headers["x-request-id"]
+            if "x-b3-traceid" in headers:
+                predict_headers["x-b3-traceid"] = headers["x-b3-traceid"]
         if isinstance(payload, InferRequest):
             payload = payload.to_rest()
         data = orjson.dumps(payload)
-        logging.debug("kfmodel predict request is %s", data)
-        logging.info("PREDICTOR_HOST : %s", self.predictor_host)
-        predict_headers = {'Content-Type': 'application/json; charset=UTF-8'}
-        if headers is not None:
-            if 'X-Request-Id' in headers:
-                predict_headers['X-Request-Id'] = headers['X-Request-Id']
-            if 'X-B3-Traceid' in headers:
-                predict_headers['X-B3-Traceid'] = headers['X-B3-Traceid']
         response = await self._http_client.post(
-            PREDICTOR_URL_FORMAT.format(self.predictor_host, self.name),
-            timeout=self.timeout,
-            headers=predict_headers,
-            content=data
+            predict_url, timeout=self.timeout, headers=predict_headers, content=data
         )
         if not response.is_success:
             message = (
                 "{error_message}, '{0.status_code} {0.reason_phrase}' for url '{0.url}'"
             )
             error_message = ""
-            if "content-type" in response.headers and response.headers["content-type"] == "application/json":
-                error_message = response.json()
-                if "error" in error_message:
-                    error_message = error_message["error"]
-            message = message.format(response, error_message=error_message)
-            raise HTTPStatusError(message, request=response.request, response=response)
-        return orjson.loads(response.content)
-
-    async def explain(self, payload: Dict, headers: Dict) -> Dict:
-        """The predict method is called when we hit the explain endpoint and handles the
-        explain request and response from the Torchserve side and passes it on to the
-        KServe side.
-
-        Args:
-            payload (Dict): Input payload from the http client side.
-            headers (Dict): Request headers.
-
-        Raises:
-            NotImplementedError: If the explainer host on the KServe side is not
-                                 available.
-
-            HTTPStatusError: If there is a bad response from the http client.
-
-        Returns:
-            Dict: The Response from the input from the explain endpoint.
-        """
-        if self.explainer_host is None:
-            raise NotImplementedError
-        data = orjson.dumps(payload)
-        logging.info("kfmodel explain request is %s", data)
-        logging.info("EXPLAINER_HOST : %s", self.explainer_host)
-        predict_headers = {"Content-Type": "application/json; charset=UTF-8"}
-        if headers is not None:
-            if 'X-Request-Id' in headers:
-                predict_headers['X-Request-Id'] = headers['X-Request-Id']
-            if 'X-B3-Traceid' in headers:
-                predict_headers['X-B3-Traceid'] = headers['X-B3-Traceid']
-        response = await self._http_client.post(
-            EXPLAINER_URL_FORMAT.format(self.explainer_host, self.name),
-            timeout=self.timeout,
-            headers=predict_headers,
-            content=data,
-        )
-        if not response.is_success:
-            message = (
-                "{error_message}, '{0.status_code} {0.reason_phrase}' for url '{0.url}'"
-            )
-            error_message = ""
-            if "content-type" in response.headers and response.headers["content-type"] == "application/json":
+            if (
+                "content-type" in response.headers
+                and response.headers["content-type"] == "application/json"
+            ):
                 error_message = response.json()
                 if "error" in error_message:
                     error_message = error_message["error"]
