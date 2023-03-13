7a8482fe4c164713bc00b54b7bc413b8ecc680d3
jagadeesh <jagadeeshj@ideas2it.com>
2023-03-13 22:51:27 +0530

    fix: kserve fastapi migration issues
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>

--- kubernetes/kserve/kf_request_json/v2/mnist/tobytes.py
+++ kubernetes/kserve/kf_request_json/v2/mnist/tobytes.py
@@ -1,6 +1,6 @@
+import argparse
 import base64
 import json
-import argparse
 import uuid
 
 parser = argparse.ArgumentParser()
@@ -10,11 +10,20 @@ args = parser.parse_args()
 image = open(args.filename, "rb")  # open binary file in read mode
 image_read = image.read()
 image_64_encode = base64.b64encode(image_read)
-bytes_array = image_64_encode.decode("utf-8")
+bytes_array = list(image_64_encode.decode("utf-8"))
 request = {
-    "inputs": [{"name": str(uuid.uuid4()), "shape": -1, "datatype": "BYTES", "data": bytes_array}]
+    "inputs": [
+        {
+            "name": str(uuid.uuid4()),
+            "shape": [-1],
+            "datatype": "BYTES",
+            "data": bytes_array,
+        }
+    ]
 }
 
-result_file = "{filename}.{ext}".format(filename=str(args.filename).split(".")[0], ext="json")
+result_file = "{filename}.{ext}".format(
+    filename=str(args.filename).split(".")[0], ext="json"
+)
 with open(result_file, "w") as outfile:
     json.dump(request, outfile, indent=4, sort_keys=True)
