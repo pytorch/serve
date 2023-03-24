fd6f7c5815b2aeee6796d449e6726a57eb6c634e
lxning <lninga@amazon.com>
2023-03-24 00:12:59 -0700

    add singleton cache

--- frontend/server/src/main/java/org/pytorch/serve/metrics/MetricCache.java
+++ frontend/server/src/main/java/org/pytorch/serve/metrics/MetricCache.java
@@ -1,11 +1,35 @@
 package org.pytorch.serve.metrics;
 
+import org.pytorch.serve.util.ConfigManager;
+import org.pytorch.serve.wlm.WorkerLifeCycle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
 
 public class MetricCache {
-    Map<String, IMetric> metricsFrontend;
-    ConcurrentMap<String, IMetric> metricsBackend;
+    private static final Logger logger = LoggerFactory.getLogger(MetricCache.class);
+    
+    private static final MetricCache METRIC_CACHE = new MetricCache();
+
+    private MetricCache() {
+        metricsFrontend = new HashMap<String, IMetric>();
+        metricsBackend = new ConcurrentMap<String, IMetric>();
+    }
+
+    public static MetricCache getInstance() {
+        return METRIC_CACHE;
+    }
+
+    private static MetricCache instance;
+    private Map<String, IMetric> metricsFrontend;
+    private ConcurrentMap<String, IMetric> metricsBackend;
 
     public void addMetricFrontend(String metricName, IMetric metric);
 
