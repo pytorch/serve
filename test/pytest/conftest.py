ab52ab54cf3d5b0401f33d5d96eac35c9435dea6
agunapal <agunapal@berkeley.edu>
2023-03-27 23:53:16 +0000

    temp comment

--- test/pytest/conftest.py
+++ test/pytest/conftest.py
@@ -15,8 +15,16 @@ collect_ignore.append("test_example_torchrec_dlrm.py")
 collect_ignore.append("test_example_near_real_time_video.py")
 collect_ignore.append("test_dali_preprocess.py")
 collect_ignore.append("test_example_dcgan.py")
-collect_ignore.append("test_example*.py")
-collect_ignore.append("test_gRPC*.py")
+collect_ignore.append("test_example_torchrec_dlrm.py")
+collect_ignore.append("test_example_scriptable_tokenzier.py")
+collect_ignore.append("test_example_intel_extension_for_pytorch.py")
+collect_ignore.append("test_gRPC_inference_api.py")
+collect_ignore.append("test_handler.py")
+collect_ignore.append("test_metrics.py")
+collect_ignore.append("test_metrics_kf.py")
+collect_ignore.append("test_model_archiver.py")
+collect_ignore.append("test_onnx.py")y
+collect_ignore.append("test_pytorch_profiler.py")
 
 
 @pytest.fixture(scope="module")
