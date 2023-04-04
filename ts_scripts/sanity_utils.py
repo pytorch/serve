70c57128126347c5dbe5a1988cd12e479d79f051
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-04-04 03:26:56 +0000

    Refactored model to validate into json file

--- ts_scripts/sanity_utils.py
+++ ts_scripts/sanity_utils.py
@@ -1,18 +1,22 @@
 import glob
+import json
 import os
 import subprocess
 import sys
+from pathlib import Path
 
 import nvgpu
 
 from ts_scripts import marsgen as mg
-
-REPO_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
-sys.path.append(REPO_ROOT)
-
 from ts_scripts import tsutils as ts
 from ts_scripts import utils
 from ts_scripts.tsutils import generate_grpc_client_stubs
+
+REPO_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+sys.path.append(REPO_ROOT)
+MODELS_CONFIG_FILE_PATH = Path(__file__).parent.joinpath(
+    "configs", "sanity_models.json"
+)
 
 
 def run_markdown_link_checker():
@@ -41,98 +45,29 @@ def validate_model_on_gpu():
     return model_loaded
 
 
+def load_model_to_validate():
+    with open(MODELS_CONFIG_FILE_PATH) as f:
+        model_list = json.load(f)
+        assert isinstance(model_list, list)
+
+    print(model_list)
+    models_to_validate = {}
+    for m in model_list:
+        models_to_validate[m["name"]] = m
+
+    # models_to_validate = {m["name"]: m for m in model_list}
+    assert len(models_to_validate) == len(
+        model_list
+    ), "Model names are expected to be unique"
+    return models_to_validate
+
+
 def test_sanity():
     generate_grpc_client_stubs()
 
     print("## Started sanity tests")
 
-    resnet18_model = {
-        "name": "resnet-18",
-        "inputs": ["examples/image_classifier/kitten.jpg"],
-        "handler": "image_classifier",
-    }
-
-    bert_token_classification_no_torchscript_model = {
-        "name": "bert_token_classification_no_torchscript",
-        "inputs": [
-            "examples/Huggingface_Transformers/Token_classification_artifacts/sample_text.txt"
-        ],
-        "handler": "custom",
-    }
-
-    bert_seqc_without_torchscript_model = {
-        "name": "bert_seqc_without_torchscript",
-        "inputs": [
-            "examples/Huggingface_Transformers/Seq_classification_artifacts/sample_text.txt"
-        ],
-        "handler": "custom",
-    }
-
-    models_to_validate = [
-        {
-            "name": "fastrcnn",
-            "inputs": ["examples/object_detector/persons.jpg"],
-            "handler": "object_detector",
-        },
-        {
-            "name": "fcn_resnet_101",
-            "inputs": [
-                "docs/images/blank_image.jpg",
-                "examples/image_segmenter/persons.jpg",
-            ],
-            "handler": "image_segmenter",
-        },
-        {
-            "name": "my_text_classifier_v4",
-            "inputs": ["examples/text_classification/sample_text.txt"],
-            "handler": "text_classification",
-        },
-        resnet18_model,
-        {
-            "name": "my_text_classifier_scripted_v3",
-            "inputs": ["examples/text_classification/sample_text.txt"],
-            "handler": "text_classification",
-        },
-        {
-            "name": "alexnet_scripted",
-            "inputs": ["examples/image_classifier/kitten.jpg"],
-            "handler": "image_classifier",
-        },
-        {
-            "name": "fcn_resnet_101_scripted",
-            "inputs": ["examples/image_segmenter/persons.jpg"],
-            "handler": "image_segmenter",
-        },
-        {
-            "name": "distill_bert_qa_eager",
-            "inputs": [
-                "examples/Huggingface_Transformers/QA_artifacts/sample_text.txt"
-            ],
-            "handler": "custom",
-        },
-        {
-            "name": "bert_token_classification_no_torchscript",
-            "inputs": [
-                "examples/Huggingface_Transformers/Token_classification_artifacts/sample_text.txt"
-            ],
-            "handler": "custom",
-        },
-        {
-            "name": "bert_seqc_without_torchscript",
-            "inputs": [
-                "examples/Huggingface_Transformers/Seq_classification_artifacts/sample_text.txt"
-            ],
-            "handler": "custom",
-        },
-    ]
-
-    if not sys.platform.startswith("win"):
-        models_to_validate.extend(
-            (
-                bert_token_classification_no_torchscript_model,
-                bert_seqc_without_torchscript_model,
-            )
-        )
+    models_to_validate = load_model_to_validate()
 
     ts_log_file = os.path.join("logs", "ts_console.log")
     is_gpu_instance = utils.is_gpu_instance()
@@ -150,8 +85,7 @@ def test_sanity():
     if not started:
         sys.exit(1)
 
-    for model in models_to_validate:
-        model_name = model["name"]
+    for model_name, model in models_to_validate.items():
         model_inputs = model["inputs"]
         model_handler = model["handler"]
 
@@ -226,7 +160,7 @@ def test_sanity():
                 )
 
         # skip unregistering resnet-18 model to test snapshot feature with restart
-        if model != resnet18_model:
+        if model_name != "resnet-18":
             response = ts.unregister_model(model_name)
             if response and response.status_code == 200:
                 print(f"## Successfully unregistered {model_name}")
@@ -239,6 +173,8 @@ def test_sanity():
     stopped = ts.stop_torchserve()
     if not stopped:
         sys.exit(1)
+
+    resnet18_model = models_to_validate["resnet-18"]
 
     # Restarting torchserve
     # This should restart with the generated snapshot and resnet-18 model should be automatically registered
