c1eeaa644b404efa39311253beab65eb65570146
lxning <lninga@amazon.com>
2023-04-03 21:01:17 -0700

    precommit fmt

---
+++ examples/large_models/deepspeed/bloom/custom_handler.py
@@ -0,0 +1,117 @@
+import logging
+import os
+from abc import ABC
+
+import torch
+import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+from ts.context import Context
+from ts.torch_handler.distributed.base_deepspeed import BaseDeepSpeedHandler
+
+logger = logging.getLogger(__name__)
+logger.info("Transformers version %s", transformers.__version__)
+
+TORCH_DTYPES = {
+    "float16": torch.float16,
+    "float32": torch.float32,
+    "float64": torch.float64,
+}
+
+
+class TransformersSeqClassifierHandler(BaseDeepSpeedHandler, ABC):
+    """
+    Transformers handler class for sequence, token classification and question answering.
+    """
+
+    def __init__(self):
+        super(TransformersSeqClassifierHandler, self).__init__()
+        self.initialized = False
+
+    def initialize(self, ctx: Context):
+        model_dir = ctx.system_properties.get("model_dir")
+        self.device = int(os.getenv("LOCAL_RANK", 0))
+        model = AutoModelForCausalLM.from_pretrained(model_dir, use_cache=False)
+        tokenizer = AutoTokenizer.from_pretrained(model_dir, return_tensors="pt")
+        self.model = pipeline(model=model, tokenizer=tokenizer, device=self.device)
+        super().initialize(ctx)
+
+    def preprocess(self, requests):
+        """Basic text preprocessing, based on the user's chocie of application mode.
+        Args:
+            requests (str): The Input data in the form of text is passed on to the preprocess
+            function.
+        Returns:
+            list : The preprocess function returns a list of Tensor for the size of the word tokens.
+        """
+        input_ids_batch = None
+        attention_mask_batch = None
+        for idx, data in enumerate(requests):
+            input_text = data.get("data")
+            if input_text is None:
+                input_text = data.get("body")
+            if isinstance(input_text, (bytes, bytearray)):
+                input_text = input_text.decode("utf-8")
+
+            max_length = self.setup_config["max_length"]
+            logger.info("Received text: '%s'", input_text)
+
+            inputs = self.tokenizer.encode_plus(
+                input_text,
+                max_length=int(max_length),
+                pad_to_max_length=True,
+                add_special_tokens=True,
+                return_tensors="pt",
+            )
+
+            input_ids = inputs["input_ids"].to(self.device)
+            attention_mask = inputs["attention_mask"].to(self.device)
+            # making a batch out of the recieved requests
+            # attention masks are passed for cases where input tokens are padded.
+            if input_ids.shape is not None:
+                if input_ids_batch is None:
+                    input_ids_batch = input_ids
+                    attention_mask_batch = attention_mask
+                else:
+                    input_ids_batch = torch.cat((input_ids_batch, input_ids), 0)
+                    attention_mask_batch = torch.cat(
+                        (attention_mask_batch, attention_mask), 0
+                    )
+        return (input_ids_batch, attention_mask_batch)
+
+    def inference(self, input_batch):
+        """Predict the class (or classes) of the received text using the
+        serialized transformers checkpoint.
+        Args:
+            input_batch (list): List of Text Tensors from the pre-process function is passed here
+        Returns:
+            list : It returns a list of the predicted value for the input text
+        """
+        (input_ids_batch, _) = input_batch
+        inferences = []
+        input_ids_batch = input_ids_batch.to(self.device)
+        outputs = self.model.generate(
+            input_ids_batch,
+            do_sample=True,
+            max_new_tokens=int(self.setup_config["max_length"]),
+            top_p=0.95,
+            top_k=60,
+        )
+        for i, _ in enumerate(outputs):
+            inferences.append(
+                self.tokenizer.decode(outputs[i], skip_special_tokens=True)
+            )
+
+        logger.info("Generated text: '%s'", inferences)
+
+        print("Generated text", inferences)
+        return inferences
+
+    def postprocess(self, inference_output):
+        """Post Process Function converts the predicted response into Torchserve readable format.
+        Args:
+            inference_output (list): It contains the predicted response of the input text.
+        Returns:
+            (list): Returns a list of the Predictions and Explanations.
+        """
+        return inference_output
---
+++ ts/torch_handler/distributed/base_deepspeed.py
@@ -0,0 +1,21 @@
+from abc import ABC
+
+from ts.context import Context
+from ts.handler_utils.distributed.deepspeed import get_ds_engine
+from ts.torch_handler.base_handler import BaseHandler, logger
+
+
+class BaseDeepSpeedHandler(BaseHandler, ABC):
+    """
+    Base default DeepSpeed handler.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.initialized = False
+
+    def initialize(self, ctx: Context):
+        ds_engine = get_ds_engine(self.model, ctx)
+        self.model = ds_engine.module
+        self.initialized = True
+        logger.debug("Model %s loaded successfully", ctx.model_name)
