a49e5be5d1388dbc31e9e46ca8bdd93af410c2e0
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-04-06 00:09:58 +0000

    clean up

--- ts/handler_utils/distributed/pt_pippy.py
+++ ts/handler_utils/distributed/pt_pippy.py
@@ -6,20 +6,25 @@ import pippy
 import torch
 import torch.distributed.rpc as rpc
 from pippy import split_into_equal_size
-from pippy.hf import PiPPyHFTracer
+from pippy.hf import PiPPyHFTracer, inject_pipeline_forward
 
 logger = logging.getLogger(__name__)
 
 
 def initialize_rpc_workers(local_rank, world_size, ctx):
+    # Get RPC configuration options from model YAML config
     rpc_timeout = ctx.model_yaml_config["pippy"]["rpc_timeout"]
     options = rpc.TensorPipeRpcBackendOptions(
         num_worker_threads=512, rpc_timeout=rpc_timeout
     )
+
+    # Set up device mapping for RPC workers
     n_devs = torch.cuda.device_count()
     dev_id = local_rank % n_devs
     for i in range(world_size):
         options.set_device_map(f"worker{i}", {dev_id: i % n_devs})
+
+    # Initialize RPC worker
     logger.info(f"rank = {local_rank} pid/device = " f"{os.getpid()}/{dev_id}")
     rpc.init_rpc(
         f"worker{local_rank}",
@@ -29,21 +34,40 @@ def initialize_rpc_workers(local_rank, world_size, ctx):
     )
 
 
-def get_pipline_driver(model, world_size, ctx):
+def get_pipeline_driver(model, world_size, ctx):
+    """Returns a pipeline driver for the given model.
+    Args:
+        model (torch.nn.Module): The model to pipeline.
+        world_size (int): The number of pipeline stages.
+        ctx (Context): The context containing configuration information.
+    Returns:
+        torch.nn.Sequential: The pipeline driver for the model.
+    """
+    # Extract configuration parameters from the context
     chunks = ctx.model_yaml_config["pippy"]["chunks"]
     input_names = ctx.model_yaml_config["pippy"]["input_names"]
     model_type = ctx.model_yaml_config["pippy"]["model_type"]
+
+    # Set the model to evaluation mode
     model.eval()
-    split_policy = split_into_equal_size(world_size)
+
+    # Extract the concrete arguments for the model's forward method
     sig = inspect.signature(model.forward)
     concrete_args = {
         p.name: p.default for p in sig.parameters.values() if p.name not in input_names
     }
-    logger.info("initializing the model pipline")
-    if model_type == "HF":
-        tracer = PiPPyHFTracer()
-    else:
-        tracer = None
+
+    logger.info("Initializing the model pipeline")
+
+    # Create a tracer if necessary
+    tracer = PiPPyHFTracer() if model_type == "HF" else None
+
+    # Add deprecated_arguments to concrete_args if necessary
+    if model_type == "HF" and "bloom" in str(model.__class__):
+        concrete_args.setdefault("deprecated_arguments", {})
+
+    # Compile the pipeline using PiPPy
+    split_policy = split_into_equal_size(world_size)
     pipe_driver, stage_mode = pippy.all_compile(
         model,
         num_ranks=world_size,
@@ -54,6 +78,8 @@ def get_pipline_driver(model, world_size, ctx):
         concrete_args=concrete_args,
     )
 
-    # inject_pipeline_forward(model, pipe_driver)
+    # Inject the pipeline forward method if necessary
+    if model_type == "HF":
+        inject_pipeline_forward(model, pipe_driver)
 
-    return pipe_driver
+    return model
