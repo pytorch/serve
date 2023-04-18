1be4b0928b7dc52a1345e91dd20ecd60d11d334d
Mark Saroufim <marksaroufim@gmail.com>
2023-04-18 02:47:56 +0000

    lint

--- test/pytest/test_data/torch_compile/compile_handler.py
+++ test/pytest/test_data/torch_compile/compile_handler.py
@@ -2,6 +2,7 @@ import torch
 
 from ts.torch_handler.base_handler import BaseHandler
 
+
 class CompileHandler(BaseHandler):
     def __init__(self):
         super().__init__()
@@ -10,10 +11,9 @@ class CompileHandler(BaseHandler):
         super().initialize(context)
 
     def preprocess(self, data):
-        instances = data[0]['body']['instances']
+        instances = data[0]["body"]["instances"]
         input_tensor = torch.as_tensor(instances, dtype=torch.float32)
         return input_tensor
-    
 
     def postprocess(self, data):
         # Convert the output tensor to a list and return
