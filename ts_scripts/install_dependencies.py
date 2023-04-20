0c5417f04c132d45d71c253731034936be54e01c
agunapal <agunapal@ischool.berkeley.edu>
2023-04-20 20:28:26 +0000

    Refactoring dependencies

--- ts_scripts/install_dependencies.py
+++ ts_scripts/install_dependencies.py
@@ -50,18 +50,21 @@ class Common:
             # as it may reinstall the packages with different versions
             os.system("conda install -y conda-build")
 
-        self.install_torch_packages(cuda_version)
         os.system(f"{sys.executable} -m pip install -U pip setuptools")
         # developer.txt also installs packages from common.txt
         os.system(f"{sys.executable} -m pip install -U -r {requirements_file_path}")
-        # If conda is available install conda-build package
 
-        # TODO: This will run 2 installations for torch but to make this cleaner we should first refactor all of our requirements.txt into just 2 files
-        # And then make torch an optional dependency for the common.txt
+        # Install dependencies for GPU
+        if cuda_version:
+            os.system(f"{sys.executable} -m pip install -U -r gpu.txt")
+
+        # Install PyTorch packages
         if nightly:
             os.system(
                 f"pip3 install numpy --pre torch[dynamo] torchvision torchtext torchaudio --force-reinstall --extra-index-url https://download.pytorch.org/whl/nightly/{cuda_version}"
             )
+        else:
+            self.install_torch_packages(cuda_version)
 
     def install_node_packages(self):
         os.system(
@@ -176,7 +179,7 @@ def install_dependencies(cuda_version=None, nightly=False):
     # Sequence of installation to be maintained
     system.install_java()
     requirements_file_path = "requirements/" + (
-        "production.txt" if args.environment == "prod" else "developer.txt"
+        "common.txt" if args.environment == "prod" else "developer.txt"
     )
     system.install_python_packages(cuda_version, requirements_file_path, nightly)
 
