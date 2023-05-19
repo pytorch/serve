771f2b03a9de6d654d4fa98c4c54e2a228c9268e
Jagadeesh J <jagadeeshj@ideas2it.com>
2023-05-20 05:07:52 +0530

    fix: kserve fastapi migration issues (#2175)
    * fix: kserve fastapi migration issues

--- kubernetes/kserve/kserve_wrapper/TorchserveModel.py
+++ kubernetes/kserve/kserve_wrapper/TorchserveModel.py
@@ -1,22 +1,18 @@
 """ The torchserve side inference end-points request are handled to
     return a KServe side response """
-import json
 import logging
 import pathlib
-from typing import Dict
 
 import kserve
-import tornado.web
+from kserve.errors import ModelMissingError
 from kserve.model import Model as Model
-from kserve.model import ModelMissingError
 
 logging.basicConfig(level=kserve.constants.KSERVE_LOGLEVEL)
 
+PREDICTOR_URL_FORMAT = PREDICTOR_V2_URL_FORMAT = "http://{0}/predictions/{1}"
+EXPLAINER_URL_FORMAT = EXPLAINER_V2_URL_FORMAT = "http://{0}/explanations/{1}"
 REGISTER_URL_FORMAT = "{0}/models?initial_workers=1&url={1}"
 UNREGISTER_URL_FORMAT = "{0}/models/{1}"
-
-PREDICTOR_URL_FORMAT = "http://{0}/v1/models/{1}:predict"
-EXPLAINER_URL_FORMAT = "http://{0}/v1/models/{1}:explain"
 
 
 class TorchserveModel(Model):
@@ -49,76 +45,9 @@ class TorchserveModel(Model):
         self.management_address = management_address
         self.model_dir = model_dir
 
-        logging.info("kfmodel Predict URL set to %s", self.predictor_host)
+        logging.info("Predict URL set to %s", self.predictor_host)
         self.explainer_host = self.predictor_host
-        logging.info("kfmodel Explain URL set to %s", self.explainer_host)
-
-    async def predict(self, request: Dict) -> Dict:
-        """The predict method is called when we hit the inference endpoint and handles
-        the inference request and response from the Torchserve side and passes it on
-        to the KServe side.
-
-        Args:
-            request (Dict): Input request from the http client side.
-
-        Raises:
-            NotImplementedError: If the predictor host on the KServe side is not
-                                 available.
-
-            tornado.web.HTTPError: If there is a bad response from the http client.
-
-        Returns:
-            Dict: The Response from the input from the inference endpoint.
-        """
-        if not self.predictor_host:
-            raise NotImplementedError
-        logging.debug("kfmodel predict request is %s", json.dumps(request))
-        logging.info("PREDICTOR_HOST : %s", self.predictor_host)
-        headers = {"Content-Type": "application/json; charset=UTF-8"}
-        response = await self._http_client.fetch(
-            PREDICTOR_URL_FORMAT.format(self.predictor_host, self.name),
-            method="POST",
-            request_timeout=self.timeout,
-            headers=headers,
-            body=json.dumps(request),
-        )
-
-        if response.code != 200:
-            raise tornado.web.HTTPError(status_code=response.code, reason=response.body)
-        return json.loads(response.body)
-
-    async def explain(self, request: Dict) -> Dict:
-        """The predict method is called when we hit the explain endpoint and handles the
-        explain request and response from the Torchserve side and passes it on to the
-        KServe side.
-
-        Args:
-            request (Dict): Input request from the http client side.
-
-        Raises:
-            NotImplementedError: If the predictor host on the KServe side is not
-                                 available.
-
-            tornado.web.HTTPError: If there is a bad response from the http client.
-
-        Returns:
-            Dict: The Response from the input from the explain endpoint.
-        """
-        if self.explainer_host is None:
-            raise NotImplementedError
-        logging.info("kfmodel explain request is %s", json.dumps(request))
-        logging.info("EXPLAINER_HOST : %s", self.explainer_host)
-        headers = {"Content-Type": "application/json; charset=UTF-8"}
-        response = await self._http_client.fetch(
-            EXPLAINER_URL_FORMAT.format(self.explainer_host, self.name),
-            method="POST",
-            request_timeout=self.timeout,
-            headers=headers,
-            body=json.dumps(request),
-        )
-        if response.code != 200:
-            raise tornado.web.HTTPError(status_code=response.code, reason=response.body)
-        return json.loads(response.body)
+        logging.info("Explain URL set to %s", self.explainer_host)
 
     def load(self) -> bool:
         """This method validates model availabilty in the model directory
