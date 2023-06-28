2551a99e495d973fc916322bed352b3a13bf9d5f
Naman Nandan <namankt55@gmail.com>
2023-06-28 16:15:39 -0700

    Publish torchserve-plugins-sdk artifacts to Maven Central repository (#2422)
    * Fix missing torchserve-plugins-sdk artifacts and version
    
    * Add support for releasing torchserve plugins sdk to Maven Central
    
    * enable gpg signing
    
    * Add necessary gpg arguments and update distribution management
    
    * Revert workaround used to build and install torchserve-plugins-sdk locally
    
    * Disable building torchserve-plugins-sdk in CI
    
    ---------
    
    Co-authored-by: Naman Nandan <namannan@amazon.com>

--- ts_scripts/install_dependencies.py
+++ ts_scripts/install_dependencies.py
@@ -81,9 +81,6 @@ class Common:
     def install_numactl(self):
         pass
 
-    def install_torchserve_plugins_sdk(self):
-        pass
-
 
 class Linux(Common):
     def __init__(self):
@@ -113,9 +110,6 @@ class Linux(Common):
         if os.system("numactl --show") != 0 or args.force:
             os.system(f"{self.sudo_cmd}apt-get install -y numactl")
 
-    def install_torchserve_plugins_sdk(self):
-        os.system(f"cd {REPO_ROOT}/serving-sdk/ && ./mvnw clean install -q && cd ../")
-
 
 class Windows(Common):
     def __init__(self):
@@ -133,11 +127,6 @@ class Windows(Common):
 
     def install_numactl(self):
         pass
-
-    def install_torchserve_plugins_sdk(self):
-        os.system(
-            f"cd {REPO_ROOT}/serving-sdk/ && .\mvnw.cmd clean install -q && cd ../"
-        )
 
 
 class Darwin(Common):
@@ -167,9 +156,6 @@ class Darwin(Common):
         if os.system("numactl --show") != 0 or args.force:
             os.system("brew install numactl")
 
-    def install_torchserve_plugins_sdk(self):
-        os.system(f"cd {REPO_ROOT}/serving-sdk/ && ./mvnw clean install -q && cd ../")
-
 
 def install_dependencies(cuda_version=None, nightly=False):
     os_map = {"Linux": Linux, "Windows": Windows, "Darwin": Darwin}
@@ -180,7 +166,6 @@ def install_dependencies(cuda_version=None, nightly=False):
         system.install_nodejs()
         system.install_node_packages()
         system.install_numactl()
-        system.install_torchserve_plugins_sdk()
 
     # Sequence of installation to be maintained
     system.install_java()
