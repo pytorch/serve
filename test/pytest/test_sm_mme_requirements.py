675447e4bee7363f671c9b246738dd18f6ff11bb
agunapal <agunapal@ischool.berkeley.edu>
2023-06-29 02:40:35 +0000

    test sm mme

--- test/pytest/test_sm_mme_requirements.py
+++ test/pytest/test_sm_mme_requirements.py
@@ -78,7 +78,7 @@ def test_oom_on_invoke():
         "model_name": "BERTSeqClassification",
         "url": "https://torchserve.pytorch.org/mar_files/BERTSeqClassification.mar",
         "batch_size": 8,
-        "initial_workers": 8,
+        "initial_workers": 6,
         "synchronous": True,
     }
     response = test_utils.register_model_with_params(params)
