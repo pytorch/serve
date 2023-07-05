91c2fc67f7733f2626362f8904391b652e2b454e
Bryan Weber <bryan.w.weber@gmail.com>
2023-07-05 09:12:25 -0400

    Set the content type of the long description
    This will format more nicely on PyPI

--- model-archiver/setup.py
+++ model-archiver/setup.py
@@ -71,6 +71,7 @@ if __name__ == "__main__":
         description="Torch Model Archiver is used for creating archives of trained neural net models "
         "that can be consumed by TorchServe inference",
         long_description=pypi_description(),
+        long_description_content_type="text/x-rst",
         author="PyTorch Serving team",
         author_email="noreply@noreply.com",
         url="https://github.com/pytorch/serve/model-archiver/",
