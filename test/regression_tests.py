5b7cee07436da09ec08f50e75f33868c303517b6
agunapal <agunapal@berkeley.edu>
2023-08-31 21:00:03 +0000

    testing changes

--- test/regression_tests.py
+++ test/regression_tests.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import sys
 
@@ -10,27 +11,77 @@ import datetime
 from ts_scripts import marsgen as mg
 from ts_scripts.api_utils import test_api
 from ts_scripts.install_from_src import install_from_src
-from ts_scripts.regression_utils import test_regression
-from ts_scripts.utils import check_python_version
+from ts_scripts.utils import check_python_version, try_and_handle
 
-now = datetime.datetime.now()
-print("Current date and time : " + now.strftime("%Y-%m-%d %H:%M:%S"))
 
-check_python_version()
+def regression_tests(binaries, pypi, conda, nightly):
+    now = datetime.datetime.now()
+    print("Current date and time : " + now.strftime("%Y-%m-%d %H:%M:%S"))
 
-# Install from source
-install_from_src()
+    check_python_version()
 
-# Generate mar file
-mg.generate_mars()
+    if binaries:
+        if pypi:
+            if nightly:
+                cmd = f"pip install torchserve-nightly torch-model-archiver-nightly torch-workflow-archiver-nightly"
+            else:
+                cmd = f"pip install torchserve torch-model-archiver torch-workflow-archiver"
+        elif conda:
+            if nightly:
+                cmd = f"conda install -c pytorch-nightly torchserve torch-model-archiver torch-workflow-archiver"
+            else:
+                cmd = f"conda install -c pytorch torchserve torch-model-archiver torch-workflow-archiver"
+        print(f"## In directory: {os.getcwd()}; Executing command: {cmd}")
+        try_and_handle(cmd, False)
+    else:
+        # Install from source
+        install_from_src()
 
-# Run newman api tests
-test_api(
-    "all"
-)  # "all" > management, inference, increased_timeout_inference, https collections
+    # Generate mar file
+    mg.generate_mars()
 
-# Run regression tests
-test_regression()
+    # Run newman api tests
+    test_api(
+        "all"
+    )  # "all" > management, inference, increased_timeout_inference, https collections
 
-# delete mar_gen_dir
-mg.delete_model_store_gen_dir()
+    # Run regression tests
+    # test_regression()
+
+    # delete mar_gen_dir
+    mg.delete_model_store_gen_dir()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Regression tests for TorchServe")
+    parser.add_argument(
+        "--nightly",
+        action="store_true",
+        required=False,
+        help="Run regression tests using nightly binaries",
+    )
+
+    parser.add_argument(
+        "--binaries",
+        action="store_true",
+        required=False,
+        help="Run regression tests using binaries",
+    )
+
+    parser.add_argument(
+        "--pypi",
+        action="store_true",
+        required=False,
+        help="Run regression tests using pypi",
+    )
+
+    parser.add_argument(
+        "--conda",
+        action="store_true",
+        required=False,
+        help="Run regression tests using conda",
+    )
+
+    args = parser.parse_args()
+
+    regression_tests(args.binaries, args.pypi, args.conda, args.nightly)
