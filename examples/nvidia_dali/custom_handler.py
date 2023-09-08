8ae2f510ab97d7fd4a6116867bbae3f91cf9add5
Jagadeesh J <jagadeeshj@ideas2it.com>
2023-09-09 04:58:34 +0530

    move dali preprocess to handler util (#2485)
    * move dali preprocess to handler util
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * add dali handler with default preprocess pipeline
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * change dali example to use model-config file
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * update serialize pipeline with  model-config file
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * add dali handler for image classifier and segmentation
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * change the dali example to mnist model
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * fix test
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * add pipeline params to args
    
    * fix readme doc
    
     - remove dali_image_segmenter handler
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * fix spell check
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * add pipeline generation to setup.py
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * Revert "add pipeline generation to setup.py"
    
    This reverts commit 0fc8ba428dc52829083bd0364ef336805e128a36.
    
    * retest
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * re-test
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * update readme docs
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    * re-test
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    
    ---------
    
    Signed-off-by: jagadeesh <jagadeeshj@ideas2it.com>
    Co-authored-by: Ankith Gunapal <agunapal@ischool.Berkeley.edu>
    Co-authored-by: Geeta Chauhan <4461127+chauhang@users.noreply.github.com>
    Co-authored-by: Mark Saroufim <marksaroufim@fb.com>

--- examples/nvidia_dali/custom_handler.py
+++ examples/nvidia_dali/custom_handler.py
@@ -3,74 +3,43 @@
 """
 Base module for all vision handlers
 """
-import json
-import os
+from torch.profiler import ProfilerActivity
 
-import numpy as np
-import torch
-from nvidia.dali.pipeline import Pipeline
-
-from ts.torch_handler.image_classifier import ImageClassifier
+from ts.torch_handler.dali_handler import DALIHandler
 
 
-class DALIHandler(ImageClassifier):
+class DALIMNISTDigitClassifier(DALIHandler):
     """
     Base class for all vision handlers
     """
 
+    topk = 5
+    # These are the standard Imagenet dimensions
+    # and statistics
+
     def __init__(self):
-        super(DALIHandler, self).__init__()
+        super(DALIMNISTDigitClassifier, self).__init__()
 
     def initialize(self, context):
         super().initialize(context)
-        properties = context.system_properties
-        self.model_dir = properties.get("model_dir")
+        self.profiler_args = {
+            "activities": [ProfilerActivity.CPU],
+            "record_shapes": True,
+        }
 
-        self.dali_file = [
-            file for file in os.listdir(self.model_dir) if file.endswith(".dali")
-        ]
-        if not len(self.dali_file):
-            raise RuntimeError("Missing dali pipeline file.")
-        dali_config_file = os.path.join(self.model_dir, "dali_config.json")
-        if not os.path.isfile(dali_config_file):
-            raise RuntimeError("Missing dali_config.json file.")
-        with open(dali_config_file) as setup_config_file:
-            self.dali_configs = json.load(setup_config_file)
-        dali_filename = os.path.join(self.model_dir, self.dali_file[0])
-        self.pipe = Pipeline.deserialize(
-            filename=dali_filename,
-            batch_size=self.dali_configs["batch_size"],
-            num_threads=self.dali_configs["num_threads"],
-            prefetch_queue_depth=1,
-            device_id=self.dali_configs["device_id"],
-            seed=self.dali_configs["seed"],
-        )
-        self.pipe.build()
-        # pylint: disable=protected-access
-        self.pipe._max_batch_size = self.dali_configs["batch_size"]
-        self.pipe._num_threads = self.dali_configs["num_threads"]
-        self.pipe._device_id = self.dali_configs["device_id"]
+    def set_max_result_classes(self, topk):
+        self.topk = topk
 
-    def preprocess(self, data):
-        """The preprocess function of MNIST program converts the input data to a float tensor
+    def get_max_result_classes(self):
+        return self.topk
+
+    def postprocess(self, data):
+        """The post process of MNIST converts the predicted output response to a label.
 
         Args:
-            data (List): Input data from the request is in the form of a Tensor
-
+            data (list): The predicted output from the Inference with probabilities is passed
+            to the post-process function
         Returns:
-            list : The preprocess function returns the input image as a list of float tensors.
+            list : A list of dictionaries with predictions and explanations is returned
         """
-        batch_tensor = []
-        result = []
-
-        input_byte_arrays = [i["body"] if "body" in i else i["data"] for i in data]
-        for byte_array in input_byte_arrays:
-            np_image = np.frombuffer(byte_array, dtype=np.uint8)
-            batch_tensor.append(np_image)  # we can use numpy
-
-        response = self.pipe.run(source=batch_tensor)
-        for idx, _ in enumerate(response[0]):
-            data = torch.tensor(response[0].at(idx))
-            result.append(data.unsqueeze(0))
-
-        return torch.cat(result).to(self.device)
+        return data.argmax(1).tolist()
