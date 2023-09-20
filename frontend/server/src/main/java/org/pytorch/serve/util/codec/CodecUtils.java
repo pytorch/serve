8f19e00d570870798f6f52240f298a06cd166e8c
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-09-20 18:04:25 +0000

    Fix buffer underruns; wait until enough bytes are in for reading

--- frontend/server/src/main/java/org/pytorch/serve/util/codec/CodecUtils.java
+++ frontend/server/src/main/java/org/pytorch/serve/util/codec/CodecUtils.java
@@ -10,14 +10,20 @@ public final class CodecUtils {
 
     public static final int END = -1;
     public static final int BUFFER_UNDER_RUN = -3;
+    public static final long TIMEOUT_IN_MILLIS = 100;
 
     private CodecUtils() {}
 
     public static int readLength(ByteBuf byteBuf, int maxLength) {
+
         int size = byteBuf.readableBytes();
-        if (size < 4) {
-            return BUFFER_UNDER_RUN;
+
+        long start_time = System.currentTimeMillis();
+        while (size < 4 && (System.currentTimeMillis() - start_time) < TIMEOUT_IN_MILLIS) {
+            size = byteBuf.readableBytes();
         }
+        if(size < 4)
+            return BUFFER_UNDER_RUN;
 
         int len = byteBuf.readInt();
         if (len > maxLength) {
@@ -25,6 +31,9 @@ public final class CodecUtils {
                     "Message size exceed limit: "
                             + len
                             + "\nConsider increasing the 'max_response_size' in 'config.properties' to fix.");
+        }
+        start_time = System.currentTimeMillis();
+        while (len > byteBuf.readableBytes() && (System.currentTimeMillis() - start_time) < TIMEOUT_IN_MILLIS) {
         }
         if (len > byteBuf.readableBytes()) {
             return BUFFER_UNDER_RUN;
