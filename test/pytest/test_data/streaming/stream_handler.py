8c3a8903ce6076b2bfb9a3da1c49b8a2dce8e388
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-09-22 11:39:11 +0000

    Cleaned up streaming handler code

--- test/pytest/test_data/streaming/stream_handler.py
+++ test/pytest/test_data/streaming/stream_handler.py
@@ -18,10 +18,12 @@ class StreamingHandler(BaseHandler):
 
         logger.info(f"Initialized {self.__class__}")
         
+        #Initialize model
         self.tokenizer = AutoTokenizer.from_pretrained("gpt2")
         self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
         self.model = GPT2LMHeadModel.from_pretrained("gpt2")
         
+        # Replace _update_model_kwargs_for_generation of model with a method that extracts the kv cache for us
         old_update = self.model._update_model_kwargs_for_generation
         ctx.kv_cache = {}
         
@@ -36,13 +38,14 @@ class StreamingHandler(BaseHandler):
         assert len(self.context.request_ids.values()) <= 2
         prefill, decode = [], []
         for req_id, req_data in zip(self.context.request_ids.values(), data):
+            #Tokenizer requests which are not prefilled yet
             if not req_id in self.context.cache:
                 print(req_data["body"])
                 data = json.loads(req_data["body"])
                 encoded = self.tokenizer(data["prompt"], return_tensors='pt')
                 encoded["past_key_values"] = None
                 self.context.cache[req_id] = {
-                    "stopping_criteria": self.create_stopping_criteria(req_id, max_new_tokens=data["max_new_tokens"]),
+                    "stopping_criteria": self._create_stopping_criteria(req_id, max_new_tokens=data["max_new_tokens"]),
                     "encoded": encoded,
                     "prompt_length": len(encoded["input_ids"]),
                 }
@@ -54,11 +57,13 @@ class StreamingHandler(BaseHandler):
     def inference(self, *args):
         prefill, decode_ids = args[0]
         
+        #Prefill requests
         results = {}
         for req_id in prefill:
-            results[req_id] = self.run_prefill(req_id)
+            results[req_id] = self._run_prefill(req_id)
         
-        decode_result = self.run_decode(decode_ids) if decode_ids else {}
+        #Decode the rest
+        decode_result = self._run_decode(decode_ids) if decode_ids else {}
         results.update(decode_result)
         return [results[i] for i in self.context.request_ids.values()
         ]
@@ -69,8 +74,113 @@ class StreamingHandler(BaseHandler):
             for i in self.context.request_ids.values()
         ]
         return x
+    
+    def _run_prefill(self, req_id):
+        assert self.context.cache[req_id]["encoded"]["past_key_values"] is None, "There should be no cached values"
+        output = self.model.generate(
+            **self.context.cache[req_id]["encoded"],
+            max_new_tokens=1,
+            return_dict_in_generate=True,
+            use_cache=True
+            )
+        # Save extracted kv cache values and adjust attention mask for next call
+        self.context.cache[req_id]["encoded"]["past_key_values"] = self.context.kv_cache["past_key_values"]
+        del self.context.kv_cache["past_key_values"]
+        self.context.cache[req_id]["encoded"]["input_ids"] = output.sequences
+        attention_mask = self.context.cache[req_id]["encoded"]["attention_mask"]
+        attention_mask = torch.concat((attention_mask, torch.ones((1,1), dtype=torch.int64)), dim=1)
+        self.context.cache[req_id]["encoded"]["attention_mask"] = attention_mask
+        
+        result = {
+            "text": self.tokenizer.decode(output.sequences[0], skip_special_tokens=True),
+            "ids": output.sequences[0].tolist(),
+        }
+        return result
+        
+    def _run_decode(self, ids):
+        assert len(ids)
+        
+        encoded = self._prepare_model_inputs(ids)
+        
+        outputs = self.model.generate(
+            **encoded,
+            max_new_tokens=1,
+            return_dict_in_generate=True,
+            use_cache=True
+            )
 
-    def create_stopping_criteria(self, req_id, max_new_tokens=25):
+        results = {}
+        for idx, req_id in enumerate(ids):
+            self.context.cache[req_id]["encoded"]["past_key_values"] = self._collect_kv_cache_of_idx_in_batch(idx)
+            self.context.cache[req_id]["encoded"]["input_ids"] = outputs.sequences[idx].unsqueeze(0)
+            attention_mask = encoded["attention_mask"][idx].unsqueeze(0)
+            attention_mask = torch.concat((attention_mask, torch.ones((1,1), dtype=torch.int64)), dim=1)
+            self.context.cache[req_id]["encoded"]["attention_mask"] = attention_mask
+            results[req_id] = {
+                "text":self.tokenizer.decode(outputs.sequences[idx][-1], skip_special_tokens=True),
+                "ids":[outputs.sequences[idx][-1].item()],
+                }
+        del self.context.kv_cache["past_key_values"]
+        return results
+    
+    def _prepare_model_inputs(self, ids):
+        lengths = list(torch.sum(self.context.cache[i]["encoded"]["attention_mask"], dim=1).item() for i in ids)
+        max_len = max(lengths)
+        
+        input_ids = []
+        attention_mask = []
+        kv_cache = {}
+        for req_id, seq_len in zip(ids, lengths):
+            input_ids.append(self.context.cache[req_id]["encoded"]["input_ids"])
+            attention_mask.append(self.context.cache[req_id]["encoded"]["attention_mask"])
+            
+            for layer_idx, layer_kv in enumerate(self.context.cache[req_id]["encoded"]["past_key_values"]):
+                k, v = layer_kv
+                kv_cache[layer_idx] = kv_cache.get(layer_idx, {})
+                kv_cache[layer_idx][0] = kv_cache.get(layer_idx, {}).get(0,[]) + [k]
+                kv_cache[layer_idx][1] = kv_cache.get(layer_idx, {}).get(1,[]) + [v]
+            padded_len = input_ids[-1].size()[-1]
+            if padded_len < max_len:
+                # Apply padding to input_ids, attention_mask and past_key_values
+                n = max_len - seq_len
+                input_ids[-1] = torch.concat((self.tokenizer.pad_token_id + torch.zeros((1,n), dtype=torch.int64), input_ids[-1]), dim=1)
+                attention_mask[-1] = torch.concat((torch.zeros((1,n), dtype=torch.int64), attention_mask[-1]), dim=1)
+                
+                size_delta = list(kv_cache[0][0][-1].size())
+                size_delta[2] = n
+                dtype = kv_cache[0][0][-1].dtype
+                for layer_idx in range(len(kv_cache)):
+                    kv_cache[layer_idx][0][-1] = torch.concat((torch.zeros(size_delta, dtype=dtype), kv_cache[layer_idx][0][-1]), dim=2)
+                    kv_cache[layer_idx][1][-1] = torch.concat((torch.zeros(size_delta, dtype=dtype), kv_cache[layer_idx][1][-1]), dim=2)
+                
+            elif padded_len > max_len:
+                # Truncate padding from input_ids, attention_mask and past_key_values
+                input_ids[-1] = input_ids[-1][:, -max_len:]
+                attention_mask[-1] = attention_mask[-1][:,-max_len:]
+                
+                for layer_idx in range(len(kv_cache)):
+                    kv_cache[layer_idx][0][-1] = kv_cache[layer_idx][0][-1][:,:,(-max_len+1):,:]
+                    kv_cache[layer_idx][1][-1] = kv_cache[layer_idx][1][-1][:,:,(-max_len+1):,:]
+            del self.context.cache[req_id]["encoded"]["past_key_values"]
+        
+        for layer_idx in range(len(kv_cache)):
+            kv_cache[layer_idx][0] = torch.concat(kv_cache[layer_idx][0], dim=0)
+            kv_cache[layer_idx][1] = torch.concat(kv_cache[layer_idx][1], dim=0)
+            
+        kv_cache = tuple((kv_cache[layer_idx][0], kv_cache[layer_idx][1]) for layer_idx in range(len(kv_cache)))
+            
+        encoded = {
+            "input_ids": torch.concat(input_ids, dim=0),
+            "attention_mask": torch.concat(attention_mask, dim=0),
+            "past_key_values": kv_cache,
+        }
+        return encoded
+    
+    def _collect_kv_cache_of_idx_in_batch(self, idx):
+        #The materialization of the tuple here is important for some reason (TODO: figure out why); Otherwise prediction differ
+        return tuple(tuple(kv[idx,...].unsqueeze(0) for kv in layers) for layers in self.context.kv_cache["past_key_values"])
+    
+    def _create_stopping_criteria(self, req_id, max_new_tokens=25):
         class StoppingCriteria(object):
             def __init__(
                 self, cache, req_id, stop_token, max_new_tokens, 
@@ -98,94 +208,3 @@ class StreamingHandler(BaseHandler):
             max_new_tokens,
         )
         
-    def run_prefill(self, req_id):
-        assert self.context.cache[req_id]["encoded"]["past_key_values"] is None
-        output = self.model.generate(
-            **self.context.cache[req_id]["encoded"],
-            max_new_tokens=1,
-            return_dict_in_generate=True,
-            use_cache=True
-            )
-        self.context.cache[req_id]["encoded"]["past_key_values"] = self.context.kv_cache["past_key_values"]
-        del self.context.kv_cache["past_key_values"]
-        result = {
-            "text": self.tokenizer.decode(output.sequences[0], skip_special_tokens=True),
-            "ids": output.sequences[0].tolist(),
-        }
-        self.context.cache[req_id]["encoded"]["input_ids"] = output.sequences
-        attention_mask = self.context.cache[req_id]["encoded"]["attention_mask"]
-        attention_mask = torch.concat((attention_mask, torch.ones((1,1), dtype=torch.int64)), dim=1)
-        self.context.cache[req_id]["encoded"]["attention_mask"] = attention_mask
-        return result
-        
-    def run_decode(self, ids):
-        assert len(ids)
-        lengths = list(torch.sum(self.context.cache[i]["encoded"]["attention_mask"], dim=1).item() for i in ids)
-        max_len = max(lengths)
-        
-        input_ids = []
-        attention_mask = []
-        kv_cache = {}
-        for i, seq_len in zip(ids, lengths):
-            input_ids.append(self.context.cache[i]["encoded"]["input_ids"])
-            attention_mask.append(self.context.cache[i]["encoded"]["attention_mask"])
-            
-            for layer_idx, layer_kv in enumerate(self.context.cache[i]["encoded"]["past_key_values"]):
-                k, v = layer_kv
-                kv_cache[layer_idx] = kv_cache.get(layer_idx, {})
-                kv_cache[layer_idx][0] = kv_cache.get(layer_idx, {}).get(0,[]) + [k]
-                kv_cache[layer_idx][1] = kv_cache.get(layer_idx, {}).get(1,[]) + [v]
-            padded_len = input_ids[-1].size()[-1]
-            if padded_len < max_len:
-                n = max_len - seq_len
-                input_ids[-1] = torch.concat((self.tokenizer.pad_token_id + torch.zeros((1,n), dtype=torch.int64), input_ids[-1]), dim=1)
-                attention_mask[-1] = torch.concat((torch.zeros((1,n), dtype=torch.int64), attention_mask[-1]), dim=1)
-                
-                size_delta = list(kv_cache[0][0][-1].size())
-                size_delta[2] = n
-                dtype = kv_cache[0][0][-1].dtype
-                for layer_idx in range(len(kv_cache)):
-                    kv_cache[layer_idx][0][-1] = torch.concat((torch.zeros(size_delta, dtype=dtype), kv_cache[layer_idx][0][-1]), dim=2)
-                    kv_cache[layer_idx][1][-1] = torch.concat((torch.zeros(size_delta, dtype=dtype), kv_cache[layer_idx][1][-1]), dim=2)
-                
-            elif padded_len > max_len:
-                input_ids[-1] = input_ids[-1][:, -max_len:]
-                attention_mask[-1] = attention_mask[-1][:,-max_len:]
-                
-                for layer_idx in range(len(kv_cache)):
-                    kv_cache[layer_idx][0][-1] = kv_cache[layer_idx][0][-1][:,:,(-max_len+1):,:]
-                    kv_cache[layer_idx][1][-1] = kv_cache[layer_idx][1][-1][:,:,(-max_len+1):,:]
-            del self.context.cache[i]["encoded"]["past_key_values"]
-        
-        for layer_idx in range(len(kv_cache)):
-            kv_cache[layer_idx][0] = torch.concat(kv_cache[layer_idx][0], dim=0)
-            kv_cache[layer_idx][1] = torch.concat(kv_cache[layer_idx][1], dim=0)
-            
-        kv_cache = tuple((kv_cache[layer_idx][0], kv_cache[layer_idx][1]) for layer_idx in range(len(kv_cache)))
-            
-        encoded = {
-            "input_ids": torch.concat(input_ids, dim=0),
-            "attention_mask": torch.concat(attention_mask, dim=0),
-            "past_key_values": kv_cache,
-        }
-        
-        outputs = self.model.generate(
-            **encoded,
-            max_new_tokens=1,
-            return_dict_in_generate=True,
-            use_cache=True
-            )
-
-        results = {}
-        for idx, req_id in enumerate(ids):
-            self.context.cache[req_id]["encoded"]["past_key_values"] = tuple(tuple(kv[idx,...].unsqueeze(0) for kv in layers) for layers in self.context.kv_cache["past_key_values"])
-            self.context.cache[req_id]["encoded"]["input_ids"] = outputs.sequences[idx].unsqueeze(0)
-            attention_mask = encoded["attention_mask"][idx].unsqueeze(0)
-            attention_mask = torch.concat((attention_mask, torch.ones((1,1), dtype=torch.int64)), dim=1)
-            self.context.cache[req_id]["encoded"]["attention_mask"] = attention_mask
-            results[req_id] = {
-                "text":self.tokenizer.decode(outputs.sequences[idx][-1], skip_special_tokens=True),
-                "ids":[outputs.sequences[idx][-1].item()],
-                }
-        del self.context.kv_cache["past_key_values"]
-        return results
