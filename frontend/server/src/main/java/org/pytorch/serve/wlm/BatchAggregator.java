c3a2cca6444b0d31167c20767a458adc3f888762
lxning <lninga@amazon.com>
2023-09-27 16:51:17 -0700

    fix conflict

--- frontend/server/src/main/java/org/pytorch/serve/wlm/BatchAggregator.java
+++ frontend/server/src/main/java/org/pytorch/serve/wlm/BatchAggregator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
