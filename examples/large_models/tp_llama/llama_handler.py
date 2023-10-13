bc4375cac3c064897f93f4315e39657aafa04732
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-10-13 04:46:35 +0000

    [WIP] Converting llama_tp into cont batching handler

--- examples/large_models/tp_llama/llama_handler.py
+++ examples/large_models/tp_llama/llama_handler.py
@@ -16,7 +16,7 @@ from ts.torch_handler.base_handler import BaseHandler
 current_working_directory = os.getcwd()
 sys.path.insert(0,current_working_directory)
 from llama2 import Llama
-from generate import chat_completion, text_completion, Dialog
+from generate import chat_completion, text_completion, Dialog, sample_top_p
 
 
 logger = logging.getLogger(__name__)
@@ -50,7 +50,9 @@ class LlamaHandler(BaseHandler,ABC):
         # super().initialize(ctx)
         if not torch.distributed.is_initialized():
             torch.distributed.init_process_group("nccl")
-
+        
+        self.context = ctx
+        
         self.manifest = ctx.manifest
         properties = ctx.system_properties
         model_dir = properties.get("model_dir")
@@ -73,10 +75,12 @@ class LlamaHandler(BaseHandler,ABC):
         self.model = llama_model_and_tok.model
         self.model.eval()
     
-  
         self.tokenizer = llama_model_and_tok.tokenizer
 
         logger.info(f"Llama model from path {model_dir} loaded successfully in {load_time} seconds")
+        
+        self.max_bsz = self.model.layers[0].attention.cache_k.size(0)
+        self.batch_idx_to_req_ids = [None,] * self.max_bsz
 
         self.initialized = True
 
@@ -90,24 +94,31 @@ class LlamaHandler(BaseHandler,ABC):
             tuple: A tuple with two tensors: the batch of input ids and the batch of
                 attention masks.
         """
-        input_texts = [data.get("data") or data.get("body") for data in requests]
+        self._clean_cache()
+        prefill, decode = [], []
         
-        if self.mode == "chat":
-            if input_texts:
+        for req_id, req_data in zip(self.context.request_ids.values(), requests):
+            # Tokenizer requests which are not prefilled yet
+            if not req_id in self.context.cache:
+                data = req_data.get("data") or req_data.get("body")
+                input_text = self.prep_input_text(data["prompt"])
+                logger.debug("Received text: '%s'", input_text)
                 
-                try:
-                    dialog = json.loads(input_texts[0])
-                except json.JSONDecodeError:
-                    raise ValueError(f"Invalid JSON format in text: {input_texts[0]}")
-                return dialog
-              
-        elif self.mode == "text_completion":
-            try:
-                return [self.prep_input_text(text) for text in input_texts]
-            except TypeError:
-                raise ValueError("Expected input_texts to contain text (string) values.")
-        else:
-            raise NotImplementedError("Unsupported mode. Please select a valid mode.")
+                encoded = self.tokenizer.encode(input_text, bos=True, eos=False)
+                
+                encoded = torch.tensor(encoded, dtype=torch.long, device="cuda")
+                
+                self.context.cache[req_id] = {
+                    "stopping_criteria": self._create_stopping_criteria(
+                        req_id, max_new_tokens=min(self.max_new_tokens, data.get("max_new_tokens",self.max_new_tokens))
+                    ),
+                    "encoded": encoded,
+                    "prompt_length": encoded.size(-1),
+                }
+                prefill.append(req_id)
+            else:
+                decode.append(req_id)
+        return prefill, decode
 
 
     def prep_input_text(self, input_text):
@@ -118,11 +129,22 @@ class LlamaHandler(BaseHandler,ABC):
         Returns:
             decoded input text
         """
-        if isinstance(input_text, (bytes, bytearray)):
-            input_text = input_text.decode("utf-8")
-        logger.debug("Received text: '%s'", input_text)
+        if self.mode == "chat":
+            try:
+                return json.loads(input_text)
+            except json.JSONDecodeError:
+                raise ValueError(f"Invalid JSON format in text: {input_text}")
+              
+        elif self.mode == "text_completion":
+            try:
+                if isinstance(input_text, (bytes, bytearray)):
+                    input_text = input_text.decode("utf-8")
+                return input_text
+            except TypeError:
+                raise ValueError("Expected input_texts to contain text (string) values.")
+        else:
+            raise NotImplementedError("Unsupported mode. Please select a valid mode.")
         
-        return input_text
 
     def inference(self, input_batch):
         """
@@ -168,3 +190,73 @@ class LlamaHandler(BaseHandler,ABC):
         logger.info("Generated text: %s", inference_output)
         
         return [inference_output]
+    
+    @torch.no_grad()
+    def _run_prefill(self, req_id):
+        rearrangement_indices = self._rearrange_kv_cache_before_prefill(req_id)
+        
+        logits = self.model.forward(self.context.cache[req_id]["encoded"], 0)
+        
+        if self.temperature > 0:
+            probs = torch.softmax(logits[:, -1] / self.temperature, dim=-1)
+            next_token = sample_top_p(probs, self.top_p)
+        else:
+            next_token = torch.argmax(logits[:, -1], dim=-1)
+        
+        result = {
+            "ids": next_token.item(),
+        }
+        
+        self.context.cache[req_id]["padding"] = 0
+        
+        self._rearrange_kv_cache_after_prefill(rearrangement_indices)
+        return result
+
+
+    def _evaculate_kv_cache_before_prefill(self, req_id: str) -> torch.Tensor:
+        req_batch_idx = list(self.context.request_ids.values()).index(req_id)
+        
+        rearrangement_indices = torch.LongTensor(range(self.max_bsz), device="cuda")
+        rearrangement_indices[0] = req_batch_idx
+        rearrangement_indices[req_batch_idx] = 0
+        
+        for l in self.model.layers:
+            l.attention.cache_k = l.attention.cache_k[rearrangement_indices, ...]
+            l.attention.cache_v = l.attention.cache_v[rearrangement_indices, ...]
+        
+        return rearrangement_indices
+        
+            
+    def _clean_cache(self):
+        new_ids = set(self.context.request_ids.keys())
+        self.batch_idx_to_req_ids = [key if key in new_ids else None for key in self.batch_idx_to_req_ids]
+        self._compact_kv_cache()
+            
+                
+    def _compact_kv_cache(self):
+        assert self.batch_idx_to_req_ids.count(None) != self.max_bsz, "Batch should never be empty"
+        
+        i = 0
+        j = self.max_bsz-1
+        
+        while i < j:
+            if self.batch_idx_to_req_ids[j] is None:
+                j -= 1
+                continue
+            elif self.batch_idx_to_req_ids[i] is None:
+                self._swap_kv_cache(i,j)
+                self.batch_idx_to_req_ids[i], self.batch_idx_to_req_ids[j] = self.batch_idx_to_req_ids[j], self.batch_idx_to_req_ids[i]
+                i += 1
+                j -= 1
+            else:
+                i += 1
+            
+            
+                
+    def _swap_kv_cache(self, i: int, j: int) -> None:
+        rearrangement_indices = torch.tensor(range(self.max_bsz), dtype=torch.long, device="cuda")
+        rearrangement_indices[i], rearrangement_indices[j] = rearrangement_indices[j], rearrangement_indices[i]
+        
+        for l in self.model.layers:
+            l.attention.cache_k = l.attention.cache_k[rearrangement_indices, ...]
+            l.attention.cache_v = l.attention.cache_v[rearrangement_indices, ...]
--- test/pytest/test_tp_llama.py
+++ test/pytest/test_tp_llama.py
@@ -39,22 +39,78 @@ handler:
     mode: "text_completion" #choices are text_completion, chat
 """
 
-def call_handler(rank: int, world_size: int, queue: Queue, yaml_path: str):
+# def call_handler(rank: int, world_size: int, queue: Queue, yaml_path: str):
+#     from llama_handler import LlamaHandler
+#     handler = LlamaHandler()
+#     ctx = MockContext(
+#         model_pt_file=None,
+#         model_dir=LLAMA_PATH.as_posix(),
+#     )
+    
+#     with open(yaml_path, "r") as f:
+#         config = yaml.safe_load(f)
+    
+#     ctx.model_yaml_config = config
+    
+#     os.environ["RANK"] = str(rank)
+#     os.environ["LOCAL_RANK"] = str(rank)
+#     os.environ["WORLD_SIZE"] = str(world_size)
+#     os.environ["MASTER_ADDR"] = "localhost"
+#     os.environ["MASTER_PORT"] = "1234"
+    
+
+#     torch.manual_seed(42 * 42)
+#     handler.initialize(ctx)
+
+#     handler.context = ctx
+    
+#     queue.put(handler.inference([{"The capital of France", "what is the recipes for Mayonnaise?"]))
+    
+    
+# @pytest.mark.skipif(not (LLAMA_PATH / converted_checkpoints_path).exists(), reason="Required files are not present")
+# def test_tp_llama(tmp_path):
+#     world_size = 2
+    
+#     model_config_yaml = tmp_path / "model-config.yaml"
+#     model_config_yaml.write_text(YAML_CONFIG)
+    
+#     q = Queue()
+    
+#     procs = [Process(target=call_handler, args=(rank,world_size,q,model_config_yaml,)) for rank in range(world_size)]
+    
+#     for p in procs:
+#         p.start()
+        
+#     for p in procs:
+#         p.join()
+    
+#     results = []
+#     while not q.empty():
+#         results.append(q.get())
+    
+#     assert len(results) == 2
+    
+#     assert results[0][0]["generation"] == ", Paris, is a city of romance, art, and culture. It is also a city of fashion, food, and fun. Paris is a city that has something for everyone.\nParis is a city that is full of history."
+#     assert results[0][1]["generation"] == "\nI have a recipe for mayonnaise that I use all the time. It is very easy and tastes great.\n1. In a bowl, whisk together the egg yolks, mustard, lemon ju"
+
+def test_handler(tmp_path):
     from llama_handler import LlamaHandler
     handler = LlamaHandler()
     ctx = MockContext(
         model_pt_file=None,
         model_dir=LLAMA_PATH.as_posix(),
     )
+    model_config_yaml = tmp_path / "model-config.yaml"
+    model_config_yaml.write_text(YAML_CONFIG)
     
-    with open(yaml_path, "r") as f:
+    with open(model_config_yaml, "r") as f:
         config = yaml.safe_load(f)
     
     ctx.model_yaml_config = config
     
-    os.environ["RANK"] = str(rank)
-    os.environ["LOCAL_RANK"] = str(rank)
-    os.environ["WORLD_SIZE"] = str(world_size)
+    os.environ["RANK"] = str(0)
+    os.environ["LOCAL_RANK"] = str(0)
+    os.environ["WORLD_SIZE"] = str(1)
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "1234"
     
@@ -63,33 +119,53 @@ def call_handler(rank: int, world_size: int, queue: Queue, yaml_path: str):
     handler.initialize(ctx)
 
     handler.context = ctx
+    device = next(iter(handler.model.parameters())).device
+
+    ctx.cache = {
+        "id1": {
+            "encoded": torch.randint(42, (1, 5), device=device),
+            },
+        "id2": {
+            "encoded": torch.randint(42, (1, 8), device=device),
+            },
+    }
+
+    res = handler._run_prefill("id1")
+    res = handler._run_prefill("id2")
     
-    queue.put(handler.inference(["The capital of France", "what is the recipes for Mayonnaise?"]))
+    res = handler._run_decode(["id1"])
+    
+    assert len(res["id1"]["ids"]) == len(res["id1"]["text"]) == 1
+
+    assert ctx.cache["id1"]["encoded"].size()[-1] == 5
     
     
-@pytest.mark.skipif(not (LLAMA_PATH / converted_checkpoints_path).exists(), reason="Required files are not present")
-def test_tp_llama(tmp_path):
-    world_size = 2
+def test_compact_cache(mocker):
+    from llama_handler import LlamaHandler
+    handler = LlamaHandler()
     
-    model_config_yaml = tmp_path / "model-config.yaml"
-    model_config_yaml.write_text(YAML_CONFIG)
+    handler.batch_idx_to_req_ids = [None, 1, None, 2, 3, None]
+    handler.max_bsz = len(handler.batch_idx_to_req_ids)
     
-    q = Queue()
+    handler.model = mocker.MagicMock(name="model")
+    handler.model.layers = [mocker.MagicMock(name="layer")]
+    handler.model.layers[0].attention.cache_k = -1 * torch.ones((handler.max_bsz, 1), dtype=torch.long, device="cuda")
+    handler.model.layers[0].attention.cache_k[1] = 1
+    handler.model.layers[0].attention.cache_k[3] = 2
+    handler.model.layers[0].attention.cache_k[4] = 3
     
-    procs = [Process(target=call_handler, args=(rank,world_size,q,model_config_yaml,)) for rank in range(world_size)]
+    handler.model.layers[0].attention.cache_v = handler.model.layers[0].attention.cache_k.clone()
     
-    for p in procs:
-        p.start()
-        
-    for p in procs:
-        p.join()
+    handler._compact_kv_cache()
     
-    results = []
-    while not q.empty():
-        results.append(q.get())
+    assert handler.batch_idx_to_req_ids.index(None) == 3
+    assert handler.batch_idx_to_req_ids[0] == 3
+    assert handler.batch_idx_to_req_ids[1] == 1
+    assert handler.batch_idx_to_req_ids[2] == 2
     
-    assert len(results) == 2
+    assert handler.model.layers[0].attention.cache_k[0] == 3
+    assert handler.model.layers[0].attention.cache_k[1] == 1
+    assert handler.model.layers[0].attention.cache_k[2] == 2
     
-    assert results[0][0]["generation"] == ", Paris, is a city of romance, art, and culture. It is also a city of fashion, food, and fun. Paris is a city that has something for everyone.\nParis is a city that is full of history."
-    assert results[0][1]["generation"] == "\nI have a recipe for mayonnaise that I use all the time. It is very easy and tastes great.\n1. In a bowl, whisk together the egg yolks, mustard, lemon ju"
+    assert handler.model.layers[0].attention.cache_k.equal(handler.model.layers[0].attention.cache_v)
     
