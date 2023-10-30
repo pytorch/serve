b60b0a2e01af1302862fd2fbfcb942446e230060
G <g.castro@degould.com>
2023-10-30 22:07:22 +0000

    integrated ModelArchiverConfig in existing scripts

--- model-archiver/model_archiver/model_packaging.py
+++ model-archiver/model_archiver/model_packaging.py
@@ -4,31 +4,32 @@ Command line interface to export model files to be used for inference by MXNet M
 
 import logging
 import shutil
-import sys
+from typing import Optional
 
 from model_archiver.arg_parser import ArgParser
+from model_archiver.model_archiver_config import ModelArchiverConfig
 from model_archiver.model_archiver_error import ModelArchiverError
 from model_archiver.model_packaging_utils import ModelExportUtils
 
 
-def package_model(args, manifest):
+def package_model(config: ModelArchiverConfig, manifest: str):
     """
     Internal helper for the exporting model command line interface.
     """
-    model_file = args.model_file
-    serialized_file = args.serialized_file
-    model_name = args.model_name
-    handler = args.handler
-    extra_files = args.extra_files
-    export_file_path = args.export_path
-    requirements_file = args.requirements_file
-    config_file = args.config_file
+    model_file = config.model_file
+    serialized_file = config.serialized_file
+    model_name = config.model_name
+    handler = config.handler
+    extra_files = config.extra_files
+    export_file_path = config.export_path
+    requirements_file = config.requirements_file
+    config_file = config.config_file
 
     try:
         ModelExportUtils.validate_inputs(model_name, export_file_path)
         # Step 1 : Check if .mar already exists with the given model name
         export_file_path = ModelExportUtils.check_mar_already_exists(
-            model_name, export_file_path, args.force, args.archive_format
+            model_name, export_file_path, config.force, config.archive_format
         )
 
         # Step 2 : Copy all artifacts to temp directory
@@ -45,7 +46,7 @@ def package_model(args, manifest):
 
         # Step 2 : Zip 'em all up
         ModelExportUtils.archive(
-            export_file_path, model_name, model_path, manifest, args.archive_format
+            export_file_path, model_name, model_path, manifest, config.archive_format
         )
         shutil.rmtree(model_path)
         logging.info(
@@ -53,19 +54,21 @@ def package_model(args, manifest):
         )
     except ModelArchiverError as e:
         logging.error(e)
-        sys.exit(1)
+        raise e
 
 
-def generate_model_archive():
+def generate_model_archive(config: Optional[ModelArchiverConfig] = None):
     """
     Generate a model archive file
     :return:
     """
 
     logging.basicConfig(format="%(levelname)s - %(message)s")
-    args = ArgParser.export_model_args_parser().parse_args()
-    manifest = ModelExportUtils.generate_manifest_json(args)
-    package_model(args, manifest=manifest)
+    if config is None:
+        args = ArgParser.export_model_args_parser().parse_args()
+        config = ModelArchiverConfig.from_args(args)
+    manifest = ModelExportUtils.generate_manifest_json(config)
+    package_model(config, manifest=manifest)
 
 
 if __name__ == "__main__":
