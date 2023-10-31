2d4ac046752db659c21e7d6a70bfbd91cf6bd48e
G <g.castro@degould.com>
2023-10-31 22:31:01 +0000

    ModelArchiverConfig to dataclass

--- model-archiver/model_archiver/model_archiver_config.py
+++ model-archiver/model_archiver/model_archiver_config.py
@@ -1,38 +1,25 @@
 import os
 from argparse import Namespace
+from dataclasses import dataclass
 from typing import Literal, Optional
 
 from model_archiver.manifest_components.manifest import RuntimeType
 
 
+@dataclass
 class ModelArchiverConfig:
-    def __init__(
-        self,
-        model_name: str,
-        handler: str,
-        version: str,
-        serialized_file: Optional[str] = None,
-        model_file: Optional[str] = None,
-        extra_files: Optional[str] = None,
-        runtime: str = RuntimeType.PYTHON.value,
-        export_path: str = os.getcwd(),
-        archive_format: Literal["default", "tgz", "no-archive"] = "default",
-        force: bool = False,
-        requirements_file: Optional[str] = None,
-        config_file: Optional[str] = None,
-    ):
-        self.model_name = model_name
-        self.handler = handler
-        self.version = version
-        self.serialized_file = serialized_file
-        self.model_file = model_file
-        self.extra_files = extra_files
-        self.runtime = runtime
-        self.export_path = export_path
-        self.archive_format = archive_format
-        self.force = force
-        self.requirements_file = requirements_file
-        self.config_file = config_file
+    model_name: str
+    handler: str
+    version: str
+    serialized_file: Optional[str] = None
+    model_file: Optional[str] = None
+    extra_files: Optional[str] = None
+    runtime: str = RuntimeType.PYTHON.value
+    export_path: str = os.getcwd()
+    archive_format: Literal["default", "tgz", "no-archive"] = "default"
+    force: bool = False
+    requirements_file: Optional[str] = None
+    config_file: Optional[str] = None
 
     @staticmethod
     def from_args(args: Namespace) -> "ModelArchiverConfig":
