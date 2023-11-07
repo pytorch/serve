9f0c51316263c377a86b7253115b78437a7c0ebf
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-11-07 21:22:42 +0000

    Add notebook to demo ts.deploy

--- ts/deploy/deploy_utils.py
+++ ts/deploy/deploy_utils.py
@@ -1,5 +1,7 @@
+import re
+import textwrap
+from inspect import getsource, getmodule
 from string import Template
-
 
 HANDLER_TEMPLATE=Template("""
 from ${base_class_module} import ${base_class}
@@ -11,6 +13,29 @@ ${inference}
 ${postprocess}
 """)
 
+HANDLER_METHODS = [
+    "init",
+    "initialize",
+    "preprocess",
+    "postprocess",
+]
+
+DEPLOY_KWARGS = [
+    "handler_name",
+]
+
+
+POSSIBLE_KWARGS = HANDLER_METHODS + DEPLOY_KWARGS
+
+
+def deploy(func):
+    _check_deployable(func)
+    
+    print(_codegen_handler_from_deployable(func))
+    
+    
+    
+    
 def deployable(base_class, **kwargs):
     print(f"{kwargs=}")
     def inner(func, **inner_kwargs):
@@ -23,51 +48,45 @@ def deployable(base_class, **kwargs):
     return inner
 
 
-def codegen_handler_from_deployable(deployable):
-    assert hasattr(deployable, "__is_deployable__")
-    assert hasattr(deployable, "_kwargs")
-    assert hasattr(deployable, "_base_class")
+def _codegen_handler_from_deployable(dep):
     
-    from types import MethodType
-    from inspect import getsource, getmodule, getmodulename
-    
-    handler = deployable._base_class()
-    
-    handler.inference = MethodType(deployable, handler)
-    
-    for f_name in ["initialize", "preprocess", "postprocess"]:
-        if f_name in deployable._kwargs:
-            setattr(handler, f_name, MethodType(deployable._kwargs[f_name], handler))
-            
-    def correct_indentation(source):
-        import textwrap
-        source = textwrap.dedent(source)
-        source = textwrap.indent(source, '    ')
-        return source
-    
-    inference_source = getsource(deployable)
-    import re
+    inference_source = getsource(dep)
     pattern = r'\@deployable\([^)]+\)'
     inference_source = re.sub(pattern, '', inference_source)
     
     sub_dict = {
-        "handler_name": "CustomHandler",
-        "base_class": deployable._base_class.__name__,
-        "base_class_module": getmodule(deployable._base_class).__name__,
-        "inference": correct_indentation(inference_source),
+        "handler_name": dep._kwargs.get("handle_name", "CustomHandler"),
+        "base_class": dep._base_class.__name__,
+        "base_class_module": getmodule(dep._base_class).__name__,
+        "inference": _correct_indentation(inference_source),
     }
-    for f_name in ["init", "initialize", "preprocess", "postprocess"]:
-        func = deployable._kwargs.get(f_name, None)
-        sub_dict[f_name] = correct_indentation(getsource(func)) if func else ""
+    for f_name in HANDLER_METHODS:
+        func = dep._kwargs.get(f_name, None)
+        sub_dict[f_name] = _correct_indentation(getsource(func)) if func else ""
         
     handler_code = HANDLER_TEMPLATE.substitute(**sub_dict)
             
     return handler_code
+
+
+def _check_deployable(func):
+    assert hasattr(func, "__is_deployable__")
+    assert hasattr(func, "_kwargs")
+    assert hasattr(func, "_base_class")
+    unknown_kwargs = [kw for kw in func._kwargs.keys() if kw not in POSSIBLE_KWARGS]
+    assert len(unknown_kwargs)==0, f"Unknown parameter given: {unknown_kwargs}"
     
 
+def _correct_indentation(source):
+        source = textwrap.dedent(source)
+        source = textwrap.indent(source, '    ')
+        return source
 
-def deploy(deployable):
-    print(codegen_handler_from_deployable(deployable))
+
+
+# def _create_mar_from_deployable(dep):
+    
+
     
     
     
