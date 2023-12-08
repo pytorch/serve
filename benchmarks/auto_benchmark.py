f8946011dff2109bc84cc072d3dfc40e7abefba0
Ubuntu <ubuntu@ip-172-31-15-190.us-west-2.compute.internal>
2023-12-08 22:57:45 +0000

    Revert changes in auto_benchmark.py and remove push trigger in workflow

--- benchmarks/auto_benchmark.py
+++ benchmarks/auto_benchmark.py
@@ -136,8 +136,8 @@ def install_torchserve(skip_ts_install, hw, ts_version, nightly):
         return
 
     # git checkout branch if it is needed
-    # cmd = "git checkout master && git reset --hard && git clean -dffx . && git pull --rebase"
-    # execute(cmd, wait=True)
+    cmd = "git checkout master && git reset --hard && git clean -dffx . && git pull --rebase"
+    execute(cmd, wait=True)
     print("successfully reset git")
 
     ts_install_cmd = None
