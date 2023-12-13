f93a1085f4a4d546e530630d98bb042694e3f1e2
lxning <lninga@amazon.com>
2023-12-13 11:01:28 -0800

    fix code link

--- cpp/src/backends/torch_scripted/handler/base_handler.cc
+++ cpp/src/backends/torch_scripted/handler/base_handler.cc
@@ -37,13 +37,13 @@ void BaseHandler::Handle(
   std::pair<std::string&, std::map<uint8_t, std::string>&> idx_to_req_id(
       req_ids, map_idx_to_req_id);
   try {
-    auto start_time = std::chrono::high_resolution_clock::now();
+    auto start_time = std::chrono::system_clock::now();
     auto inputs =
         Preprocess(device, idx_to_req_id, request_batch, response_batch);
     auto outputs =
         Inference(model, inputs, device, idx_to_req_id, response_batch);
     Postprocess(outputs, idx_to_req_id, response_batch);
-    auto stop_time = std::chrono::high_resolution_clock::now();
+    auto stop_time = std::chrono::system_clock::now();
     std::chrono::duration<double, std::milli> duration = stop_time - start_time;
     try {
       auto& handler_time_metric =
@@ -97,7 +97,7 @@ std::vector<torch::jit::IValue> BaseHandler::Preprocess(
   /**
    * @brief
    * Ref:
-   * https://github.com/pytorch/serve/blob/master/ts/torch_handler/vision_handler.py#L27
+   * https://github.com/pytorch/serve/blob/be5ff32dab0d81ceb1c2a9d42550ed5904ae9282/ts/torch_handler/vision_handler.py#L33
    */
   std::vector<torch::jit::IValue> batch_ivalue;
   std::vector<torch::Tensor> batch_tensors;
