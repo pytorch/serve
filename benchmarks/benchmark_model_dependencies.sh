e89c1ba9a617a4151faae8441da7fb4635e0b509
Ubuntu <ubuntu@ip-172-31-25-131.us-west-2.compute.internal>
2023-12-19 19:11:16 +0000

    Retrigger tests

--- benchmarks/benchmark_model_dependencies.sh
+++ benchmarks/benchmark_model_dependencies.sh
@@ -4,8 +4,8 @@
 
 # Install dependencies
 pip install chardet
-chmod +x ../examples/large_models/segment_anything_fast/install_segment_anything_fast.sh
-source ../examples/large_models/segment_anything_fast/install_segment_anything_fast.sh
+chmod +x examples/large_models/segment_anything_fast/install_segment_anything_fast.sh
+source examples/large_models/segment_anything_fast/install_segment_anything_fast.sh
 
 # Turn off A100G optimization
 export SEGMENT_ANYTHING_FAST_USE_FLASH_4=0
