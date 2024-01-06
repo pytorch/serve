a3ad43a60f3c84259d133bddc9d650fc3c8a20f9
lxning <lninga@amazon.com>
2024-01-05 16:42:28 -0800

    add handler utils

--- ts/handler_utils/utils.py
+++ ts/handler_utils/utils.py
@@ -2,10 +2,19 @@ import importlib
 
 
 def import_class(class_name: str, module_prefix=None):
-    module_name, class_name = class_name.rsplit(".", maxsplit=1)
+    module_name = ""
+    arr = class_name.rsplit(".", maxsplit=1)
+    if len(arr) == 2:
+        module_name, class_name = arr
+    else:
+        class_name = arr[0]
+
     if module_prefix is not None:
         module = importlib.import_module(f"{module_prefix}.{module_name}")
+    else:
+        module = importlib.import_module(module_name)
+
     model_class = getattr(module, class_name, None)
     if model_class is None:
-        raise ImportError(f"{class_name} not found in {module_name}.")
+        raise ImportError(f"class:{class_name} not found in module:{module_name}.")
     return model_class
