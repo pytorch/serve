ae0e7d3a5646a98387c0aa77bd7601c40e1d77df
lxning <lninga@amazon.com>
2024-01-05 16:53:04 -0800

    fix typo

--- examples/large_models/inferentia2/llama2/continuous_batching/inf2_handler.py
+++ examples/large_models/inferentia2/llama2/continuous_batching/inf2_handler.py
@@ -6,7 +6,7 @@ from ts.torch_handler.distributed.base_neuronx_continuous_batching_handler impor
 
 class LlamaContinuousBatchingHandler(BaseNeuronXContinuousBatchingHandler):
     def __init__(self):
-        super(BaseNeuronXContinuousBatchingHandler, self).__init__()
+        super(LlamaContinuousBatchingHandler, self).__init__()
         self.model_class = import_class(
             class_name="llama.model.LlamaForSampling",
             module_prefix="transformers_neuronx",
