49649d5c25db7c1861c89fa711929185e25ab28b
Matthias Reso <13337103+mreso@users.noreply.github.com>
2024-01-25 00:20:47 +0000

    Re-add test for llama.cpp example

--- cpp/test/examples/examples_test.cc
+++ cpp/test/examples/examples_test.cc
@@ -8,3 +8,12 @@ TEST_F(ModelPredictTest, TestLoadPredictBabyLlamaHandler) {
       "test/resources/torchscript_model/babyllama/babyllama_handler",
       "test/resources/torchscript_model/babyllama/prompt.txt", "llm_ts", 200);
 }
+
+TEST_F(ModelPredictTest, TestLoadPredictLlmHandler) {
+  this->LoadPredict(
+      std::make_shared<torchserve::LoadModelRequest>(
+          "test/resources/torchscript_model/llamacpp/llamacpp_handler", "llm",
+          -1, "", "", 1, false),
+      "test/resources/torchscript_model/llamacpp/llamacpp_handler",
+      "test/resources/torchscript_model/llamacpp/prompt.txt", "llm_ts", 200);
+}
