884964e21dc8ac0cc0a82a53508189200347cba2
agunapal <agunapal@ischool.berkeley.edu>
2024-01-25 01:41:31 +0000

    update the install script

--- examples/pt2/torch_export_aot_compile/install_pytorch_nightlies.sh
+++ examples/pt2/torch_export_aot_compile/install_pytorch_nightlies.sh
@@ -4,7 +4,11 @@
 pip uninstall torchtext torchdata torch torchvision torchaudio -y
 
 # Install nightly PyTorch and torchvision from the specified index URL
-pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121 --ignore-installed
+if nvidia-smi > /dev/null 2>&1; then
+    pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121 --ignore-installed
+else
+    pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu --ignore-installed
+fi
 
 # Optional: Display the installed PyTorch and torchvision versions
 python -c "import torch; print('PyTorch version:', torch.__version__)"
