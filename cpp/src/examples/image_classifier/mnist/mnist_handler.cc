3ecaf0bc3ced1d7f4c30b668252b666b1ef56546
Matthias Reso <13337103+mreso@users.noreply.github.com>
2024-01-25 20:38:35 -0800

    Feature/cpp baby llama rework (#2903)
    * Baby Llama - Porting run.c for integration and fixed clang type conversion errors.
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Custom preprocess implementation
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Free memory only after the inference is done
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Implement Postprocess
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Setting Fast compiler option
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Reading checkpoint path and tokenizer path from config file using folly
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Removing run.c from cmake
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Replace auto with appropriate data type
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Using smartpointers and initializing the vector with appropriate size upfront
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Using smartpointers
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Directly converting the tensor values to prompt token ids
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Moving run.c and common variables to .cc file
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Moving run.c to a separate folder
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Uncommenting the original run.c main method
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Implemented destructor to free up resources
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Supporting files for unit test
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Processing all the batch inputs
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Setting InferenceMode guard
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Updating InferenceMode to use torch::InferenceMode
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Updating class name to BabyLlamaHandler
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Renaming llm_handler target to babyllama_handler
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Adding dummy pt file
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Typo Fix
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Calculate tokens/per second for batch input
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Adding README.md for babyllama example
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Fixing out-of-bound mem access in babyllama example
    
    Move model instance out of ts_backend
    
    Use shared_ptr<void> for model to detangle from torchscript
    
    Move BaseHAndler to backends/handler
    
    Move model instance into core
    
    Remove Torchscript as a backend and implement it as a handler
    
    Move torchscript test out of backend folder
    
    Remove dummy.pt in babyllama + update README + mvoe babyllama test to new examples/examples_test.cc file
    
    * fix spell check
    
    * Move cpp babyllama example to main example folder
    
    * Add last successful location to error message in handle function
    
    * Fix babyllama batching by changing input/output from tensor to IValue
    
    * rename prompt file
    
    * Fix spellcheck
    
    ---------
    
    Co-authored-by: Shrinath Suresh <shrinath@ideas2it.com>

--- cpp/src/examples/image_classifier/mnist/mnist_handler.cc
+++ cpp/src/examples/image_classifier/mnist/mnist_handler.cc
@@ -2,15 +2,16 @@
 
 namespace mnist {
 void MnistHandler::Postprocess(
-    const torch::Tensor& data,
+    c10::IValue& data,
     std::pair<std::string&, std::map<uint8_t, std::string>&>& idx_to_req_id,
     std::shared_ptr<torchserve::InferenceResponseBatch>& response_batch) {
+  auto data_tensor = data.toTensor();
   for (const auto& kv : idx_to_req_id.second) {
     try {
       auto response = (*response_batch)[kv.second];
-      response->SetResponse(200, "data_tpye",
-                            torchserve::PayloadType::kDATA_TYPE_BYTES,
-                            torch::pickle_save(torch::argmax(data[kv.first])));
+      response->SetResponse(
+          200, "data_tpye", torchserve::PayloadType::kDATA_TYPE_BYTES,
+          torch::pickle_save(torch::argmax(data_tensor[kv.first])));
     } catch (const std::runtime_error& e) {
       LOG(ERROR) << "Failed to load tensor for request id:" << kv.second
                  << ", error: " << e.what();
@@ -34,11 +35,11 @@ void MnistHandler::Postprocess(
 
 #if defined(__linux__) || defined(__APPLE__)
 extern "C" {
-torchserve::torchscripted::BaseHandler* allocatorMnistHandler() {
+torchserve::BaseHandler* allocatorMnistHandler() {
   return new mnist::MnistHandler();
 }
 
-void deleterMnistHandler(torchserve::torchscripted::BaseHandler* p) {
+void deleterMnistHandler(torchserve::BaseHandler* p) {
   if (p != nullptr) {
     delete static_cast<mnist::MnistHandler*>(p);
   }
