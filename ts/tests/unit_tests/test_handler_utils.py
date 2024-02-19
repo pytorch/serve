cbfcec496e808c5c810600f0b02446a9d50077f8
lxning <lninga@amazon.com>
2024-02-19 13:23:40 -0800

    update test data

--- ts/tests/unit_tests/test_handler_utils.py
+++ ts/tests/unit_tests/test_handler_utils.py
@@ -3,14 +3,14 @@ from ts.handler_utils.utils import import_class
 
 def test_import_class_no_module_prefix():
     model_class = import_class(
-        class_name="ts.torch_handler.base_handler.BaseHandler",
+        class_name="transformers.LlamaTokenizer",
     )
-    assert "BaseHandler" == model_class.__class__.__name__
+    assert "LlamaTokenizer" == model_class.__class__.__name__
 
 
 def test_import_class_module_prefix():
     model_class = import_class(
-        class_name="BaseHandler",
-        module_prefix="ts.torch_handler.base_handler",
+        class_name="LlamaTokenizer",
+        module_prefix="transformers",
     )
-    assert "BaseHandler" == model_class.__class__.__name__
+    assert "LlamaTokenizer" == model_class.__class__.__name__
