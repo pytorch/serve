616c1adac7fb25905dc460da840ea6ee17baefeb
Ankith Gunapal <agunapal@ischool.Berkeley.edu>
2024-03-09 12:11:30 -0800

    Update pytorch nightly installation to use cpu version of torchtext (#3011)
    * torchtext nightly should be cpu
    
    * lint failure

--- ts_scripts/install_dependencies.py
+++ ts_scripts/install_dependencies.py
@@ -133,7 +133,10 @@ class Common:
         if nightly:
             pt_nightly = "cpu" if not cuda_version else cuda_version
             os.system(
-                f"pip3 install numpy --pre torch torchvision torchtext torchaudio --force-reinstall --index-url https://download.pytorch.org/whl/nightly/{pt_nightly}"
+                f"pip3 install numpy --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/{pt_nightly}"
+            )
+            os.system(
+                f"pip3 install --pre torchtext --index-url https://download.pytorch.org/whl/nightly/cpu"
             )
         else:
             self.install_torch_packages(cuda_version)
