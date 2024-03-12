0e0e91f3a93a908467fdb454ad87a33f77f79888
Matthias Reso <13337103+mreso@users.noreply.github.com>
2024-03-12 01:26:31 +0000

    Remove make format call from build.sh

--- cpp/build.sh
+++ cpp/build.sh
@@ -82,7 +82,6 @@ function build() {
   fi
 
   make -j "$JOBS"
-  make format
   make install
   echo -e "${COLOR_GREEN}torchserve_cpp build is complete. To run unit test: \
   ./_build/test/torchserve_cpp_test ${COLOR_OFF}"
