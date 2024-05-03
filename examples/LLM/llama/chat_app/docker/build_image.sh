c1db5c8c7c93752847d7f7b41d97e1fa0b4eeae1
Matthias Reso <13337103+mreso@users.noreply.github.com>
2024-05-02 18:11:05 -0700

    Updated chat bot example to support llama3

--- examples/LLM/llama/chat_app/docker/build_image.sh
+++ examples/LLM/llama/chat_app/docker/build_image.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-# Check if there are enough arguments
-if [ "$#" -eq 0 ] || [ "$#" -gt 1 ]; then
-  echo "Usage: $0 <HF Model>"
-  exit 1
-fi
-
-MODEL_NAME=$(echo "$1" | sed 's/\//---/g')
-echo "Model: " $MODEL_NAME
-
 BASE_IMAGE="pytorch/torchserve:latest-cpu"
 
-DOCKER_TAG="pytorch/torchserve:${MODEL_NAME}"
+DOCKER_TAG="pytorch/torchserve:chat_bot"
 
 # Get relative path of example dir
 EXAMPLE_DIR=$(dirname "$(readlink -f "$0")")
@@ -20,9 +11,10 @@ ROOT_DIR=$(realpath "$ROOT_DIR")
 EXAMPLE_DIR=$(echo "$EXAMPLE_DIR" | sed "s|$ROOT_DIR|./|")
 
 # Build docker image for the application
-DOCKER_BUILDKIT=1 docker buildx build --platform=linux/amd64 --file ${EXAMPLE_DIR}/Dockerfile --build-arg BASE_IMAGE="${BASE_IMAGE}" --build-arg EXAMPLE_DIR="${EXAMPLE_DIR}" --build-arg MODEL_NAME="${MODEL_NAME}"  --build-arg HUGGINGFACE_TOKEN -t "${DOCKER_TAG}" .
+DOCKER_BUILDKIT=1 docker buildx build --platform=linux/amd64 --file ${EXAMPLE_DIR}/Dockerfile --build-arg BASE_IMAGE="${BASE_IMAGE}" --build-arg EXAMPLE_DIR="${EXAMPLE_DIR}" --build-arg HUGGINGFACE_TOKEN -t "${DOCKER_TAG}" .
 
 echo "Run the following command to start the chat bot"
 echo ""
-echo docker run --rm -it --platform linux/amd64 -p 127.0.0.1:8080:8080 -p 127.0.0.1:8081:8081 -p 127.0.0.1:8082:8082 -p 127.0.0.1:8084:8084 -p 127.0.0.1:8085:8085 -v $(pwd)/model_store_1:/home/model-server/model-store $DOCKER_TAG
+echo docker run --rm -it --platform linux/amd64 -p 127.0.0.1:8080:8080 -p 127.0.0.1:8081:8081 -p 127.0.0.1:8082:8082 -p 127.0.0.1:8084:8084 -p 127.0.0.1:8085:8085 -v $(pwd)/model_store_1:/home/model-server/model-store -e MODEL_NAME="meta-llama/Llama-2-7b-chat-hf" $DOCKER_TAG
 echo ""
+echo "Note: You can replace the model identifier as needed"
--- examples/LLM/llama/chat_app/docker/dockerd-entrypoint.sh
+++ examples/LLM/llama/chat_app/docker/dockerd-entrypoint.sh
@@ -1,75 +1,75 @@
 #!/bin/bash
 set -e
 
-export LLAMA2_Q4_MODEL=/home/model-server/model-store/$MODEL_NAME/model/ggml-model-q4_0.gguf
+MODEL_DIR=$(echo "$MODEL_NAME" | sed 's/\//---/g')
+
+export LLAMA_Q4_MODEL=/home/model-server/model-store/$MODEL_DIR/model/ggml-model-q4_0.gguf
 
 
 create_model_cfg_yaml() {
   # Define the YAML content with a placeholder for the model name
   yaml_content="# TorchServe frontend parameters\nminWorkers: 1\nmaxWorkers: 1\nresponseTimeout: 1200\n#deviceType: \"gpu\"\n#deviceIds: [0,1]\n#torchrun:\n#    nproc-per-node: 1\n\nhandler:\n    model_name: \"${2}\"\n    manual_seed: 40"
 
-  # Create the YAML file with the specified model name
-  echo -e "$yaml_content" > "model-config-${1}.yaml"
+  # Create the YAML file
+  echo -e "$yaml_content" > "model-config.yaml"
 }
 
 create_model_archive() {
-    MODEL_NAME=$1
-    MODEL_CFG=$2
-    echo "Create model archive for ${MODEL_NAME} if it doesn't already exist"
-    if [ -d "/home/model-server/model-store/$MODEL_NAME" ]; then
-        echo "Model archive for $MODEL_NAME exists."
+    MODEL_DIR=$1
+    echo "Create model archive for ${MODEL_DIR} if it doesn't already exist"
+    if [ -d "/home/model-server/model-store/$MODEL_DIR" ]; then
+        echo "Model archive for $MODEL_DIR exists."
     fi
-    if [ -d "/home/model-server/model-store/$MODEL_NAME/model" ]; then
+    if [ -d "/home/model-server/model-store/$MODEL_DIR/model" ]; then
         echo "Model already download"
-        mv /home/model-server/model-store/$MODEL_NAME/model /home/model-server/model-store/
+        mv /home/model-server/model-store/$MODEL_DIR/model /home/model-server/model-store/
     else
         echo "Model needs to be downloaded"
     fi
-    torch-model-archiver --model-name "$MODEL_NAME" --version 1.0 --handler llama_cpp_handler.py --config-file $MODEL_CFG -r requirements.txt --archive-format no-archive --export-path /home/model-server/model-store -f
+    torch-model-archiver --model-name "$MODEL_DIR" --version 1.0 --handler llama_cpp_handler.py --config-file "model-config.yaml" -r requirements.txt --archive-format no-archive --export-path /home/model-server/model-store -f
     if [ -d "/home/model-server/model-store/model" ]; then
-        mv /home/model-server/model-store/model /home/model-server/model-store/$MODEL_NAME/
+        mv /home/model-server/model-store/model /home/model-server/model-store/$MODEL_DIR/
     fi
 }
 
 download_model() {
-   MODEL_NAME=$1
-   HF_MODEL_NAME=$2
-    if [ -d "/home/model-server/model-store/$MODEL_NAME/model" ]; then
-        echo "Model $HF_MODEL_NAME already downloaded"
+   MODEL_DIR=$1
+   MODEL_NAME=$2
+    if [ -d "/home/model-server/model-store/$MODEL_DIR/model" ]; then
+        echo "Model $MODEL_NAME already downloaded"
     else
-        echo "Downloading  model $HF_MODEL_NAME"
-        python Download_model.py --model_path /home/model-server/model-store/$MODEL_NAME/model --model_name $HF_MODEL_NAME
+        echo "Downloading  model $MODEL_NAME"
+        python Download_model.py --model_path /home/model-server/model-store/$MODEL_DIR/model --model_name $MODEL_NAME
     fi
 }
 
 quantize_model() {
-    if [ ! -f "$LLAMA2_Q4_MODEL" ]; then
-        tmp_model_name=$(echo "$MODEL_NAME" | sed 's/---/--/g')
-        directory_path=/home/model-server/model-store/$MODEL_NAME/model/models--$tmp_model_name/snapshots/
+    if [ ! -f "$LLAMA_Q4_MODEL" ]; then
+        tmp_model_name=$(echo "$MODEL_DIR" | sed 's/---/--/g')
+        directory_path=/home/model-server/model-store/$MODEL_DIR/model/models--$tmp_model_name/snapshots/
         HF_MODEL_SNAPSHOT=$(find $directory_path -type d -mindepth 1)
-        echo "Cleaning up previous build of llama-cpp"
-        git clone https://github.com/ggerganov/llama.cpp.git build
         cd build
-        make
-        python -m pip install -r requirements.txt
 
-        echo "Convert the 7B model to ggml FP16 format"
-        python convert.py $HF_MODEL_SNAPSHOT --outfile ggml-model-f16.gguf
+        echo "Convert the model to ggml FP16 format"
+        if [[ $MODEL_NAME == *"Meta-Llama-3"* ]]; then
+            python convert.py $HF_MODEL_SNAPSHOT --vocab-type bpe,hfft --outfile ggml-model-f16.gguf
+        else
+            python convert.py $HF_MODEL_SNAPSHOT --outfile ggml-model-f16.gguf
+        fi
 
         echo "Quantize the model to 4-bits (using q4_0 method)"
-        ./quantize ggml-model-f16.gguf $LLAMA2_Q4_MODEL q4_0
+        ./quantize ggml-model-f16.gguf $LLAMA_Q4_MODEL q4_0
 
         cd ..
-        echo "Saved quantized model weights to $LLAMA2_Q4_MODEL"
+        echo "Saved quantized model weights to $LLAMA_Q4_MODEL"
     fi
 }
 
-HF_MODEL_NAME=$(echo "$MODEL_NAME" | sed 's/---/\//g')
 if [[ "$1" = "serve" ]]; then
     shift 1
-    create_model_cfg_yaml $MODEL_NAME $HF_MODEL_NAME
-    create_model_archive $MODEL_NAME "model-config-$MODEL_NAME.yaml"
-    download_model $MODEL_NAME $HF_MODEL_NAME
+    create_model_cfg_yaml $MODEL_DIR $MODEL_NAME
+    create_model_archive $MODEL_DIR
+    download_model $MODEL_DIR $MODEL_NAME
     quantize_model
     streamlit run torchserve_server_app.py --server.port 8084 &
     streamlit run client_app.py --server.port 8085
