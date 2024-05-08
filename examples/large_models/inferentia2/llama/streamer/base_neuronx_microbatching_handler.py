0b4539fa7fde9b5715aea4c92210803086d156d8
lxning <23464292+lxning@users.noreply.github.com>
2024-05-08 08:56:21 -0700

    Example llama3 on inf2 (#3133)
    * add llama3 support
    
    * delete model config yaml
    
    * update model config
    
    * fix typo
    
    ---------
    
    Co-authored-by: Matthias Reso <13337103+mreso@users.noreply.github.com>

--- examples/large_models/inferentia2/llama/streamer/base_neuronx_microbatching_handler.py
+++ examples/large_models/inferentia2/llama/streamer/base_neuronx_microbatching_handler.py
@@ -5,7 +5,8 @@ from threading import Thread
 
 import torch_neuronx
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
-from transformers_neuronx.config import NeuronConfig
+from transformers_neuronx.config import GenerationConfig, NeuronConfig
+from transformers_neuronx.constants import GQA
 from transformers_neuronx.generation_utils import HuggingFaceGenerationModelAdapter
 from transformers_neuronx.module import save_pretrained_split
 
@@ -103,7 +104,7 @@ class BaseNeuronXContinuousBatchingHandler(BaseHandler):
 
             raise error
         self._set_class(ctx)
-        self.tokenizer = self.tokenizer_class.from_pretrained(
+        self.tokenizer = AutoTokenizer.from_pretrained(
             model_checkpoint_path, return_tensors="pt", padding_side="left"
         )
         self.tokenizer.pad_token = self.tokenizer.eos_token
@@ -118,6 +119,9 @@ class BaseNeuronXContinuousBatchingHandler(BaseHandler):
             context_length_estimate=handler_config.get(
                 "context_length_estimate", [self.max_length]
             ),
+            attention_layout="BSH",
+            group_query_attention=GQA.REPLICATED_HEADS,
+            on_device_generation=GenerationConfig(do_sample=True),
             neuron_config=neuron_config,
         )
         self.model = self.model_class.from_pretrained(model_checkpoint_path, **kwargs)
@@ -208,15 +212,4 @@ class BaseNeuronXContinuousBatchingHandler(BaseHandler):
         self.model_class = import_class(
             class_name=model_class_name,
             module_prefix=model_module_prefix,
-        )
-
-        tokenizer_class_name = handler_config.get("tokenizer_class_name", None)
-        assert (
-            tokenizer_class_name
-        ), "tokenizer_class_name not found in the section of handler in model config yaml file"
-
-        tokenizer_module_prefix = handler_config.get("tokenizer_module_prefix", None)
-
-        self.tokenizer_class = import_class(
-            class_name=tokenizer_class_name, module_prefix=tokenizer_module_prefix
         )
