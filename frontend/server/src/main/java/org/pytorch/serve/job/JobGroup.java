81525c5c908249fde79eb081c3285155b0e6ac9b
lxning <lninga@amazon.com>
2024-05-30 10:31:27 -0700

    fmt

--- frontend/server/src/main/java/org/pytorch/serve/job/JobGroup.java
+++ frontend/server/src/main/java/org/pytorch/serve/job/JobGroup.java
@@ -3,7 +3,6 @@ package org.pytorch.serve.job;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
