5f3df71e714d5657e64bb82026a5ebbfe43e1c6d
Matthias Reso <13337103+mreso@users.noreply.github.com>
2024-06-21 21:54:29 -0700

    Asynchronous worker communication and vllm integration (#3146)
    * Added dummy async comm worker thread
    
    * First version of async worker in frontend running
    
    * [WIP]Running async worker but requests get corrupted if parallel
    
    * First version running with thread feeding + async predict
    
    * shorten vllm test time
    
    * Added AsyncVLLMEngine
    
    * Extend vllm test with multiple possible prompts
    
    * Batch size =1 and remove stream in test
    
    * Switched vllm examples to async comm and added llama3 example
    
    * Fix typo
    
    * Corrected java file formatting
    
    * Cleanup and silent chatty debug message
    
    * Added multi-gpu support to vllm examples
    
    * fix java format
    
    * Remove debugging messages
    
    * Fix async comm worker test
    
    * Added cl_socket to fixture
    
    * Added multi worker note to vllm example readme
    
    * Disable tests
    
    * Enable async worker comm test
    
    * Debug CI
    
    * Fix python version <= 3.9 issue in async worker
    
    * Renamed async worker test
    
    * Update frontend/server/src/main/java/org/pytorch/serve/wlm/AsyncBatchAggregator.java
    
    Remove job from jobs_in_backend on error
    
    Co-authored-by: Naman Nandan <namankt55@gmail.com>
    
    * Unskip vllm example test
    
    * Clean up async worker code
    
    * Safely remove jobs from jobs_in_backend
    
    * Let worker die if one of the threads in async service dies
    
    * Add description of parallelLevel and parallelType=custom to docs/large_model_inference.md
    
    * Added description of parallelLevel to model-archiver readme.md
    
    * fix typo + added words
    
    * Fix skip condition for vllm example test
    
    ---------
    
    Co-authored-by: Naman Nandan <namankt55@gmail.com>

--- examples/large_models/vllm/Readme.md
+++ examples/large_models/vllm/Readme.md
@@ -1,10 +1,16 @@
 # Example showing inference with vLLM
 
 This folder contains multiple demonstrations showcasing the integration of [vLLM Engine](https://github.com/vllm-project/vllm) with TorchServe, running inference with continuous batching.
-vLLM achieves high throughput using PagedAttention. More details can be found [here](https://vllm.ai/)
+vLLM achieves high throughput using PagedAttention. More details can be found [here](https://vllm.ai/).
+The vLLM integration uses our new asynchronous worker communication mode which decoupled communication between frontend and backend from running the actual inference.
+By using this new feature TorchServe is capable to feed incoming requests into the vLLM engine while asynchronously running the engine in the backend.
+As long as a single request is inside the engine it will continue to run and asynchronously stream out the results until the request is finished.
+New requests are added to the engine in a continuous fashion similar to the continuous batching mode shown in other examples.
+For all examples distributed inference can be enabled by following the instruction [here](./Readme.md#distributed-inference)
 
-- demo1: [Mistral](mistral)
-- demo2: [lora](lora)
+- demo1: [Meta-Llama3](llama3)
+- demo2: [Mistral](mistral)
+- demo3: [lora](lora)
 
 ### Supported vLLM Configuration
 * LLMEngine configuration:
@@ -13,3 +19,25 @@ vLLM [EngineArgs](https://github.com/vllm-project/vllm/blob/258a2c58d08fc7a24255
 
 * Sampling parameters for text generation:
 vLLM [SamplingParams](https://github.com/vllm-project/vllm/blob/258a2c58d08fc7a242556120877a89404861fbce/vllm/sampling_params.py#L27) is defined in the JSON format, for example, [prompt.json](lora/prompt.json).
+
+### Distributed Inference
+All examples can be easily distributed over multiple GPUs by enabling tensor parallelism in vLLM.
+To enable distributed inference the following additions need to made to the model-config.yaml of the examples where 4 is the number of desired GPUs to use for the inference:
+
+```yaml
+# TorchServe frontend parameters
+...
+parallelType: "custom"
+parallelLevel: 4
+
+handler:
+    ...
+    vllm_engine_config:
+        ...
+        tensor_parallel_size: 4
+```
+
+### Multi-worker Note:
+While this example in theory works with multiple workers it would distribute the incoming requests in a round robin fashion which might lead to non optimal worker/hardware utilization.
+It is therefore advised to only use a single worker per engine and utilize tensor parallelism to distribute the model over multiple GPUs as described in the previous section.
+This will result in better hardware utilization and inference performance.
---
+++ frontend/server/src/main/java/org/pytorch/serve/wlm/AsyncWorkerThread.java
@@ -0,0 +1,267 @@
+package org.pytorch.serve.wlm;
+
+import static org.pytorch.serve.wlm.WorkerThread.logger;
+import static org.pytorch.serve.wlm.WorkerThread.loggerTelemetryMetrics;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.SocketAddress;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import org.pytorch.serve.job.Job;
+import org.pytorch.serve.job.RestJob;
+import org.pytorch.serve.util.ConfigManager;
+import org.pytorch.serve.util.Connector;
+import org.pytorch.serve.util.codec.ModelResponseDecoder;
+import org.pytorch.serve.util.messages.InputParameter;
+import org.pytorch.serve.util.messages.ModelWorkerResponse;
+import org.pytorch.serve.util.messages.RequestInput;
+import org.pytorch.serve.util.messages.WorkerCommands;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AsyncWorkerThread extends WorkerThread {
+    // protected ConcurrentHashMap requestsInBackend;
+    protected static final Logger logger = LoggerFactory.getLogger(AsyncWorkerThread.class);
+
+    protected boolean loadingFinished;
+
+    public AsyncWorkerThread(
+            ConfigManager configManager,
+            EventLoopGroup backendEventGroup,
+            int port,
+            int gpuId,
+            Model model,
+            BatchAggregator aggregator,
+            WorkerStateListener listener) {
+        super(configManager, backendEventGroup, port, gpuId, model, aggregator, listener);
+        loadingFinished = false;
+    }
+
+    @Override
+    public void run() {
+        responseTimeout = model.getResponseTimeout();
+        Thread thread = Thread.currentThread();
+        thread.setName(getWorkerName());
+        currentThread.set(thread);
+        req = null;
+        int status = HttpURLConnection.HTTP_INTERNAL_ERROR;
+
+        try {
+            connect();
+
+            while (isRunning()) {
+                req = aggregator.getRequest(workerId, state);
+                WorkerCommands workerCmd = req.getCommand();
+
+                long wtStartTime = System.currentTimeMillis();
+                int repeats = getRepeats(workerCmd);
+                logger.debug(
+                        "Flushing req.cmd {} repeats {} to backend at: {}",
+                        workerCmd,
+                        repeats,
+                        wtStartTime);
+
+                try {
+                    backendChannel.get(0).writeAndFlush(req).sync();
+                    logger.debug("Successfully flushed req");
+                } catch (InterruptedException e) {
+                    logger.error("Failed to send request to backend", e);
+                }
+                req = null;
+            }
+        } catch (InterruptedException e) {
+            logger.debug("System state is : " + state);
+            if (state == WorkerState.WORKER_SCALED_DOWN || state == WorkerState.WORKER_STOPPED) {
+                logger.debug("Shutting down the thread .. Scaling down.");
+            } else {
+                logger.debug(
+                        "Backend worker monitoring thread interrupted or backend worker process died., responseTimeout:"
+                                + responseTimeout
+                                + "sec",
+                        e);
+            }
+        } catch (WorkerInitializationException e) {
+            logger.error("Backend worker error", e);
+        } catch (OutOfMemoryError oom) {
+            logger.error("Out of memory error when creating workers", oom);
+            status = HttpURLConnection.HTTP_ENTITY_TOO_LARGE;
+            if (ConfigManager.getInstance().isTelemetryEnabled()) {
+                loggerTelemetryMetrics.info(
+                        "ModelServerError.Count:1|#TorchServe:{},{}:-1",
+                        ConfigManager.getInstance().getVersion(),
+                        oom.getClass().getCanonicalName());
+            }
+        } catch (IllegalStateException e) {
+            logger.error("IllegalStateException error", e);
+        } catch (Throwable t) {
+            logger.warn("Backend worker thread exception.", t);
+            if (ConfigManager.getInstance().isTelemetryEnabled()) {
+                loggerTelemetryMetrics.info(
+                        "ModelServerError.Count:1|#TorchServe:{},{}:-1",
+                        ConfigManager.getInstance().getVersion(),
+                        t.getClass().getCanonicalName());
+            }
+        } finally {
+            // WorkerThread is running in thread pool, the thread will be assigned to next
+            // Runnable once this worker is finished. If currentThread keep holding the reference
+            // of the thread, currentThread.interrupt() might kill next worker.
+            for (int i = 0; i < backendChannel.size(); i++) {
+                backendChannel.get(i).disconnect();
+            }
+            backendChannel.clear();
+            currentThread.set(null);
+            Integer exitValue = lifeCycle.getExitValue();
+
+            if (exitValue != null && exitValue == 137) {
+                status = HttpURLConnection.HTTP_ENTITY_TOO_LARGE;
+            }
+
+            if (req != null) {
+                aggregator.sendError(req, "Worker died.", status);
+            }
+            aggregator.cleanJobs();
+            setState(WorkerState.WORKER_STOPPED, status);
+            lifeCycle.exit();
+            if (isHealthy()) { // still within maxRetryTimeoutInMill window
+                retry();
+            }
+        }
+    }
+
+    protected void connect() throws WorkerInitializationException, InterruptedException {
+        if (!configManager.isDebug()) {
+            String ids = getDeviceIds();
+            logger.debug("Device Ids: " + ids);
+            lifeCycle.startWorker(port, ids);
+        }
+
+        String modelName = model.getModelName();
+        String modelVersion = model.getVersion();
+        setState(WorkerState.WORKER_STARTED, HttpURLConnection.HTTP_OK);
+        final CountDownLatch latch = new CountDownLatch(1);
+        final int responseBufferSize = configManager.getMaxResponseSize();
+        try {
+            Connector connector = new Connector(port);
+            Bootstrap b = new Bootstrap();
+            b.group(backendEventGroup)
+                    .channel(connector.getClientChannel())
+                    .handler(
+                            new ChannelInitializer<Channel>() {
+                                @Override
+                                public void initChannel(Channel ch) {
+                                    ChannelPipeline p = ch.pipeline();
+                                    p.addLast(ENCODER);
+                                    p.addLast(new ModelResponseDecoder(responseBufferSize));
+                                    p.addLast(new AsyncWorkerHandler());
+                                }
+                            });
+
+            SocketAddress address = connector.getSocketAddress();
+            logger.info("Connecting to: {}", address);
+            backendChannel.add(b.connect(address).sync().channel());
+            backendChannel
+                    .get(0)
+                    .closeFuture()
+                    .addListener(
+                            (ChannelFutureListener)
+                                    future -> {
+                                        latch.countDown();
+                                        logger.info(
+                                                "{} Worker disconnected. {}", getWorkerId(), state);
+                                        Thread thread = currentThread.getAndSet(null);
+                                        if (thread != null) {
+                                            thread.interrupt();
+                                        }
+                                    });
+            backendChannel
+                    .get(0)
+                    .newSucceededFuture()
+                    .addListener(
+                            (ChannelFutureListener)
+                                    future -> {
+                                        // TODO:
+                                        // use gpu, batch size in load model command
+                                        if (latch.getCount() == 1) {
+                                            RequestInput input =
+                                                    new RequestInput(UUID.randomUUID().toString());
+                                            if (gpuId >= 0) {
+                                                input.addParameter(
+                                                        new InputParameter(
+                                                                "gpu", String.valueOf(gpuId)));
+                                            }
+
+                                            Job job =
+                                                    new RestJob(
+                                                            null,
+                                                            modelName,
+                                                            modelVersion,
+                                                            WorkerCommands.LOAD,
+                                                            input);
+                                            model.addJob(workerId, job);
+                                        }
+                                        latch.countDown();
+                                    });
+
+            if (!latch.await(WORKER_TIMEOUT, TimeUnit.MINUTES)) {
+                throw new WorkerInitializationException(
+                        "Worker failed to initialize within " + WORKER_TIMEOUT + " mins");
+            }
+            running.set(true);
+        } catch (Throwable t) {
+            /* https://github.com/netty/netty/issues/2597 */
+            if (t instanceof IOException) {
+                throw new WorkerInitializationException("Failed to connect to worker.", t);
+            }
+            throw t;
+        }
+    }
+
+    @ChannelHandler.Sharable
+    protected class AsyncWorkerHandler extends SimpleChannelInboundHandler<ModelWorkerResponse> {
+
+        @Override
+        public void channelRead0(ChannelHandlerContext ctx, ModelWorkerResponse msg) {
+            try {
+                aggregator.sendResponse(msg);
+                if (!loadingFinished) {
+                    if (msg.getCode() == 200) {
+                        logger.info("Worker loaded the model successfully");
+                        setState(WorkerState.WORKER_MODEL_LOADED, HttpURLConnection.HTTP_OK);
+                        backoffIdx = 0;
+                        loadingFinished = true;
+                    } else {
+                        setState(WorkerState.WORKER_ERROR, msg.getCode());
+                    }
+                }
+            } catch (NullPointerException e) {
+                logger.error("Failed to send response", e);
+                throw new IllegalStateException("Message was empty");
+            }
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+            logger.error("Unknown exception", cause);
+            if (cause instanceof OutOfMemoryError) {
+                ModelWorkerResponse msg = new ModelWorkerResponse();
+                msg.setCode(HttpURLConnection.HTTP_ENTITY_TOO_LARGE);
+                msg.setMessage(cause.getMessage());
+                if (!replies.offer(msg)) {
+                    throw new IllegalStateException("Reply queue is full.");
+                }
+            }
+            ctx.close();
+        }
+    }
+}
