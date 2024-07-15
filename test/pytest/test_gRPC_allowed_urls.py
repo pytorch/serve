9bc00ae4ae8166aa74455f9143b1e0e6ed6eafbd
Naman Nandan <namankt55@gmail.com>
2024-07-14 17:57:04 -0700

    Fix linter errors

--- test/pytest/test_gRPC_allowed_urls.py
+++ test/pytest/test_gRPC_allowed_urls.py
@@ -1,6 +1,5 @@
-import pytest
-
 import management_pb2
+import pytest
 import test_gRPC_utils
 import test_utils
 
@@ -9,7 +8,9 @@ def setup_module(module):
     test_utils.torchserve_cleanup()
     config_file = test_utils.ROOT_DIR + "/config.properties"
     with open(config_file, "w") as f:
-        f.write("allowed_urls=https://torchserve.s3.amazonaws.com/mar_files/densenet161.mar") 
+        f.write(
+            "allowed_urls=https://torchserve.s3.amazonaws.com/mar_files/densenet161.mar"
+        )
     test_utils.start_torchserve(snapshot_file=config_file, gen_mar=False)
 
 
@@ -17,7 +18,7 @@ def teardown_module(module):
     test_utils.torchserve_cleanup()
 
 
-def register(stub, model_url, model_name, metadata): 
+def register(stub, model_url, model_name, metadata):
     params = {
         "url": model_url,
         "initial_workers": 1,
@@ -30,21 +31,38 @@ def register(stub, model_url, model_name, metadata):
 
 
 def test_gRPC_allowed_urls():
-    management_stub = test_gRPC_utils.get_management_stub() 
+    management_stub = test_gRPC_utils.get_management_stub()
 
     # register model with permitted url
     metadata = (("protocol", "gRPC"),)
-    register(management_stub, "https://torchserve.s3.amazonaws.com/mar_files/densenet161.mar", "densenet161", metadata)
+    register(
+        management_stub,
+        "https://torchserve.s3.amazonaws.com/mar_files/densenet161.mar",
+        "densenet161",
+        metadata,
+    )
 
     # register model with unpermitted url
-    with pytest.raises(Exception, match=r".*Given URL.*does not match any allowed URL.*"):
-        register(management_stub, "https://torchserve.s3.amazonaws.com/mar_files/resnet-18.mar", "resnet-18", metadata)
+    with pytest.raises(
+        Exception, match=r".*Given URL.*does not match any allowed URL.*"
+    ):
+        register(
+            management_stub,
+            "https://torchserve.s3.amazonaws.com/mar_files/resnet-18.mar",
+            "resnet-18",
+            metadata,
+        )
 
 
 def test_gRPC_allowed_urls_relative_path():
-    management_stub = test_gRPC_utils.get_management_stub() 
+    management_stub = test_gRPC_utils.get_management_stub()
 
     # register model with relative path in model url
-    metadata = (("protocol", "gRPC"),) 
+    metadata = (("protocol", "gRPC"),)
     with pytest.raises(Exception, match=r".*Relative path is not allowed in url.*"):
-        register(management_stub, "https://torchserve.s3.amazonaws.com/mar_files/../mar_files/densenet161.mar", "densenet161-relative-path", metadata)
+        register(
+            management_stub,
+            "https://torchserve.s3.amazonaws.com/mar_files/../mar_files/densenet161.mar",
+            "densenet161-relative-path",
+            metadata,
+        )
