41e1d65bc5e37f5975fc6d7b827df03a75412d5d
Udai Jain <udaij12@gmail.com>
2024-09-18 00:18:16 +0000

    testing adding startup

--- frontend/server/src/main/java/org/pytorch/serve/snapshot/SnapshotManager.java
+++ frontend/server/src/main/java/org/pytorch/serve/snapshot/SnapshotManager.java
@@ -112,8 +112,9 @@ public final class SnapshotManager {
     }
 
     public void restore(String modelSnapshot) throws InvalidSnapshotException, IOException {
-        logger.info("Started restoring models from snapshot {}", modelSnapshot);
-        Snapshot snapshot = snapshotSerializer.getSnapshot(modelSnapshot);
+        String result = modelSnapshot.substring(0, modelSnapshot.length() - 1) + ",\n \"startupTimeout\": 120 }";
+        logger.info("Started restoring models from snapshot {}", result);
+        Snapshot snapshot = snapshotSerializer.getSnapshot(result);
         // Validate snapshot
         validate(snapshot);
         // Init. models
